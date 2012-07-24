19d67a2cede09caae28d4440be68714aa3b31b32
petrockblog <florian@petrockbloggit>
2012-07-24 21:56:15 +0200

    Added EmulationStation as front end and several smaller improvements
    - EmulationStation as front end
    - New folder structure with "RetroPie" as main directory
    - some smaller improvements and bug fixes

--- retropie_setup.sh
+++ retropie_setup.sh
@@ -18,7 +18,7 @@
 # 
 #  RetroPie-Setup is freeware for PERSONAL USE only. Commercial users should
 #  seek permission of the copyright holders first. Commercial use includes
-#  charging money for SNESDev or software derived from SNESDev.
+#  charging money for RetroPie-Setup or software derived from RetroPie-Setup.
 # 
 #  The copyright holders request that bug fixes and improvements to the code
 #  should be forwarded to them so everyone can benefit from the modifications
@@ -27,9 +27,14 @@
 #  Raspberry Pi is a trademark of the Raspberry Pi Foundation.
 # 
 
-# add user pi to groups "video" and "audio"
+# add user pi to groups "video", "audio", and "input"
 sudo usermod -a -G video pi
 sudo usermod -a -G audio pi
+sudo usermod -a -G input pi
+
+# make sure ALSA soundmodule is active
+echo "Enabling ALSA module"
+sudo modprobe snd_bcm2835
 
 # needed by SDL for working joypads
 export SDL_NOMOUSE=1
@@ -43,10 +48,23 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 
 # make sure that all needed packages are installed
-sudo apt-get install -y libsdl1.2-dev screen scons libasound2-dev pkg-config libgtk2.0-dev
+sudo apt-get install -y libsdl1.2-dev screen scons libasound2-dev pkg-config libgtk2.0-dev libsdl-ttf2.0-dev libboost-filesystem-dev
 
 # install latest rpi-update script (to enable firmware update)
 sudo wget http://goo.gl/1BOfJ -O /usr/bin/rpi-update && sudo chmod +x /usr/bin/rpi-update
+
+# prepare folder structure for emulator, cores, front end, and roms
+rootdir=RetroPie
+cd
+mkdir $rootdir
+mkdir "$rootdir/roms"
+mkdir "$rootdir/roms/snes"
+mkdir "$rootdir/roms/nes"
+mkdir "$rootdir/roms/megadrive"
+mkdir "$rootdir/roms/gba"
+mkdir "$rootdir/roms/mame"
+
+mkdir "$rootdir/emulatorcores"
 
 # install BCM library to enable GPIO access by SNESDev-RPi
 echo "Installing BCM2835 library"
@@ -59,16 +77,13 @@ make
 sudo make install
 cd
 rm bcm2835-1.3.tar.gz
-
-# make sure ALSA soundmodule is active
-echo "Enabling ALSA module"
-sudo modprobe snd_bcm2835
+rm -rf bcm2835-1.3
 
 # install RetroArch emulator
 echo "Installing RetroArch emulator"
 cd
-git clone git://github.com/ToadKing/RetroArch-Rpi.git
-cd RetroArch-Rpi
+git clone git://github.com/ToadKing/RetroArch-Rpi.git "$rootdir/RetroArch-Rpi"
+cd "$rootdir/RetroArch-Rpi"
 ./configure --disable-libpng
 make
 sudo make install
@@ -76,48 +91,58 @@ sudo make install
 # install SNES emulator core
 echo "Installing SNES core"
 cd
-git clone git://github.com/ToadKing/pocketsnes-libretro.git
-cd pocketsnes-libretro 
+git clone git://github.com/ToadKing/pocketsnes-libretro.git "$rootdir/emulatorcores/pocketsnes-libretro"
+cd "$rootdir/emulatorcores/pocketsnes-libretro"
 make
 
 # install NES emulator core
 echo "Installing NES core"
 cd
-git clone git://github.com/libretro/fceu-next.git
-cd fceu-next
+git clone git://github.com/libretro/fceu-next.git "$rootdir/emulatorcores/fceu-next"
+cd "$rootdir/emulatorcores/fceu-next"
 make -f Makefile.libretro-fceumm
 
 # install Sega Mega Drive emulator core
 echo "Installing Mega Drive core"
 cd
-git clone git://github.com/libretro/Genesis-Plus-GX.git
-cd Genesis-Plus-GX
+git clone git://github.com/libretro/Genesis-Plus-GX.git "$rootdir/emulatorcores/Genesis-Plus-GX"
+cd "$rootdir/emulatorcores/Genesis-Plus-GX"
 make -f Makefile.libretro 
 
 # install Gameboy emulator core
 echo "Installing Gameboy core"
 cd
-git clone git://github.com/libretro/gambatte-libretro.git
+git clone git://github.com/libretro/gambatte-libretro.git "$rootdir/emulatorcores/gambatte-libretro"
 cd gambatte-libretro/libgambatte
 make -f Makefile.libretro 
 
 # install MAME emulator core
 echo "Installing MAME core"
 cd
-git clone git://github.com/libretro/imame4all-libretro.git
-cd imame4all-libretro
+git clone git://github.com/libretro/imame4all-libretro.git "$rootdir/emulatorcores/imame4all-libretro"
+cd "$rootdir/emulatorcores/imame4all-libretro"
 make -f makefile.libretro ARM=1
 
 # install Doom WADs emulator core
 echo "Installing Doom core"
 cd 
-git clone git://github.com/libretro/libretro-prboom.git
-cd libretro-prboom
+git clone git://github.com/libretro/libretro-prboom.git "$rootdir/emulatorcores/libretro-prboom"
+cd "$rootdir/emulatorcores/libretro-prboom"
 make
 cd
 
 # install SNESDev as GPIO interface for SNES controllers
 cd
-git clone git://github.com/petrockblog/SNESDev-RPi.git
-cd SNESDev-RPi
+git clone git://github.com/petrockblog/SNESDev-RPi.git "$rootdir/SNESDev-Rpi"
+cd "$rootdir/SNESDev-Rpi"
+make clean
 make
+
+# install EmulationStation as graphical front end for the emulators
+cd
+git clone git://github.com/Aloshi/EmulationStation.git "$rootdir/EmulationStation"
+cd "$rootdir/EmulationStation"
+make
+
+cd
+echo "Finishes with RetroPie setup. Have fun :-)"
