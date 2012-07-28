1d5f43ef7c809f115ce7357fb133606d652ef65d
petrockblog <florian@petrockbloggit>
2012-07-28 16:47:27 +0200

    Added checks for modules, variables, and added also use of pushd/popd

--- retropie_setup.sh
+++ retropie_setup.sh
@@ -39,137 +39,210 @@ function ask()
     esac
 }
 
+function addLineToFile()
+{
+	if [[ -f "$2" ]]; then
+        cp "$2" ./temp
+        sudo mv "$2" "$2.old"
+    fi
+    echo "$1" >> ./temp
+    sudo mv ./temp "$2"
+}
+
+function printMsg()
+{
+	echo -e "\n= = = = = = = = = = = = = = = = = = = = =\n$1\n= = = = = = = = = = = = = = = = = = = = =\n"
+}
+
+if [ $# -ne 1 ]
+then
+    rootdir="~/RetroPie"
+else
+    rootdir=$1
+fi
+printMsg "Installing all RetroPie-packages into the directory $rootdir"
+
 # install latest rpi-update script (to enable firmware update)
+printMsg "Installing latest rpi-update script"
 sudo wget http://goo.gl/1BOfJ -O /usr/bin/rpi-update && sudo chmod +x /usr/bin/rpi-update
-ask "Do you want to perform a firmware upgrade now?\nThis might take some time." && sudo rpi-update
+ask "Do you want to perform a firmware upgrade now?\nThis might take some minutes." && sudo rpi-update
 
 # update APT repositories and update system
 sudo apt-get -y update
-ask "Do you want to perform a system upgrade now?\nThis might take some time." && sudo apt-get -y upgrade
+ask "Do you want to perform a system upgrade now?\nThis might take some minutes." && sudo apt-get -y upgrade
 
 # add user pi to groups "video", "audio", and "input"
-echo -e "Adding user pi to groups video, audio, and input."
+printMsg "Adding user pi to groups video, audio, and input."
 sudo usermod -a -G video pi
 sudo usermod -a -G audio pi
 sudo usermod -a -G input pi
 
 # make sure ALSA, uinput, and joydev modules are active
-echo "Enabling ALSA, uinput, and joydev modules permanently"
+printMsg "Enabling ALSA, uinput, and joydev modules permanently"
 sudo modprobe snd_bcm2835
 sudo modprobe uinput
 sudo modprobe joydev
 
-cp /etc/modules ./temp
-sudo mv /etc/modules /etc/modules.old
-sudo echo -e "uinput\njoydev" >> ./temp
-sudo mv ./temp /etc/modules
+if [[ ! grep -Fxq "uinput" /etc/modules ]]; then
+	addLineToFile "uinput" "/etc/modules"
+fi
+if [[ ! grep -Fxq "joydev" /etc/modules ]]; then
+	addLineToFile "joydev" "/etc/modules"
+fi
 
 # needed by SDL for working joypads
-echo "Exporting SDL_MOUSE=1 permanently to .bashrc"
+printMsg "Exporting SDL_MOUSE=1 permanently to .bashrc"
 export SDL_NOMOUSE=1
-cd
-echo -e "\nexport SDL_NOMOUSE=1" >> .bashrc
+if [[ ! grep -Fxq "export SDL_NOMOUSE=1" ~/.bashrc ]]; then
+    echo -e "\nexport SDL_NOMOUSE=1" >> ~/.bashrc
+fi
 
 # make sure that all needed packages are installed
+printMsg "Making sure that all needed packaged are installed"
 sudo apt-get install -y libsdl1.2-dev screen scons libasound2-dev pkg-config libgtk2.0-dev libsdl-ttf2.0-dev libboost-filesystem-dev zip libxml2
 
 # prepare folder structure for emulator, cores, front end, and roms
-rootdir=RetroPie
-cd
-mkdir $rootdir
-mkdir "$rootdir/roms"
-mkdir "$rootdir/roms/snes"
-mkdir "$rootdir/roms/nes"
-mkdir "$rootdir/roms/megadrive"
-mkdir "$rootdir/roms/gba"
-mkdir "$rootdir/roms/mame"
-mkdir "$rootdir/roms/doom"
+if [[ ! -d $rootdir ]]; then
+    mkdir $rootdir
+fi
+if [[ ! -d "$rootdir/roms" ]]; then
+    mkdir "$rootdir/roms"
+fi
+if [[ ! -d "$rootdir/roms/snes" ]]; then
+    mkdir "$rootdir/roms/snes"
+fi
+if [[ ! -d "$rootdir/roms/nes" ]]; then
+    mkdir "$rootdir/roms/nes"
+fi
+if [[ ! -d "$rootdir/roms/megadrive" ]]; then
+    mkdir "$rootdir/roms/megadrive"
+fi
+if [[ ! -d "$rootdir/roms/gba" ]]; then
+    mkdir "$rootdir/roms/gba"
+fi
+if [[ ! -d "$rootdir/roms/mame" ]]; then
+    mkdir "$rootdir/roms/mame"
+fi
+if [[ ! -d "$rootdir/roms/doom" ]]; then
+    mkdir "$rootdir/roms/doom"
+fi
 
-mkdir "$rootdir/emulatorcores"
+if [[ ! -d "$rootdir/emulatorcores" ]]; then
+    mkdir "$rootdir/emulatorcores"
+fi
 
 # install RetroArch emulator
-echo "Installing RetroArch emulator"
-cd
+printMsg "Installing RetroArch emulator"
+if [[ -d "$rootdir/RetroArch-Rpi" ]]; then
+	rm -rf "$rootdir/RetroArch-Rpi"
+fi
 git clone git://github.com/Themaister/RetroArch.git "$rootdir/RetroArch-Rpi"
-cd "$rootdir/RetroArch-Rpi"
+pushd "$rootdir/RetroArch-Rpi"
 ./configure --disable-libpng
 make
 sudo make install
+popd
 
 # install SNES emulator core
-echo "Installing SNES core"
-cd
+printMsg "Installing SNES core"
+if [[ -d "$rootdir/emulatorcores/pocketsnes-libretro" ]]; then
+	rm -rf "$rootdir/emulatorcores/pocketsnes-libretro"
+fi
 git clone git://github.com/ToadKing/pocketsnes-libretro.git "$rootdir/emulatorcores/pocketsnes-libretro"
-cd "$rootdir/emulatorcores/pocketsnes-libretro"
+pushd "$rootdir/emulatorcores/pocketsnes-libretro"
 make
+popd
 
 # install NES emulator core
-echo "Installing NES core"
-cd
+printMsg "Installing NES core"
+if [[ -d "$rootdir/emulatorcores/fceu-next" ]]; then
+	rm -rf "$rootdir/emulatorcores/fceu-next"
+fi
 git clone git://github.com/libretro/fceu-next.git "$rootdir/emulatorcores/fceu-next"
-cd "$rootdir/emulatorcores/fceu-next"
+pushd "$rootdir/emulatorcores/fceu-next"
 make -f Makefile.libretro-fceumm
+popd
 
 # install Sega Mega Drive emulator core
-echo "Installing Mega Drive core"
-cd
+printMsg "Installing Mega Drive core"
+if [[ -d "$rootdir/emulatorcores/Genesis-Plus-GX" ]]; then
+	rm -rf "$rootdir/emulatorcores/Genesis-Plus-GX"
+fi
 git clone git://github.com/libretro/Genesis-Plus-GX.git "$rootdir/emulatorcores/Genesis-Plus-GX"
-cd "$rootdir/emulatorcores/Genesis-Plus-GX"
+pushd "$rootdir/emulatorcores/Genesis-Plus-GX"
 make -f Makefile.libretro 
+popd
 
 # install Gameboy emulator core
-echo "Installing Gameboy core"
-cd
+printMsg "Installing Gameboy core"
+if [[ -d "$rootdir/emulatorcores/gambatte-libretro/libgambatte" ]]; then
+	rm -rf "$rootdir/emulatorcores/gambatte-libretro/libgambatte"
+fi
 git clone git://github.com/libretro/gambatte-libretro.git "$rootdir/emulatorcores/gambatte-libretro"
-cd $rootdir/emulatorcores/gambatte-libretro/libgambatte
+pushd "$rootdir/emulatorcores/gambatte-libretro/libgambatte"
 make -f Makefile.libretro 
+popd
 
 # install MAME emulator core
-echo "Installing MAME core"
-cd
+printMsg "Installing MAME core"
+if [[ -d "$rootdir/emulatorcores/imame4all-libretro" ]]; then
+	rm -rf "$rootdir/emulatorcores/imame4all-libretro"
+fi
 git clone git://github.com/libretro/imame4all-libretro.git "$rootdir/emulatorcores/imame4all-libretro"
-cd "$rootdir/emulatorcores/imame4all-libretro"
+pushd "$rootdir/emulatorcores/imame4all-libretro"
 make -f makefile.libretro ARM=1
+popd
 
 # install Doom WADs emulator core
-echo "Installing Doom core"
-cd 
+printMsg "Installing Doom core"
+if [[ -d "$rootdir/emulatorcores/libretro-prboom" ]]; then
+	rm -rf "$rootdir/emulatorcores/libretro-prboom"
+fi
 git clone git://github.com/libretro/libretro-prboom.git "$rootdir/emulatorcores/libretro-prboom"
-cd "$rootdir/emulatorcores/libretro-prboom"
+pushd "$rootdir/emulatorcores/libretro-prboom"
 make
-cd
+popd
 
 # install BCM library to enable GPIO access by SNESDev-RPi
-echo "Installing BCM2835 library"
-cd
+printMsg "Installing BCM2835 library"
+pushd $rootdir
 wget http://www.open.com.au/mikem/bcm2835/bcm2835-1.3.tar.gz
 tar -zxvf bcm2835-1.3.tar.gz
 cd bcm2835-1.3
 ./configure
 make
 sudo make install
-cd
+cd ..
 rm bcm2835-1.3.tar.gz
 rm -rf bcm2835-1.3
+popd 
 
 # install SNESDev as GPIO interface for SNES controllers
-cd
+printMsg "Installing SNESDev as GPIO interface for SNES controllers"
+if [[ -d "$rootdir/SNESDev-Rpi" ]]; then
+	rm -rf "$rootdir/SNESDev-Rpi"
+fi
 git clone git://github.com/petrockblog/SNESDev-RPi.git "$rootdir/SNESDev-Rpi"
-cd "$rootdir/SNESDev-Rpi"
+pushd "$rootdir/SNESDev-Rpi"
 make clean
 make
+popd
 
 # install EmulationStation as graphical front end for the emulators
-cd
+printMsg "Installing EmulationStation as graphical front end"
+if [[ -d "$rootdir/EmulationStation" ]]; then
+	rm -rf "$rootdir/EmulationStation"
+fi
 git clone git://github.com/Aloshi/EmulationStation.git "$rootdir/EmulationStation"
-cd "$rootdir/EmulationStation"
+pushd "$rootdir/EmulationStation"
 make clean
 make
-echo -e "Generating symbolic link to /home/pi/RetroPie/EmulationStation/emulationstation\n -->>\n /usr/bin/emulationstation"
-sudo ln -s /home/pi/RetroPie/EmulationStation/emulationstation /usr/bin/emulationstation
+printMsg "Generating symbolic link to $rootdir/EmulationStation/emulationstation/emulationstation\n -->>\n /usr/bin/emulationstation"
+sudo ln -s "$rootdir/EmulationStation/emulationstation" /usr/bin/emulationstation
+popd
 
 # generate EmulationStation configuration
-echo -e "Generating configuration file ~/.es_systems.cfg for EmulationStation"
+printMsg "Generating configuration file ~/.es_systems.cfg for EmulationStation"
 cat > ~/.es_systems.cfg << EOF
 NAME=MAME
 PATH=~/RetroPie/roms/mame
@@ -197,6 +270,5 @@ EXTENSION=.gba
 COMMAND=retroarch -L ~/RetroPie/emulatorcores/gambatte-libretro/libgambatte/libretro.so %ROM%
 EOF
 
-cd
 echo -e "Finished compiling and installation.\nStart the front end with .emulationstation\nHave fun :-)"
 echo -e "You now have to copy roms to the roms folder:\nSNES to roms/snes,\nNES to roms/nes\nGameboy Advance to roms/gba\nMAME to roms/mame\nMega Drive to roms/megadrive"
