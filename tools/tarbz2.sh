33611b50212302477ae0caba27938600ad1ca93e
petrockblog <florian@petrockbloggit>
2012-08-04 17:40:11 +0200

    Added shell script for creating tar.bz2 archives on MacOS without hidden utility files

---
+++ tools/tarbz2.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
+export COPYFILE_DISABLE=true
+tar -c --exclude='._*' --exclude='.svn' --exclude='.DS_Store' --exclude='*.bak' --exclude='*~' -vjf "$@"
