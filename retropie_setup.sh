c7054356c00f0b23b56077b53fa60de2675d13c3
petrockblog <florian@petrockbloggit>
2012-10-06 19:22:05 +0200

    Added information about using ScummVM

--- retropie_setup.sh
+++ retropie_setup.sh
@@ -519,7 +519,7 @@ function install_scummvm()
     if [[ $? -gt 0 ]]; then
         __ERRMSGS="$__ERRMSGS Could not successfully install ScummVM."
     else
-        __INFMSGS="$__INFMSGS ScummVM has successfully been installed. You can start the ScummVM GUI by typing 'scummvm' in the console. Copy your Scumm games into the directory '$rootdir/roms/scummvm'."
+        __INFMSGS="$__INFMSGS ScummVM has successfully been installed. You can start the ScummVM GUI by typing 'scummvm' in the console. Copy your Scumm games into the directory '$rootdir/roms/scummvm'. When you get a blank screen after running scumm for the first time, press CTRL-q. You should not get a blank screen with further runs of scummvm."
     fi 
 }
 
