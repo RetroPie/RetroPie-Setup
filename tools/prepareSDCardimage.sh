2f030d7f2ae0fea806d05958e7bb3a02cfef894c
petrockblog <florian@petrockbloggit>
2013-02-19 09:13:44 +0100

    Updated packages to be removed for SD card image

--- tools/prepareSDCardimage.sh
+++ tools/prepareSDCardimage.sh
@@ -1,4 +1,5 @@
 # Here follows a script for preparing the downloadable SD card image.
 # Inspired by article at http://www.cnx-software.com/2012/07/31/84-mb-minimal-raspbian-armhf-image-for-raspberry-pi/
 
-apt-get purge scratch squeak xpdf dillo midori netsurf xarchiver lxterminal gaclulator lxde
+apt-get purge scratch xpdf dillo midori netsurf xarchiver lxterminal lxde lxde-common lxde-icon-theme omxplayer
+apt-get autoremove
