b162966294c046bacf53c0677437e01a6b0979ce
Florian <info@petrockblock.com>
2013-11-12 07:12:18 +0100

    Added timeout disabling function to binaries-based installation

--- tools/prepareSDCardimage.sh
+++ tools/prepareSDCardimage.sh
@@ -16,14 +16,13 @@ rm ~/.bash_history
 # check file system for errors
 # e2fsck -f -y -v /dev/sdaX
 
-# resize partition on SD card to 3200 MB with gparted
-# resize2fs /dev/sdaX 3276800K
+# resize partition on SD card to 3400 MB with gparted
 
 # re-check file system for errors
 # e2fsck -f -y -v /dev/sdaX
 
 # read and compress image of SD card with
-#   dd if=/dev/disk2 bs=2m of=RetroPieImage_verX.img count=1650
+#   dd if=/dev/disk2 bs=2m of=RetroPieImage_verX.img count=1750
 #   zip -9 RetroPieImage_verX.zip RetroPieImage_verX.img
 
 # create SHA1 hash with 
