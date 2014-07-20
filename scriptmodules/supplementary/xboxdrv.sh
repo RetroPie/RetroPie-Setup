e53ca18bb8af645cd20fcea090c36046e4e1267f
Jools Wills <buzz@exotica.org.uk>
2014-07-20 21:49:15 +0100

    fixed xboxdrv function naming

--- scriptmodules/supplementary/xboxdrv.sh
+++ scriptmodules/supplementary/xboxdrv.sh
@@ -2,7 +2,7 @@ rp_module_id="xboxdrv"
 rp_module_desc="Install XBox contr. 360 driver"
 rp_module_menus="3+"
 
-function set_install_xboxdrv() {
+function install_xboxdrv() {
     rps_checkNeededPackages xboxdrv
     if [[ -z `cat /etc/rc.local | grep "xboxdrv"` ]]; then
         sed -i -e '13,$ s|exit 0|xboxdrv --daemon --id 0 --led 2 --deadzone 4000 --silent --trigger-as-button --next-controller --id 1 --led 3 --deadzone 4000 --silent --trigger-as-button --dbus disabled --detach-kernel-driver \&\nexit 0|g' /etc/rc.local
