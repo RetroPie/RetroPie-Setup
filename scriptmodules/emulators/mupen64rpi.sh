78d7c7f8b7a9771b49f8ba3317bb071aaa83b5ff
gizmo98 <atz.SE@gmx.de>
2014-07-31 19:54:17 +0200

    Replace mupen64plus-rpi with mupen64plus

--- scriptmodules/emulators/mupen64rpi.sh
+++ scriptmodules/emulators/mupen64rpi.sh
@@ -3,16 +3,27 @@ rp_module_desc="N64 emulator MUPEN64Plus-RPi"
 rp_module_menus="2+"
 
 function sources_mupen64rpi() {
-    gitPullOrClone "$rootdir/emulators/mupen64plus-rpi" https://github.com/ricrpi/mupen64plus-rpi
+    gitPullOrClone "$rootdir/emulators/mupen64plus" https://github.com/ricrpi/mupen64plus
 }
 
 function build_mupen64rpi() {
-    pushd "$rootdir/emulators/mupen64plus-rpi"
-    ./m64p_build.sh
-    if [[ ! -f "$rootdir/emulators/mupen64plus-rpi/test/mupen64plus" ]]; then
+    dphys-swapfile swapoff
+    echo "CONF_SWAPSIZE=512" > /etc/dphys-swapfile
+    dphys-swapfile setup
+    dphys-swapfile swapon
+
+    pushd "$rootdir/emulators/mupen64plus"
+    ./build.sh
+    ./install.sh
+    if [[ ! -f "$rootdir/emulators/mupen64plus" ]]; then
         __ERRMSGS="$__ERRMSGS Could not successfully compile Mupen 64 Plus RPi."
     fi
     popd
+
+    dphys-swapfile swapoff
+    echo "CONF_SWAPSIZE=99" > /etc/dphys-swapfile
+    dphys-swapfile setup
+    dphys-swapfile swapon
 }
 
 function configure_mupen64rpi() {
