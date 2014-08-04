fe268cc36e1856469adc868a96a1f3a7e42f56ff
Florian <info@petrockblock.com>
2014-08-04 20:57:11 +0200

    Updated installation of openMSX

--- scriptmodules/emulators/openmsx.sh
+++ scriptmodules/emulators/openmsx.sh
@@ -7,21 +7,35 @@ function depen_openmsx() {
 }
 
 function sources_openmsx() {
-    wget http://downloads.sourceforge.net/project/openmsx/openmsx/0.10.0/openmsx-0.10.0.tar.gz
+    wget http://downloads.petrockblock.com/retropiearchives/openmsx-0.10.0.tar.gz
     tar xvfz openmsx-0.10.0.tar.gz  -C "$rootdir/emulators"
     rm openmsx-0.10.0.tar.gz
 }
 
 function build_openmsx() {
+    dphys-swapfile swapoff
+    echo "CONF_SWAPSIZE=512" > /etc/dphys-swapfile
+    dphys-swapfile setup
+    dphys-swapfile swapon
+
     pushd "$rootdir/emulators/openmsx-0.10.0"
     ./configure
     make
     popd
+
+    dphys-swapfile swapoff
+    echo "CONF_SWAPSIZE=99" > /etc/dphys-swapfile
+    dphys-swapfile setup
+    dphys-swapfile swapon    
 }
 
 function configure_openmsx() {
     mkdir -p $romdir/msx
+    wget http://downloads.petrockblock.com/retropiearchives/openmsxroms.zip
+    mkdir -p "$home/.openMSX/share/systemroms/"
+    chown -R $user:$user "$home/.openMSX/share/systemroms/"
+    unzip openmsxroms.zip "$home/.openMSX/share/systemroms/"
+    rm openmsxroms.zip
 
-    setESSystem "MSX" "msx" "~/RetroPie/roms/msx" ".rom .ROM" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"$rootdir/emulators/openmsx-0.10.0/derived/arm-linux-opt/bin/openmsx %BASENAME%\"" "" "msx"
-
+    setESSystem "MSX / MSX2" "msx" "~/RetroPie/roms/msx" ".rom .ROM .mx2 .MX2 .mx1 .MX1" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"$rootdir/emulators/openmsx-0.10.0/derived/arm-linux-opt/bin/openmsx -cart %ROM%\"" "" "msx"
 }
