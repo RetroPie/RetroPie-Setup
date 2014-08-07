a17519895cec4c219bef90e72c22d08ea36d43a6
Jools Wills <buzz@exotica.org.uk>
2014-08-08 00:40:44 +0100

    fixed building of jzint #426 remove older folder if it exists use folder name without version number correct makefile for gcc 4.7 remove rom building from submakefiles to account for missing examples folder

--- scriptmodules/emulators/jzint.sh
+++ scriptmodules/emulators/jzint.sh
@@ -6,16 +6,24 @@ function sources_jzint() {
     # wget 'http://spatula-city.org/~im14u2c/intv/dl/jzintv-1.0-beta4-src.zip' -O jzintv.zip
     wget http://downloads.petrockblock.com/retropiearchives/jzintv-svn.zip -O jzintv.zip
     mkdir -p "$rootdir/emulators"
+    rmDirExists "$rootdir/emulators/jzintv"
     unzip -n jzintv.zip -d "$rootdir/emulators/"
     rm jzintv.zip
+    # use our default gcc-4.7
+    sed -i "s/-4\.8\.0/-4.7/g" "$rootdir/emulators/jzintv/src/Makefile"
+    # don't build event_diag.rom/emu_ver.rom/joy_diag.rom/jlp_test.bin due to missing example/library files from zip
+    sed -i '/^PROGS/,$d' \
+        "$rootdir/emulators/jzintv/src/"{event,joy,jlp,util}/subMakefile \
+        "$rootdir/emulators/jzintv/src/util/subMakefile" \
+        "$rootdir/emulators/jzintv/src/joy/subMakefile"
 }
 
 function build_jzint() {
-    pushd "$rootdir/emulators/jzintv-1.0-beta4/src/"
-    mkdir "$rootdir/emulators/jzintv-1.0-beta4/bin"
+    pushd "$rootdir/emulators/jzintv/src/"
+    mkdir -p "$rootdir/emulators/jzintv/bin"
     make clean
     make OPT_FLAGS="-O3 -fomit-frame-pointer -fprefetch-loop-arrays -march=armv6 -mfloat-abi=hard -mfpu=vfp"
-    if [[ ! -f "$rootdir/emulators/jzintv-1.0-beta4/bin/jzintv" ]]; then
+    if [[ ! -f "$rootdir/emulators/jzintv/bin/jzintv" ]]; then
         __ERRMSGS="$__ERRMSGS Could not successfully compile jzintv."
     else
         __INFMSGS="$__INFMSGS You need to copy Intellivision BIOS files to the folder '/usr/local/share/jzintv/rom'."
@@ -26,5 +34,5 @@ function build_jzint() {
 function configure_jzint() {
     mkdir -p "$romdir/intellivision"
 
-    setESSystem "Intellivision" "intellivision" "~/RetroPie/roms/intellivision" ".int .INT .bin .BIN" "$rootdir/emulators/jzintv-1.0-beta4/bin/jzintv -z1 -f1 -q %ROM%" "intellivision" ""
+    setESSystem "Intellivision" "intellivision" "~/RetroPie/roms/intellivision" ".int .INT .bin .BIN" "$rootdir/emulators/jzintv/bin/jzintv -z1 -f1 -q %ROM%" "intellivision" ""
 }
