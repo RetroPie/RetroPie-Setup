fcc83103766677e179e9a31bce6c4419fb2df033
Jools Wills <buzz@exotica.org.uk>
2014-08-07 23:10:45 +0100

    fixed incorrect https link to http for osmose. #424

--- scriptmodules/emulators/osmose.sh
+++ scriptmodules/emulators/osmose.sh
@@ -3,7 +3,7 @@ rp_module_desc="Gamegear emulator Osmose"
 rp_module_menus="2+"
 
 function sources_osmose() {
-    wget 'https://downloads.petrockblock.com/retropiearchives/osmose-0.8.1%2Brpi20121122.tar.bz2?dl=1' -O osmose.tar.bz2
+    wget 'http://downloads.petrockblock.com/retropiearchives/osmose-0.8.1%2Brpi20121122.tar.bz2?dl=1' -O osmose.tar.bz2
     mkdir -p "$rootdir/emulators"
     tar -jxvf osmose.tar.bz2 -C "$rootdir/emulators/"
     rm osmose.tar.bz2
