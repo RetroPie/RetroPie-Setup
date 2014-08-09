8d6be196868a745e246f8db9e7f03380962dae3c
Jools Wills <buzz@exotica.org.uk>
2014-08-09 16:51:27 +0100

    added missing dependencies for snes9x - #431

--- scriptmodules/emulators/snes9x.sh
+++ scriptmodules/emulators/snes9x.sh
@@ -3,7 +3,7 @@ rp_module_desc="SNES emulator SNES9X-RPi"
 rp_module_menus="2+"
 
 function depen_snes9x() {
-    rps_checkNeededPackages libsdl1.2-dev libboost-thread-dev
+    rps_checkNeededPackages libsdl1.2-dev libboost-thread-dev libboost-system-dev libsdl-ttf2.0-dev
 }
 
 function sources_snes9x() {
