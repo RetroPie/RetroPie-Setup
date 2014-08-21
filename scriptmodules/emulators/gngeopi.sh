6b59876b97963a34ca66d0b21f9d05f9e1109e31
Florian <info@petrockblock.com>
2014-08-21 21:59:02 +0200

    Fixed GNGeoPi module installation (#448), updated Linapple module (#450)

--- scriptmodules/emulators/gngeopi.sh
+++ scriptmodules/emulators/gngeopi.sh
@@ -9,7 +9,7 @@ function sources_gngeopi() {
 function build_gngeopi() {
     pushd "$rootdir/emulators/gngeo-pi-0.85/gngeo"
     chmod +x configure
-    ./configure --disable-i386asm --prefix="$rootdir/emulators/gngeo-pi-0.85/installdir"
+    ./configure --host=arm-linux --target=arm-linux --disable-i386asm --prefix="$rootdir/emulators/gngeo-pi-0.85/installdir"
     make
     popd
 }
@@ -17,6 +17,8 @@ function build_gngeopi() {
 function install_gngeopi() {
     pushd "$rootdir/emulators/gngeo-pi-0.85/gngeo"
     make install
+    mv $rootdir/emulators/gngeo-pi-0.85/installdir/bin/arm-linux-gngeo $rootdir/emulators/gngeo-pi-0.85/installdir/bin/gngeo
+    mv $rootdir/emulators/gngeo-pi-0.85/installdir/share/man/man1/arm-linux-gngeo.1 $rootdir/emulators/gngeo-pi-0.85/installdir/share/man/man1/gngeo.1    
     if [[ ! -f "$rootdir/emulators/gngeo-pi-0.85/installdir/bin/gngeo" ]]; then
         __ERRMSGS="$__ERRMSGS Could not successfully compile GnGeo-Pi emulator."
     fi
