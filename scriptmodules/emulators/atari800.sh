150443fdd8c754ab9b3e0e5a604d3cf8bd672225
Jools Wills <buzz@exotica.org.uk>
2014-10-27 01:02:26 +0000

    rework atari800 to split source/binary and use new variables/parameters

--- scriptmodules/emulators/atari800.sh
+++ scriptmodules/emulators/atari800.sh
@@ -3,32 +3,30 @@ rp_module_desc="Atari 800 emulator"
 rp_module_menus="2+"
 
 function sources_atari800() {
-    wget -O atari800-3.0.0.tar.gz http://downloads.petrockblock.com/retropiearchives/atari800-3.0.0.tar.gz
-    rmDirExists "$rootdir/emulators/atari800-3.0.0"
-    mkdir -p "$rootdir/emulators/"
-    tar xvfz atari800-3.0.0.tar.gz -C "$rootdir/emulators/"
-    rm atari800-3.0.0.tar.gz
+    rmDirExists "$builddir/$1"
+    mkdir -p "$builddir/$1"
+    wget -O- "http://downloads.petrockblock.com/retropiearchives/atari800-3.0.0.tar.gz" | tar -xvz --strip-components=1 -C "$builddir/$1"
 }
 
 function build_atari800() {
-    pushd "$rootdir/emulators/atari800-3.0.0/src"
-    mkdir -p "$rootdir/emulators/atari800-3.0.0/installdir"
-    ./configure --prefix="$rootdir/emulators/atari800-3.0.0/installdir"
+    pushd "$builddir/$1/src"
+    ./configure --prefix="$emudir/$1"
     make
+    if [[ ! -f "atari800" ]]; then
+        __ERRMSGS="$__ERRMSGS Could not successfully compile Atari 800 emulator."
+    fi
     popd
 }
 
 function install_atari800() {
-    pushd "$rootdir/emulators/atari800-3.0.0/src"
+    mkdir -p "$emudir/$1"
+    pushd "$builddir/$1/src"
     make install
-    if [[ ! -f "$rootdir/emulators/atari800-3.0.0/installdir/bin/atari800" ]]; then
-        __ERRMSGS="$__ERRMSGS Could not successfully compile Atari 800 emulator."
-    fi
     popd
 }
 
 function configure_atari800() {
     mkdir -p "$romdir/atari800"
 
-    setESSystem "Atari 800" "atari800" "~/RetroPie/roms/atari800" ".xex .XEX" "$rootdir/supplementary/runcommand/runcommand.sh 1 \"$rootdir/emulators/atari800-3.0.0/installdir/bin/atari800 %ROM%\"" "atari800" "atari800"
+    setESSystem "Atari 800" "atari800" "~/RetroPie/roms/atari800" ".xex .XEX" "$rootdir/supplementary/runcommand/runcommand.sh 1 \"$emudir/$1/bin/atari800 %ROM%\"" "atari800" "atari800"
 }
