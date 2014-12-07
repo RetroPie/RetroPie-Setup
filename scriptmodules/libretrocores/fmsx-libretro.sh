029c140d8ae1a569ccf6b66c3fcabb423b5e7fd3
gizmo98 <atz.SE@gmx.de>
2014-12-07 13:22:35 +0100

    fmsx-libretro: copy ROMs to BIOS folder

--- scriptmodules/libretrocores/fmsx-libretro.sh
+++ scriptmodules/libretrocores/fmsx-libretro.sh
@@ -19,6 +19,7 @@ function build_fmsx-libretro() {
 function configure_fmsx-libretro() {
     mkdir -p $romdir/msx
     ensureSystemretroconfig "msx"
+    cp -a $rootdir/emulatorcores/fmsx-libretro/fMSX/ROMs/. $home/RetroPie/BIOS/
     rps_retronet_prepareConfig
     setESSystem "MSX" "msx" "~/RetroPie/roms/msx" ".rom .ROM .mx1 .MX1 .mx2 .MX2 .col .COL .dsk .DSK" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"$rootdir/emulators/RetroArch/installdir/bin/retroarch -L `find $rootdir/emulatorcores/fmsx-libretro/ -name \"*libretro*.so\" | head -1` --config $rootdir/configs/all/retroarch.cfg --appendconfig $rootdir/configs/msx/retroarch.cfg $__tmpnetplaymode$__tmpnetplayhostip_cfile$__tmpnetplayport$__tmpnetplayframes %ROM%\"" "msx" "msx"
 }
