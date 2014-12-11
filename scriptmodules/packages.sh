407d27fc584e444e71f43072fe7f9b17921e4807
Jools Wills <buzz@exotica.org.uk>
2014-12-11 14:56:28 +0000

    create the install destination directory before running install

--- scriptmodules/packages.sh
+++ scriptmodules/packages.sh
@@ -111,7 +111,7 @@ function rp_callModule() {
             ;;
         install)
             desc="Installing"
-            mkdir -p "$builddir/$mod_id"
+            mkdir -p "$emudir/$mod_id"
             pushd "$builddir/$mod_id"
             ;;
         configure)
