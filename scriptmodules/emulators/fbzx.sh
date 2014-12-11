8e57f301028502d74f25c81fea51d03bc85ad640
Jools Wills <buzz@exotica.org.uk>
2014-12-11 17:35:09 +0000

    rework fbzx for new build location / system (including list of files to install)

--- scriptmodules/emulators/fbzx.sh
+++ scriptmodules/emulators/fbzx.sh
@@ -3,24 +3,37 @@ rp_module_desc="ZXSpectrum emulator FBZX"
 rp_module_menus="2+"
 
 function sources_fbzx() {
-    rmDirExists "$rootdir/emulators/fbzx-2.10.0"
-    wget -O fbzx-2.10.0.tar.bz2 http://downloads.petrockblock.com/retropiearchives/fbzx-2.10.0.tar.bz2
-    mkdir -p "$rootdir/emulators"
-    tar xvfj fbzx-2.10.0.tar.bz2  -C "$rootdir/emulators"
-    rm fbzx-2.10.0.tar.bz2
+    wget -O- -q http://downloads.petrockblock.com/retropiearchives/fbzx-2.10.0.tar.bz2 | tar -xvj --strip-components=1 
 }
 
 function build_fbzx() {
-    pushd "$rootdir/emulators/fbzx-2.10.0"
+    make clean
     make
-    if [[ ! -f "$rootdir/emulators/fbzx-2.10.0/fbzx" ]]; then
-        __ERRMSGS="$__ERRMSGS Could not successfully compile FBZX."
-    fi
-    popd
+    require="$builddir/$1/fbzx"
+}
+
+function install_fbzx() {
+    files=(
+        'AMSTRAD'
+        'CAPABILITIES'
+        'COPYING'
+        'FAQ'
+        'fbzx'
+        'fbzx.desktop'
+        'fbzx.svg'
+        'INSTALL'
+        'keymap.bmp'
+        'PORTING'
+        'README'
+        'README.TZX'
+        'spectrum-roms'
+        'TODO'
+        'VERSIONS'
+    )
 }
 
 function configure_fbzx() {
     mkdir -p $romdir/zxspectrum-fbzx
 
-    setESSystem "ZX Spectrum" "zxspectrum-fbzx" "RetroPie/roms/zxspectrum-fbzx" "z80 .Z80 .ipf .IPF" "$rootdir/emulators/fbzx-2.10.0/fbzx %ROM%" "zxspectrum" "zxspectrum"
+    setESSystem "ZX Spectrum" "zxspectrum-fbzx" "RetroPie/roms/zxspectrum-fbzx" "z80 .Z80 .ipf .IPF" "$emudir/$1/fbzx %ROM%" "zxspectrum" "zxspectrum"
 }
