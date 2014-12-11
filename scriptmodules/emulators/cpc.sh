685f09a80a2aa383232fc01f83c7e1da7f7d0485
Jools Wills <buzz@exotica.org.uk>
2014-12-11 14:58:56 +0000

    reworked cpc emulator for new build location / system

--- scriptmodules/emulators/cpc.sh
+++ scriptmodules/emulators/cpc.sh
@@ -3,27 +3,24 @@ rp_module_desc="Amstrad CPC emulator"
 rp_module_menus="2+"
 
 function sources_cpc() {
-    wget http://downloads.petrockblock.com/retropiearchives/cpc4rpi-1.1_src.tar.gz
-    mkdir -p "$rootdir/emulators"
-    rmDirExists "$rootdir/emulators/cpc4rpi-1.1"
-    tar xvfz cpc4rpi-1.1_src.tar.gz -C "$rootdir/emulators/"
-    rm cpc4rpi-1.1_src.tar.gz
+    wget -O- -q http://downloads.petrockblock.com/retropiearchives/cpc4rpi-1.1_src.tar.gz | tar -xvz --strip-components=1
 }
 
 function build_cpc() {
-    pushd "$rootdir/emulators/cpc4rpi-1.1"
+    make clean
     sed -i 's|LIBS = -L/usr/lib/arm-linux-gnueabihf -lz -lts -L/opt/vc/lib -lGLESv2 -lEGL|LIBS = -L/usr/lib/arm-linux-gnueabihf -lX11 -lz -lts -L/opt/vc/lib -lSDL -lpthread -ldl -lGLESv2 -lEGL|g' makefile
     sed -i 's|$(CC) $(CFLAGS) $(IPATHS) -o cpc4rpi cpc4rpi.cpp crtc.o fdc.o psg.o tape.o z80.o /root/Raspbian/Libs/libSDL.a /root/Raspbian/Libs/libnofun.a $(LIBS)|$(CC) $(CFLAGS) $(IPATHS) -o cpc4rpi cpc4rpi.cpp crtc.o fdc.o psg.o tape.o z80.o   $(LIBS)|g' makefile
     make RELEASE=TRUE
-    if [[ ! -f "$rootdir/emulators/cpc4rpi-1.1/cpc4rpi" ]]; then
-        __ERRMSGS="$__ERRMSGS Could not successfully compile Amstrad CPC emulator CPC4Rpi."
-    fi
-    popd
+    require="$builddir/$1/cpc4rpi"
+}
+
+function install_cpc() {
+    cp -R "$builddir/$1/"{cpc4rpi,*.txt} "$emudir/$1/"
+    require="$emudir/$1/cpc4rpi"
 }
 
 function configure_cpc() {
     mkdir -p "$romdir/amstradcpc"
 
-    setESSystem "Amstrad CPC" "amstradcpc" "~/RetroPie/roms/amstradcpc" ".cpc .CPC .dsk .DSK" "$rootdir/emulators/cpc4rpi-1.1/cpc4rpi %ROM%" "amstradcpc" ""
-
+    setESSystem "Amstrad CPC" "amstradcpc" "~/RetroPie/roms/amstradcpc" ".cpc .CPC .dsk .DSK" "$rootdir/emulators/cpc4rpi/cpc4rpi %ROM%" "amstradcpc" ""
 }
