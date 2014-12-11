14d791adb5225819f2d4f58d0142fc7ec73e904a
Jools Wills <buzz@exotica.org.uk>
2014-12-11 19:57:43 +0000

    reworked mame4all for new build location / system

--- scriptmodules/emulators/mame4all.sh
+++ scriptmodules/emulators/mame4all.sh
@@ -7,28 +7,37 @@ function depends_mame4all() {
 }
 
 function sources_mame4all() {
-    gitPullOrClone "$rootdir/emulators/mame4all-pi" https://code.google.com/p/mame4all-pi/ NS
-    sed -i "s/@mkdir/@mkdir -p/g" "$rootdir/emulators/mame4all-pi/Makefile"
+    gitPullOrClone "$builddir/$1" https://code.google.com/p/mame4all-pi/ NS
+    sed -i "s/@mkdir/@mkdir -p/g" "Makefile"
+    sed -i 's/-lglib-2.0$/-lglib-2.0 -lrt -lasound/g' Makefile
+    sed -i 's/armv6 /armv6j /g' Makefile
 }
 
 function build_mame4all() {
-    pushd "$rootdir/emulators/mame4all-pi"
-    sed -i 's/-lglib-2.0$/-lglib-2.0 -lrt -lasound/g' Makefile
-    sed -i 's/armv6 /armv6j /g' Makefile
     make clean
     make
-    if [[ ! -f "$rootdir/emulators/mame4all-pi/mame" ]]; then
-        __ERRMSGS="$__ERRMSGS Could not successfully mame4all-pi emulator."
-    fi
-    popd
+    require="$emudir/$1/mame"
+}
+
+function install_mame4all() {
+    files=(
+        'cheat.dat'
+        'clrmame.dat'
+        'folders'
+        'hiscore.dat'
+        'mame'
+        'mame.cfg'
+        'readme.txt'
+        'skins'
+    )
 }
 
 function configure_mame4all() {
     mkdir -p "$romdir/mame"
-    mkdir -p "$rootdir/emulators/mame4all-pi/"{cfg,hi,sta,roms}
-    chown -R $user:$user "$rootdir/emulators/mame4all-pi"
-    ensureKeyValueShort "samplerate" "22050" "$rootdir/emulators/mame4all-pi/mame.cfg"
-    ensureKeyValueShort "rompath" "$romdir/mame" "$rootdir/emulators/mame4all-pi/mame.cfg"
+    mkdir -p "$emudir/$1/"{cfg,hi,sta,roms}
+    chown -R $user:$user "$emudir/$1"
+    ensureKeyValueShort "samplerate" "22050" "$emudir/$1/mame.cfg"
+    ensureKeyValueShort "rompath" "$romdir/mame" "$emudir/$1/mame.cfg"
 
-    setESSystem "MAME" "mame" "~/RetroPie/roms/mame" ".zip .ZIP" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"$rootdir/emulators/mame4all-pi/mame %BASENAME%\"" "arcade" "mame"
+    setESSystem "MAME" "mame" "~/RetroPie/roms/mame" ".zip .ZIP" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"$emudir/$1/mame %BASENAME%\"" "arcade" "mame"
 }
