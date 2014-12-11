17f9f965aad333a9d835eb647c176d4158c16d29
Jools Wills <buzz@exotica.org.uk>
2014-12-11 20:25:36 +0000

    reworked mupen64rpi for new build location / system

--- scriptmodules/emulators/mupen64rpi.sh
+++ scriptmodules/emulators/mupen64rpi.sh
@@ -3,27 +3,25 @@ rp_module_desc="N64 emulator MUPEN64Plus-RPi"
 rp_module_menus="4+"
 
 function sources_mupen64rpi() {
-    gitPullOrClone "$rootdir/emulators/mupen64plus" https://github.com/ricrpi/mupen64plus
+    gitPullOrClone "$builddir/$1" https://github.com/ricrpi/mupen64plus
 }
 
 function build_mupen64rpi() {
     rpSwap on 256 400
-
-    pushd "$rootdir/emulators/mupen64plus"
     ./build.sh
-    ./install.sh
-    if [[ ! -f "$rootdir/emulators/mupen64plus" ]]; then
-        __ERRMSGS="$__ERRMSGS Could not successfully compile Mupen 64 Plus RPi."
-    fi
-    popd
-    
-    # to solve startup problems delete old config file 
-    rm /home/pi/.config/mupen64plus/mupen64plus.cfg
-
     rpSwap off
+
+    require="$builddir/$1/mupen64plus"
+}
+
+function install_mupen64rpi() {
+    ./install.sh
 }
 
 function configure_mupen64rpi() {
+    # to solve startup problems delete old config file 
+    rm /home/$user/.config/mupen64plus/mupen64plus.cfg
+
     mkdir -p "$romdir/n64"
 
     setESSystem "Nintendo 64" "n64" "~/RetroPie/roms/n64" ".z64 .Z64 .n64 .N64 .v64 .V64" "$rootdir/supplementary/runcommand/runcommand.sh 1 \"mupen64plus %ROM%\"" "n64" "n64"
