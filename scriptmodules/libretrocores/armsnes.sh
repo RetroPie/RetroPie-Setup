3749140a5a2ab052cfc33cbffbd0b659925ac3c2
Jools Wills <buzz@exotica.org.uk>
2014-12-12 03:29:55 +0000

    reworked armsnes for new build location / system

--- scriptmodules/libretrocores/armsnes.sh
+++ scriptmodules/libretrocores/armsnes.sh
@@ -3,27 +3,27 @@ rp_module_desc="SNES LibretroCore ARMSNES"
 rp_module_menus="4+"
 
 function sources_armsnes() {
-    gitPullOrClone "$rootdir/emulatorcores/armsnes-libretro" git://github.com/rmaz/ARMSNES-libretro
-    pushd "$rootdir/emulatorcores/armsnes-libretro"
-    patch -N -i $scriptdir/supplementary/pocketsnesmultip.patch $rootdir/emulatorcores/armsnes-libretro/src/ppu.cpp
-    popd
+    gitPullOrClone "$md_build" git://github.com/rmaz/ARMSNES-libretro
+    patch -N -i $scriptdir/supplementary/pocketsnesmultip.patch src/ppu.cpp
 }
 
 function build_armsnes() {
-    pushd "$rootdir/emulatorcores/armsnes-libretro"
     make clean
     make
-    if [[ -z `find $rootdir/emulatorcores/armsnes-libretro/ -name "*libretro*.so"` ]]; then
-        __ERRMSGS="$__ERRMSGS Could not successfully compile SNES core."
-    fi
-    popd
+    md_ret_require="libpocketsnes.so"
+}
+
+function install_armsnes() {
+    md_ret_files=(
+        'libpocketsnes.so'
+    )
 }
 
 function configure_armsnes() {
     mkdir -p $romdir/snes
 
     rps_retronet_prepareConfig
-    setESSystem "Super Nintendo" "snes" "~/RetroPie/roms/snes" ".smc .sfc .fig .swc .SMC .SFC .FIG .SWC" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"$emudir/$1/bin/retroarch -L `find $rootdir/emulatorcores/armsnes-libretro/ -name \"libpocketsnes.so\" | head -1` --config $rootdir/configs/all/retroarch.cfg --appendconfig $rootdir/configs/snes/retroarch.cfg $__tmpnetplaymode$__tmpnetplayhostip_cfile $__tmpnetplayport$__tmpnetplayframes %ROM%\"" "snes" "snes"
-    # <!-- alternatively: <command>$rootdir/emulators/snes9x-rpi/snes9x %ROM%</command> -->
-    # <!-- alternatively: <command>$rootdir/emulators/pisnes/snes9x %ROM%</command> -->
+    setESSystem "Super Nintendo" "snes" "~/RetroPie/roms/snes" ".smc .sfc .fig .swc .SMC .SFC .FIG .SWC" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"$emudir/$1/bin/retroarch -L $md_inst/libpocketsnes.so --config $rootdir/configs/all/retroarch.cfg --appendconfig $rootdir/configs/snes/retroarch.cfg $__tmpnetplaymode$__tmpnetplayhostip_cfile $__tmpnetplayport$__tmpnetplayframes %ROM%\"" "snes" "snes"
+    # <!-- alternatively: <command>$emudir/snes9x-rpi/snes9x %ROM%</command> -->
+    # <!-- alternatively: <command>$emudir/pisnes/snes9x %ROM%</command> -->
 }
