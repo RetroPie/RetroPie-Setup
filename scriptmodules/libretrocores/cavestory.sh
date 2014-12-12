ef1e2c3d716b019572c56f57ff70f5a47312f760
Jools Wills <buzz@exotica.org.uk>
2014-12-12 03:53:08 +0000

    reworked cavestory for new build location / system - note includes no datafiles currently as they were deleted from the upstream repository

--- scriptmodules/libretrocores/cavestory.sh
+++ scriptmodules/libretrocores/cavestory.sh
@@ -3,28 +3,29 @@ rp_module_desc="Cave Story LibretroCore"
 rp_module_menus="2+"
 
 function sources_cavestory() {
-    gitPullOrClone "$rootdir/emulatorcores/nxengine-libretro" git://github.com/libretro/nxengine-libretro.git
+    gitPullOrClone "$md_build" git://github.com/libretro/nxengine-libretro.git
 }
 
 function build_cavestory() {
-    pushd "$rootdir/emulatorcores/nxengine-libretro"
+    make clean
     make
-    if [[ -z `find $rootdir/emulatorcores/nxengine-libretro/ -name "*libretro*.so"` ]]; then
-        __ERRMSGS="$__ERRMSGS Could not successfully compile NXEngine / Cave Story core."
-    fi
-    popd
+    md_ret_require="nxengine_libretro.so"
+}
+
+function install_cavestory() {
+    md_ret_files=(
+        'nxengine_libretro.so'
+    )
 }
 
 function configure_cavestory() {
-    if [[ ! -d $romdir/ports ]]; then
-        mkdir -p $romdir/ports
-    fi
+    mkdir -p $romdir/ports
+
     cat > "$romdir/ports/Cave Story.sh" << _EOF_
 #!/bin/bash
-$rootdir/supplementary/runcommand/runcommand.sh 1 "$emudir/$1/bin/retroarch -L `find $rootdir/emulatorcores/nxengine-libretro/ -name "*libretro*.so" | head -1` --config $rootdir/configs/all/retroarch.cfg --appendconfig $rootdir/configs/cavestory/retroarch.cfg $rootdir/emulatorcores/nxengine-libretro/datafiles/Doukutsu.exe"
+$rootdir/supplementary/runcommand/runcommand.sh 1 "$emudir/$1/bin/retroarch -L $md_inst/nxengine_libretro.so --config $rootdir/configs/all/retroarch.cfg --appendconfig $rootdir/configs/cavestory/retroarch.cfg $rootdir/emulatorcores/nxengine-libretro/datafiles/Doukutsu.exe"
 _EOF_
     chmod +x "$romdir/ports/Cave Story.sh"
-    chown -R $user:$user "$rootdir/emulatorcores/nxengine-libretro/datafiles/"
 
     setESSystem 'Ports' 'ports' '~/RetroPie/roms/ports' '.sh .SH' '%ROM%' 'pc' 'ports'
 }
