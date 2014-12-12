6d1a3f2e32b00fc16a96e56ee51e6644650f1717
Jools Wills <buzz@exotica.org.uk>
2014-12-12 23:46:53 +0000

    reworked mupen64libretro for new build location / system

--- scriptmodules/libretrocores/mupen64libretro.sh
+++ scriptmodules/libretrocores/mupen64libretro.sh
@@ -3,31 +3,29 @@ rp_module_desc="N64 LibretroCore Mupen64Plus"
 rp_module_menus="4+"
 
 function sources_mupen64plus() {
-    rmDirExists "$rootdir/libretrocores/mupen64plus"
     # Base repo:
     # gitPullOrClone "$rootdir/libretrocores/mupen64plus" git://github.com/libretro/mupen64plus-libretro.git
     # Freezed fixed repo:
-    gitPullOrClone "$rootdir/libretrocores/mupen64plus" git://github.com/gizmo98/mupen64plus-libretro.git
+    gitPullOrClone "$md_build" git://github.com/gizmo98/mupen64plus-libretro.git
 }
 
 function build_mupen64plus() {
     rpSwap on 256 240
-    
-    pushd "$rootdir/libretrocores/mupen64plus"
-    # Add missing path --> Fix already merged https://github.com/libretro/mupen64plus-libretro/commit/c035cf1c7a2514aeb14adf51ad825208ff1a068d
-    # sed -i 's|GL_LIB := -lGLESv2|GL_LIB := -L/opt/vc/lib -lGLESv2|g' Makefile
     make clean
-    make platform=rpi 
-    if [[ -z `find $rootdir/libretrocores/mupen64plus/ -name "*libretro*.so"` ]]; then
-        __ERRMSGS="$__ERRMSGS Could not successfully compile N64 core."
-    fi
-    
+    make platform=rpi
     rpSwap off
-    popd
+    md_ret_require="$md_build/mupen64plus_libretro.so"
+}
+
+function install_mupen64plus() {
+    md_ret_files=(
+        'mupen64plus_libretro.so'
+        'README.md'
+    )
 }
 
 function configure_mupen64plus() {
-    mkdir -p $romdir/n64
+    mkdir -p "$romdir/n64"
 
     ensureSystemretroconfig "n64"
 
@@ -37,17 +35,9 @@ function configure_mupen64plus() {
     ensureKeyValue "mupen64-screensize" "640x480" "$rootdir/configs/all/retroarch-core-options.cfg"
 
     # Copy config files
-    cp $rootdir/libretrocores/mupen64plus/mupen64plus/mupen64plus-core/data/mupen64plus.cht $home/RetroPie/BIOS/mupen64plus.cht
-    cp $rootdir/libretrocores/mupen64plus/mupen64plus/mupen64plus-core/data/mupencheat.txt $home/RetroPie/BIOS/mupencheat.txt
-    cp $rootdir/libretrocores/mupen64plus/mupen64plus/mupen64plus-core/data/mupen64plus.ini $home/RetroPie/BIOS/mupen64plus.ini 
-    cp $rootdir/libretrocores/mupen64plus/mupen64plus/mupen64plus-core/data/font.ttf $home/RetroPie/BIOS/font.ttf
-
-    # Set permissions
-    chmod +x "$home/RetroPie/BIOS/mupen64plus.cht"
-    chmod +x "$home/RetroPie/BIOS/mupencheat.txt"
-    chmod +x "$home/RetroPie/BIOS/mupen64plus.ini"
-    chmod +x "$home/RetroPie/BIOS/font.ttf"
+    cp "$md_build/mupen64plus-core/data/"{mupen64plus.cht,mupencheat.txt,mupen64plus.ini,font.ttf} "$home/RetroPie/BIOS/"
+    chown -R $user:$user "$home/RetroPie/BIOS"
 
     rps_retronet_prepareConfig
-    setESSystem "Nintendo 64" "n64" "~/RetroPie/roms/n64" ".z64 .Z64 .n64 .N64 .v64 .V64" "$rootdir/supplementary/runcommand/runcommand.sh 1 \"$emudir/$1/bin/retroarch -L `find $rootdir/libretrocores/mupen64plus/ -name \"*libretro*.so\" | head -1` --config $rootdir/configs/all/retroarch.cfg --appendconfig $rootdir/configs/n64/retroarch.cfg $__tmpnetplaymode$__tmpnetplayhostip_cfile $__tmpnetplayport$__tmpnetplayframes %ROM%\"" "n64" "n64"
+    setESSystem "Nintendo 64" "n64" "~/RetroPie/roms/n64" ".z64 .Z64 .n64 .N64 .v64 .V64" "$rootdir/supplementary/runcommand/runcommand.sh 1 \"$emudir/$1/bin/retroarch -L $md_inst/mupen64plus_libretro.so --config $rootdir/configs/all/retroarch.cfg --appendconfig $rootdir/configs/n64/retroarch.cfg $__tmpnetplaymode$__tmpnetplayhostip_cfile $__tmpnetplayport$__tmpnetplayframes %ROM%\"" "n64" "n64"
 }
