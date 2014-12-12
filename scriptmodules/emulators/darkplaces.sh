38f569653c1be8c25a1495b18b71a0d58c6701d8
Jools Wills <buzz@exotica.org.uk>
2014-12-12 02:08:49 +0000

    reworked darkplaces quake for new build location / system

--- scriptmodules/emulators/darkplaces.sh
+++ scriptmodules/emulators/darkplaces.sh
@@ -7,43 +7,31 @@ function depends_darkplaces() {
 }
 
 function sources_darkplaces() {
-    gitPullOrClone "$rootdir/emulators/darkplaces" git://github.com/autonomous1/darkplacesrpi.git
+    gitPullOrClone "$builddir/$1" git://github.com/autonomous1/darkplacesrpi.git
 }
 
 function install_darkplaces() {
-    pushd "$rootdir/emulators/darkplaces"
-        # Install darkplaces debian package
-        sudo dpkg -i darkplaces-rpi.deb
+    # Install darkplaces debian package
+    dpkg -i darkplaces-rpi.deb
+    rm darkplaces-rpi.deb
+
+    # Download game file
+    wget ftp://ftp.idsoftware.com/idstuff/quake/quake106.zip
+    unzip -o quake106.zip
+    rm quake106.zip
+    lhasa ef resource.1
     
-        # Download game file
-        wget ftp://ftp.idsoftware.com/idstuff/quake/quake106.zip
-        unzip -o quake106.zip
-        lhasa ef resource.1
-        
-        # Create ports directory
-        mkdir -p $romdir/ports
-        mkdir -p $romdir/ports/quake
+    # Create ports directory
+    mkdir -p $romdir/ports/quake
+
+    # Copy game dir to rom dir
+    cp -rf id1 $romdir/ports/quake/id1
     
-        # Copy game dir to rom dir
-        cp -rf id1 $romdir/ports/quake/id1
-        
-        # Set game file permission
-        chmod 666 "$romdir/ports/quake/id1/pak0.pak"
-        chmod 666 "$romdir/ports/quake/id1/autoexec.cfg"
-        chmod 666 "$romdir/ports/quake/id1/config.cfg"
-        chmod 666 "$romdir/ports/quake/id1/default.cfg"
-        chmod 666 "$romdir/ports/quake/id1/quake.rc"
-        chmod 666 "$romdir/ports/quake/id1/textures/quake.tga"
-        
-        # Remove game file
-        rm quake106.zip
-    popd
+    # Set game file permission
+    chown -R $user:$user "$romdir/ports/quake/"
 }
 
 function configure_darkplaces() {
-    # Set video permissions
-    sudo usermod -a -G video $user
-    
     # Create startup script
     cat > "$romdir/ports/darkplacesquake.sh" << _EOF_
 #!/bin/bash
@@ -51,7 +39,8 @@ sudo darkplaces-sdl -basedir $romdir/ports -quake
 _EOF_
     
     # Set startup script permissions
-    chmod 666 "$romdir/ports/darkplacesquake.sh"
+    chmod u+x "$romdir/ports/darkplacesquake.sh"
+    chown $user:$user "$romdir/ports/darkplacesquake.sh"
     
     # Add darkplaces quake to emulationstation
     setESSystem 'Ports' 'ports' '~/RetroPie/roms/ports' '.sh .SH' '%ROM%' 'pc' 'ports'
