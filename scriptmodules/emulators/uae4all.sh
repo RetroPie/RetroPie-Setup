c28b1d62cc235403e40dd739e19dbcd5a895d32c
Jools Wills <buzz@exotica.org.uk>
2014-12-12 01:12:46 +0000

    reworked uae4all for new build location / system

--- scriptmodules/emulators/uae4all.sh
+++ scriptmodules/emulators/uae4all.sh
@@ -2,39 +2,36 @@ rp_module_id="uae4all"
 rp_module_desc="Amiga emulator UAE4All"
 rp_module_menus="2+"
 
-# Amiga emulator UAE4All
-
 function depends_uae4all() {
     rps_checkNeededPackages libsdl1.2-dev libsdl-mixer1.2-dev libasound2-dev
 }
 
 function sources_uae4all() {
-    rmDirExists "$rootdir/emulators/uae4rpi"
-    mkdir -p "$rootdir/emulators"
-    wget http://downloads.petrockblock.com/retropiearchives/uae4rpi.tar.bz2
-    tar -jxvf uae4rpi.tar.bz2 -C "$rootdir/emulators/"
-    rm uae4rpi.tar.bz2
-    sed -i "s/-lstdc++$/-lstdc++ -lm -lz/" "$rootdir/emulators/uae4rpi/Makefile"
+    wget -O- -q http://downloads.petrockblock.com/retropiearchives/uae4rpi.tar.bz2 | tar -xvj --strip-components=1
+    sed -i "s/-lstdc++$/-lstdc++ -lm -lz/" Makefile
 }
 
 function build_uae4all() {
-    pushd "$rootdir/emulators/uae4rpi"
-    if [[ ! -f /opt/vc/include/interface/vmcs_host/vchost_config.h ]]; then
-        touch /opt/vc/include/interface/vmcs_host/vchost_config.h
-    fi
+    touch /opt/vc/include/interface/vmcs_host/vchost_config.h
+    make clean
     make
-    if [[ ! -f "$rootdir/emulators/uae4rpi/uae4all" ]]; then
-        __ERRMSGS="$__ERRMSGS Could not successfully compile Amiga emulator."
-    fi
-    popd
+    require="$builddir/$1/uae4all"
+}
+
+function install_uae4all() {
+    files=(
+        'COPYING'
+        'docs'
+        'uae4all'
+    )
 }
 
 function configure_uae4all() {
     mkdir -p "$romdir/amiga"
 
-    cat > "$rootdir/emulators/uae4rpi/startAmigaDisk.sh" << _EOF_
+    cat > "$emudir/$1/startAmigaDisk.sh" << _EOF_
 #!/bin/bash
-pushd "$rootdir/emulators/uae4rpi/"
+pushd "$emudir/$1"
 if [[ -f "df0.adf" ]]; then
      rm df0.adf
  fi
@@ -42,7 +39,9 @@ ln -s "$romdir/amiga/$1" "df0.adf"
 ./uae4all    
 popd
 _EOF_
-    chown -R $user:$user "$rootdir/emulators/uae4rpi/"
-    chmod +x "$rootdir/emulators/uae4rpi/startAmigaDisk.sh"
-    setESSystem "Amiga" "amiga" "~/RetroPie/roms/amiga" ".adf .ADF" "$rootdir/emulators/uae4rpi/startAmigaDisk.sh %ROM%" "amiga" "amiga"
+    chmod +x "$emudir/$1/startAmigaDisk.sh"
+
+    chown -R $user:$user "$emudir/$1"
+
+    setESSystem "Amiga" "amiga" "~/RetroPie/roms/amiga" ".adf .ADF" "$emudir/$1/startAmigaDisk.sh %ROM%" "amiga" "amiga"
 }
