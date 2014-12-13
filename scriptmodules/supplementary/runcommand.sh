2d6ff9f208b2658b1018f4487922173989bbfdc8
Jools Wills <buzz@exotica.org.uk>
2014-12-13 09:14:21 +0000

    quote path

--- scriptmodules/supplementary/runcommand.sh
+++ scriptmodules/supplementary/runcommand.sh
@@ -3,6 +3,6 @@ rp_module_desc="Video mode script 'runcommand'"
 rp_module_menus="2+"
 
 function install_runcommand() {
-    cp $scriptdir/supplementary/runcommand.sh "$md_inst/"
+    cp "$scriptdir/supplementary/runcommand.sh" "$md_inst/"
     chmod +x "$md_inst/runcommand.sh"
 }
