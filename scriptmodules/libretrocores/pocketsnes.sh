937bc3095eb58317c36ed075bc9bc374fe310fc9
Jools Wills <buzz@exotica.org.uk>
2014-12-13 00:26:16 +0000

    reworked pocketsnes for new built location / system

--- scriptmodules/libretrocores/pocketsnes.sh
+++ scriptmodules/libretrocores/pocketsnes.sh
@@ -10,12 +10,13 @@ function sources_pocketsnes() {
 function build_pocketsnes() {
     make clean
     make
-    md_ret_require="$md_build/fmsx-libretro.so"
+    md_ret_require="$md_build/libretro.so"
 }
 
 function install_pocketsnes() {
     md_ret_files=(
-        'libff'
+        'libretro.so'
+        'README.txt'
     )
 }
 
@@ -23,7 +24,7 @@ function configure_pocketsnes() {
     mkdir -p "$romdir/snes"
 
     rps_retronet_prepareConfig
-    setESSystem "Super Nintendo" "snes" "~/RetroPie/roms/snes" ".smc .sfc .fig .swc .SMC .SFC .FIG .SWC" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"$emudir/$1/bin/retroarch -L `find $rootdir/libretrocores/pocketsnes-libretro/ -name \"*libretro*.so\" | head -1` --config $rootdir/configs/all/retroarch.cfg --appendconfig $rootdir/configs/snes/retroarch.cfg $__tmpnetplaymode$__tmpnetplayhostip_cfile $__tmpnetplayport$__tmpnetplayframes %ROM%\"" "snes" "snes"
-    # <!-- alternatively: <command>$rootdir/emulators/snes9x-rpi/snes9x %ROM%</command> -->
-    # <!-- alternatively: <command>$rootdir/emulators/pisnes/snes9x %ROM%</command> -->
+    setESSystem "Super Nintendo" "snes" "~/RetroPie/roms/snes" ".smc .sfc .fig .swc .SMC .SFC .FIG .SWC" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"$emudir/$1/bin/retroarch -L $md_instlibretro.so --config $rootdir/configs/all/retroarch.cfg --appendconfig $rootdir/configs/snes/retroarch.cfg $__tmpnetplaymode$__tmpnetplayhostip_cfile $__tmpnetplayport$__tmpnetplayframes %ROM%\"" "snes" "snes"
+    # <!-- alternatively: <command>$emudir/snes9x-rpi/snes9x %ROM%</command> -->
+    # <!-- alternatively: <command>$emudir/pisnes/snes9x %ROM%</command> -->
 }
