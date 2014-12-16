7c06d930c2dda8b5c7e9ac07a5c1d58ae2157a00
Jools Wills <buzz@exotica.org.uk>
2014-12-16 22:34:18 +0000

    rework/fix up retroarchjoypadautoconf

--- scriptmodules/supplementary/retroarchjoypadautoconf.sh
+++ scriptmodules/supplementary/retroarchjoypadautoconf.sh
@@ -2,14 +2,13 @@ rp_module_id="retroarchjoypadautoconf"
 rp_module_desc="RetroArch-Joypad-AutoConfig"
 rp_module_menus="4+"
 
+function sources_retroarchjoypadautoconf() {
+    gitPullOrClone "$md_build" https://github.com/libretro/retroarch-joypad-autoconfig
+
+}
+
 function install_retroarchjoypadautoconf() {
-    if [[ ! -d "$rootdir/emulators/RetroArch/configs/" ]]; then
-        mkdir -p "$rootdir/emulators/RetroArch/configs/"
-    fi
-    gitPullOrClone "$rootdir/supplementary/retroarch-joypad-autoconfig" "https://github.com/libretro/retroarch-joypad-autoconfig" || return 1
-    pushd "$rootdir/supplementary/retroarch-joypad-autoconfig" || return 1
-    cd udev
-    cp *.cfg "$rootdir/emulators/RetroArch/configs/"
-    cd ..
-    popd
+    mkdir -p "$emudir/retroarch/configs/"
+    cp udev/*.cfg "$emudir/retroarch/configs/"
+    sudo chown -R $user:$user "$emudir/retroarch/configs"
 }
