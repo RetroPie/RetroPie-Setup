a81ab3539301aaa35ce9dfd8f61afe7af7d087da
Jools Wills <buzz@exotica.org.uk>
2014-12-16 03:11:01 +0000

    added libsdl1.2-dev libsdl-net1.2-dev dependencies for fastdosbox (net needed for some modem/ipx functionality)

--- scriptmodules/emulators/fastdosbox.sh
+++ scriptmodules/emulators/fastdosbox.sh
@@ -2,6 +2,10 @@ rp_module_id="fastdosbox"
 rp_module_desc="DOS emulator FastDosbox"
 rp_module_menus="2+"
 
+function depends_fastdosbox() {
+    checkNeededPackages libsdl1.2-dev libsdl-net1.2-dev
+}
+
 function sources_fastdosbox() {
     wget -O- -q http://downloads.petrockblock.com/retropiearchives/fastdosbox-1.5_src.tar.gz | tar -xvz --strip-components=1
     sed -i 's|#include "nofun.h"|//#include "nofun.h"|g' "$md_build/src/gui/sdl_mapper.cpp"
