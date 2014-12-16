ef2b6271c103e3308b02149e273331bb679e48b9
Jools Wills <buzz@exotica.org.uk>
2014-12-16 09:04:23 +0000

    compare the binary to run against config file /opt/retropie/configs/all/dispmanx and if it matches, configure LD_LIBRARY_PATH to point to the dispmanx sdl. this way we can add a binaries name to the config to make it use the sdl dispmanx libs #518

--- supplementary/runcommand.sh
+++ supplementary/runcommand.sh
@@ -9,6 +9,7 @@
 # starttype==7: set video mode to sdtv PAL and run command
 # starttype==8: set video mode to sdtv NTSC and run command
 
+dispmanx_conf="/opt/retropie/configs/all/dispmanx"
 
 starttype=$1
 shift
@@ -16,48 +17,58 @@ shift
 # set cpu governor profile performance
 echo "performance" | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
+set -x
+
+binary=${1/% */}
+# if we have a dispmanx conf file and the current binary is in it, change the library path to load dispmanx sdl first
+if [ -f "$dispmanx_conf" ] && grep "$binary" "$dispmanx_conf"; then
+  command="LD_LIBRARY_PATH=/opt/retropie/supplementary/sdl1dispmanx/lib $@"
+else
+  command="$@"
+fi
+
 if [[ $starttype -eq 1 && ! -z `tvservice -m CEA | egrep -w "mode 1"` ]] || [[ $starttype -eq 3 ]]; then
     tvservice -e "CEA 1"
     fbset -depth 8 && fbset -depth 16
 #   fbset -rgba 5,6,5
-    eval $@
+    eval $command
     tvservice -p
     fbset -depth 8 && fbset -depth 16
 elif [[ $starttype -eq 2 ]]; then
-    eval $@
+    eval $command
 elif [[ $starttype -eq 4 && ! -z `tvservice -m CEA | egrep -w "mode 4"` ]]; then
     tvservice -e "CEA 4"
     fbset -depth 8 && fbset -depth 16
 #   fbset -rgba 5,6,5
-    eval $@
+    eval $command
     tvservice -p
     fbset -depth 8 && fbset -depth 16
 elif [[ $starttype -eq 5 && ! -z `tvservice -m CEA | egrep -w "mode 17"` ]]; then
     tvservice -e "CEA 17"
     fbset -depth 8 && fbset -depth 16
-    eval $@
+    eval $command
     tvservice -p
     fbset -depth 8 && fbset -depth 16
 elif [[ $starttype -eq 6 && ! -z `tvservice -m CEA | egrep -w "mode 19"` ]]; then
     tvservice -e "CEA 19"
     fbset -depth 8 && fbset -depth 16
-    eval $@
+    eval $command
     tvservice -p
     fbset -depth 8 && fbset -depth 16
 elif [[ $starttype -eq 7 ]]; then
     tvservice -c "PAL 4:3"
     fbset -depth 8 && fbset -depth 16
-    eval $@
+    eval $command
     tvservice -p
     fbset -depth 8 && fbset -depth 16
 elif [[ $starttype -eq 8 ]]; then
     tvservice -c "NTSC 4:3"
     fbset -depth 8 && fbset -depth 16
-    eval $@
+    eval $command
     tvservice -p
     fbset -depth 8 && fbset -depth 16
 else
-    eval $@
+    eval $command
 fi
 
 # set cpu governor profile ondemand
