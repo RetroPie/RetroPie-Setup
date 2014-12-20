e5e8dbdc5720482421bd98910fb41eb46d6cade4
Jools Wills <buzz@exotica.org.uk>
2014-12-20 20:02:56 +0000

    move mame4all data folders to $configdir/mame4all make new rom folders for mame artwork and samples

--- scriptmodules/emulators/mame4all.sh
+++ scriptmodules/emulators/mame4all.sh
@@ -34,10 +34,24 @@ function install_mame4all() {
 
 function configure_mame4all() {
     mkRomDir "mame"
-    mkdir -p "$md_inst/"{cfg,hi,sta,roms}
-    chown -R $user:$user "$md_inst"
-    ensureKeyValueShort "samplerate" "22050" "$md_inst/mame.cfg"
+    mkRomDir "mame-samples"
+    mkRomDir "mame-artwork"
+
+    mkdir -p "$configdir/$md_id/"cfg,hi,inp,memcard,nvram,snap,sta}
+    chown -R $user:$user "$configdir/$md_id"
+    ensureKeyValueShort "cfg" "$configdir/$md_id/cfg" "$md_inst/mame.cfg"
+    ensureKeyValueShort "hi" "$configdir/$md_id/hi" "$md_inst/mame.cfg"
+    ensureKeyValueShort "inp" "$configdir/$md_id/inp" "$md_inst/mame.cfg"
+    ensureKeyValueShort "memcard" "$configdir/$md_id/memcard" "$md_inst/mame.cfg"
+    ensureKeyValueShort "nvram" "$configdir/$md_id/nvram" "$md_inst/mame.cfg"
+    ensureKeyValueShort "snap" "$configdir/$md_id/snap" "$md_inst/mame.cfg"
+    ensureKeyValueShort "sta" "$configdir/$md_id/sta" "$md_inst/mame.cfg"
+
+    ensureKeyValueShort "artwork" "$romdir/mame-artwork" "$md_inst/mame.cfg"
+    ensureKeyValueShort "samplepath" "$romdir/mame-samples" "$md_inst/mame.cfg"
     ensureKeyValueShort "rompath" "$romdir/mame" "$md_inst/mame.cfg"
+
+    ensureKeyValueShort "samplerate" "22050" "$md_inst/mame.cfg"
 
     setESSystem "MAME" "mame" "~/RetroPie/roms/mame" ".zip .ZIP" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"$md_inst/mame %BASENAME%\"" "arcade" "mame"
 }
