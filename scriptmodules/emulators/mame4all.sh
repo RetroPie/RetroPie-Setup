4524c877838d2f526910b2248ef0d04bfe40821a
Jools Wills <buzz@exotica.org.uk>
2014-12-26 10:35:10 +0000

    can do a shallow checkout on github

--- scriptmodules/emulators/mame4all.sh
+++ scriptmodules/emulators/mame4all.sh
@@ -7,7 +7,7 @@ function depends_mame4all() {
 }
 
 function sources_mame4all() {
-    gitPullOrClone "$md_build" https://github.com/joolswills/mame4all-pi.git NS
+    gitPullOrClone "$md_build" https://github.com/joolswills/mame4all-pi.git
 }
 
 function build_mame4all() {
