19b5bf828cc8e4add7785d8b66687b218845267f
Jools Wills <buzz@exotica.org.uk>
2015-01-02 02:13:54 +0000

    ditched fastdosbox emulator - http://www.raspberrypi.org/forums/viewtopic.php?p=662228#p662228 switch to dosbox svn - and manually enable dynamic recompilation. add configuration for dispmanx to dosbox, and use runcommand

---
+++ scriptmodules/emulators/dosbox.sh
@@ -0,0 +1,62 @@
+rp_module_id="dosbox"
+rp_module_desc="DOS emulator"
+rp_module_menus="2+"
+
+function depends_dosbox() {
+    checkNeededPackages libsdl1.2-dev libsdl-net1.2-dev libsdl-sound1.2-dev libasound2-dev libpng12-dev automake autoconf zlib1g-dev
+}
+
+function sources_dosbox() {
+    wget -O- -q http://downloads.petrockblock.com/retropiearchives/dosbox-r3876.tar.gz | tar -xvz --strip-components=1
+}
+
+function build_dosbox() {
+    ./autogen.sh
+    ./configure --prefix="$md_inst" --disable-opengl
+    # enable dynamic recompilation for armv4
+    sed -i 's|/\* #undef C_DYNREC \*/|#define C_DYNREC 1|' config.h
+    sed -i 's/C_TARGETCPU.*/C_TARGETCPU ARMV4LE/g' config.h
+    make clean
+    make
+    md_ret_require="$md_build/src/dosbox"
+}
+
+function install_dosbox() {
+    make install
+    md_ret_require="$md_inst/bin/dosbox"
+}
+
+function configure_dosbox() {
+    mkRomDir "pc"
+
+    cat > "$romdir/pc/Start DOSBox.sh" << _EOF_
+#!/bin/bash
+$rootdir/supplementary/runcommand/runcommand.sh 1 "$md_inst/bin/dosbox -c \"MOUNT C $romdir/pc\"" "$md_id"
+_EOF_
+    chmod +x "$romdir/pc/Start DOSBox.sh"
+
+    local config_path=$(su "$user" -c "\"$md_inst/bin/dosbox\" -printconf")
+    ensureKeyValueShort "usescancodes" "false" "$config_path"
+    ensureKeyValueShort "core" "dynamic" "$config_path"
+    ensureKeyValueShort "cycles" "max" "$config_path"
+
+    configure_dispmanx_off_dosbox
+
+    setESSystem "PC (x86)" "pc" "~/RetroPie/roms/pc" ".sh" "$rootdir/supplementary/runcommand/runcommand.sh 0 \"%ROM%\" \"$md_id\"" "pc" "pc"
+}
+
+function configure_dispmanx_off_dosbox() {
+    local config_path=$("$md_inst/bin/dosbox" -printconf)
+    # turn off dispmanx
+    ensureKeyValueShort "$md_id" "0" "$configdir/all/dispmanx.cfg"
+    # scaling
+    ensureKeyValueShort "scaler" "normal2x" "$config_path"
+}
+
+function configure_dispmanx_on_dosbox() {
+    local config_path=$("$md_inst/bin/dosbox" -printconf)
+    # turn on dispmanx
+    ensureKeyValueShort "$md_id" "1" "$configdir/all/dispmanx.cfg"
+    # no scaling
+    ensureKeyValueShort "scaler" "none" "$config_path"
+}
