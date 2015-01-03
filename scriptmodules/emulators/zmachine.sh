1eba0df606cf9258810c7cbade3dae9089a26e01
Jools Wills <buzz@exotica.org.uk>
2015-01-03 17:03:13 +0000

    more logical loop for installing zork files.

--- scriptmodules/emulators/zmachine.sh
+++ scriptmodules/emulators/zmachine.sh
@@ -6,11 +6,11 @@ function install_zmachine() {
     aptInstall frotz
 
     mkRomDir "zmachine"
-    local i
-    for i in 1 2 3; do
-        wget http://downloads.petrockblock.com/retropiearchives/zork$i.zip
-        unzip -n zork$i.zip -d "$romdir/zmachine/zork$i"
-        rm zork$i.zip
+    local file
+    for file in zork1 zork2 zork3; do
+        wget http://downloads.petrockblock.com/retropiearchives/$file.zip
+        unzip -n $file.zip -d "$romdir/zmachine/$file"
+        rm $file.zip
     done
     chown -R $user:$user "$romdir/zmachine/"*
     __INFMSGS="$__INFMSGS The text adventures Zork 1 - 3 have been installed in the directory '$romdir/zmachine/'. You can start, e.g., Zork 1 with the command 'frotz $romdir/zmachine/zork1/DATA/ZORK1.DAT'."
