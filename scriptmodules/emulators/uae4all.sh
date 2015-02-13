a2962b9f54731e46366c9a6ec692f97cacf43b72
Jools Wills <jools@oxfordinspire.co.uk>
2015-02-13 03:19:58 +0000

    uae4all (2) - finally a decent Amiga emulator for the Pi. Runs very nicely on a pi2. Doesn't have the ability (yet) to launch with parameters, so we have a simple "Start.txt" to launch it, but the gui is easy to use and a massive improvement over the older emulator. Ships with a free AROS kickstart which is fine for playing many games etc. kick13.rom / kick20.rom / kick31.rom can be optionally placed in ~/RetroPie/BIOS folder

--- scriptmodules/emulators/uae4all.sh
+++ scriptmodules/emulators/uae4all.sh
@@ -4,51 +4,108 @@ rp_module_menus="2+"
 rp_module_flags="dispmanx"
 
 function depends_uae4all() {
-    getDepends libsdl1.2-dev libsdl-mixer1.2-dev libasound2-dev
+    getDepends libsdl1.2-dev libsdl-mixer1.2-dev libsdl-image1.2-dev libsdl-gfx1.2-dev libsdl-ttf2.0-dev
 }
 
 function sources_uae4all() {
-    wget -O- -q http://downloads.petrockblock.com/retropiearchives/uae4rpi.tar.bz2 | tar -xvj --strip-components=1
-    sed -i "s/-lstdc++$/-lstdc++ -lm -lz/" Makefile
+    gitPullOrClone "$md_build" https://github.com/joolswills/uae4all2.git retropie
+    mkdir guichan
+    wget -O- -q https://guichan.googlecode.com/files/guichan-0.8.2.tar.gz | tar -xvz --strip-components=1 -C "guichan"
+    cd guichan
+    # fix from https://github.com/sphaero/guichan
+    patch -p1 <<\_EOF_
+diff --git a/src/widget.cpp b/src/widget.cpp
+index 7dfc7e1..97978a7 100644
+--- a/src/widget.cpp
++++ b/src/widget.cpp
+@@ -598,7 +598,8 @@ namespace gcn
+     {
+         if (mFocusHandler == NULL)
+         {
+-            throw GCN_EXCEPTION("No focushandler set (did you add the widget to the gui?).");
++            return false;
++            //throw GCN_EXCEPTION("No focushandler set (isModalFocused: did you add the widget to the gui?).");
+         }
+ 
+         if (getParent() != NULL)
+@@ -614,7 +615,8 @@ namespace gcn
+     {
+         if (mFocusHandler == NULL)
+         {
+-            throw GCN_EXCEPTION("No focushandler set (did you add the widget to the gui?).");
++            return false;
++            //throw GCN_EXCEPTION("No focushandler set (isModalMouseInputFocused: did you add the widget to the gui?).");
+         }
+ 
+         if (getParent() != NULL)
+diff --git a/src/widgets/tabbedarea.cpp b/src/widgets/tabbedarea.cpp
+index e07d14c..5ed9d39 100644
+--- a/src/widgets/tabbedarea.cpp
++++ b/src/widgets/tabbedarea.cpp
+@@ -317,6 +317,10 @@ namespace gcn
+ 
+     void TabbedArea::logic()
+     {
++        for (unsigned int i = 0; i < mTabs.size(); i++)
++        {
++                  mTabs[i].second->logic();
++        }
+     }
+ 
+     void TabbedArea::adjustSize()
+
+_EOF_
 }
 
 function build_uae4all() {
-    touch /opt/vc/include/interface/vmcs_host/vchost_config.h
+    pushd guichan
     make clean
+    ./configure --enable-sdlimage --enable-sdl --disable-allegro --disable-opengl --disable-shared
     make
+    popd
+    make -f Makefile.pi clean
+    if isPlatform "rpi2"; then
+        make -f Makefile.pi NEON=1 DEFS="-DUSE_ARMV7 -DUSE_ARMNEON"
+    else
+        make -f Makefile.pi
+    fi
     md_ret_require="$md_build/uae4all"
 }
 
 function install_uae4all() {
+    unzip -o "AndroidData/guichan26032014.zip" -d "$md_inst"
+    unzip -o "AndroidData/guichan26032014.zip" -d "$md_inst"
+    unzip -o "AndroidData/aros20140110.zip" -d "$md_inst"
     md_ret_files=(
-        'COPYING'
-        'docs'
+        'copying'
         'uae4all'
+        'Readme.txt'
     )
 }
 
 function configure_uae4all() {
     mkRomDir "amiga"
 
-    setDispmanx "$md_id" 1
+    mkdir -p "$md_inst/conf"
+    echo "path=$romdir/amiga" >"$md_inst/conf/adfdir.conf"
+    chown -R $user:$user "$md_inst/conf"
 
-    cat > "$md_inst/startAmigaDisk.sh" << _EOF_
+    # symlinks to optional kickstart roms in our BIOS dir
+    for rom in kick12.rom kick13.rom kick20.rom kick31.rom; do
+        ln -sf "$biosdir/$rom" "$md_inst/kickstarts/$rom"
+    done
+
+    cat > "$md_inst/uae4all.sh" << _EOF_
 #!/bin/bash
-
-file="\$1"
-[[ -z "\$file" ]] && exit 1
-
 pushd "$md_inst"
-rm -rf df0.adf
-ln -s "\$file" df0.adf
-$rootdir/supplementary/runcommand/runcommand.sh 1 ./uae4all "$md_id"
+./uae4all
 popd
 _EOF_
-    chmod +x "$md_inst/startAmigaDisk.sh"
+    chmod a+x "$md_inst/uae4all.sh"
+    setDispmanx "$md_id" 1
 
-    chown -R $user:$user "$md_inst"
+    touch "$romdir/amiga/Start.txt"
+    chown $user:$user "$romdir/amiga/Start.txt"
 
-    setESSystem "Amiga" "amiga" "~/RetroPie/roms/amiga" ".adf .ADF" "$md_inst/startAmigaDisk.sh %ROM%" "amiga" "amiga"
-
-    __INFMSGS="$__INFMSGS The Amiga emulator can be started from command line with '$md_inst/uae4all'. Note that you must manually copy a Kickstart rom with the name 'kick.rom' to the directory $md_inst/uae4all/."
+    setESSystem "Amiga" "amiga" "~/RetroPie/roms/amiga" ".txt" "$md_inst/uae4all.sh" "amiga" "amiga"
 }
