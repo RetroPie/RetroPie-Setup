640ff087919f09eefea4f91589e789e8b9f6e2e0
Jools Wills <jools@oxfordinspire.co.uk>
2015-02-15 01:25:29 +0000

    force stella to 320x240, and enable dispmanx by default. also add a 320x240 mode to fb.modes if dispmanx disabled.

--- scriptmodules/emulators/stella.sh
+++ scriptmodules/emulators/stella.sh
@@ -11,5 +11,12 @@ function install_stella()
 function configure_stella() {
     mkRomDir "atari2600-stella"
 
-    setESSystem "Atari 2600" "atari2600" "~/RetroPie/roms/atari2600-stella" ".a26 .A26 .bin .BIN .rom .ROM .zip .ZIP .gz .GZ" "$rootdir/supplementary/runcommand/runcommand.sh 1 \"stella %ROM%\" \"$md_id\"" "atari2600" "atari2600"
+    setDispmanx "$md_id" 1
+
+    # when not using dispmanx
+    if [[ -z $(grep "mode \"320x240\"" /etc/fb.modes) ]]; then
+        echo -e "\nmode \"320x240\"\ngeometry 320 240 640 480 16\ntimings 0 0 0 0 0 0 0\nrgba 5/11,6/5,5/0,0/16\nendmode" >> /etc/fb.modes
+    fi
+
+    setESSystem "Atari 2600" "atari2600" "~/RetroPie/roms/atari2600-stella" ".a26 .A26 .bin .BIN .rom .ROM .zip .ZIP .gz .GZ" "$rootdir/supplementary/runcommand/runcommand.sh 0 \"stella -maxres 320x240 %ROM%\" \"$md_id\"" "atari2600" "atari2600"
 }
