3061e5a0d568f386373574b65be6985b0f5562e0
Jools Wills <jools@oxfordinspire.co.uk>
2015-02-18 19:21:39 +0000

    fuse - emulate a 128k+ machine by default

--- scriptmodules/emulators/fuse.sh
+++ scriptmodules/emulators/fuse.sh
@@ -10,5 +10,5 @@ function install_fuse() {
 function configure_fuse() {
     mkRomDir "zxspectrum"
 
-    setESSystem "ZX Spectrum" "zxspectrum" "~/RetroPie/roms/zxspectrum" ".sna .SNA .szx .SZX .z80 .Z80 .ipf .IPF .tap .TAP .tzx .TZX .gz .bz2 .udi .UDI .mgt .MGT .img .IMG .trd .TRD .scl .SCL .dsk .DSK" "$rootdir/supplementary/runcommand/runcommand.sh 1 \"fuse-sdl %ROM%\" \"$md_id\"" "zxspectrum" "zxspectrum"
+    setESSystem "ZX Spectrum" "zxspectrum" "~/RetroPie/roms/zxspectrum" ".sna .SNA .szx .SZX .z80 .Z80 .ipf .IPF .tap .TAP .tzx .TZX .gz .bz2 .udi .UDI .mgt .MGT .img .IMG .trd .TRD .scl .SCL .dsk .DSK" "$rootdir/supplementary/runcommand/runcommand.sh 1 \"fuse-sdl --machine 128 %ROM%\" \"$md_id\"" "zxspectrum" "zxspectrum"
 }
