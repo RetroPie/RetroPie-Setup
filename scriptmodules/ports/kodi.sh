f9df1a1c969af4f2218e80123333a68407b625f5
Jools Wills <jools@oxfordinspire.co.uk>
2015-02-21 02:58:20 +0000

    xbmc.sh -> kodi.sh

--- scriptmodules/ports/kodi.sh
+++ scriptmodules/ports/kodi.sh
@@ -1,19 +1,19 @@
 # http://www.gtkdb.de/index_36_2176.html
-rp_module_id="xbmc"
+rp_module_id="kodi"
 rp_module_desc="Install Kodi"
 rp_module_menus="4+"
 rp_module_flags="nobin"
 
-function depends_xbmc() {
+function depends_kodi() {
     echo "deb http://archive.mene.za.net/raspbian wheezy contrib" > /etc/apt/sources.list.d/mene.list
     apt-key adv --keyserver keyserver.ubuntu.com --recv-key 5243CDED
 }
 
-function build_xbmc() {
-    getDepends xbmc
+function install_kodi() {
+    aptInstall kodi
 }
 
-function configure_xbmc() {
+function configure_kodi() {
     echo 'SUBSYSTEM=="input", GROUP="input", MODE="0660"' > /etc/udev/rules.d/99-input.rules
 
     mkRomDir "ports"
