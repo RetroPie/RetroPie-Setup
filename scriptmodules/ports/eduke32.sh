63e53648530bc282979ddcec65da376fcb3c8e54
Jools Wills <jools@oxfordinspire.co.uk>
2015-02-22 04:39:16 +0000

    switch eduke32 to source + binary based distribution (and build for both rpi1/rpi2) - #152

--- scriptmodules/ports/eduke32.sh
+++ scriptmodules/ports/eduke32.sh
@@ -1,28 +1,51 @@
 rp_module_id="eduke32"
 rp_module_desc="Duke3D Port"
 rp_module_menus="2+"
-rp_module_flags="nobin"
+rp_module_flags="dispmanx"
+
+function depends_eduke32() {
+    getDepends libsdl1.2-dev libsdl-mixer1.2-dev libflac-dev libvorbis-dev libpng12-dev libvpx-dev freepats
+    # remove old eduke packages
+    hasPackage eduke32 && apt-get remove -y eduke32 duke3d-shareware
+}
+
+function sources_eduke32() {
+    wget -O- -q http://downloads.petrockblock.com/retropiearchives/eduke32.tar.gz | tar -xvz --strip-components=1
+}
+
+function build_eduke32() {
+    make veryclean
+    make NOASM=1 LTO=0 USE_OPENGL=0 SDL_TARGET=1
+    md_ret_require="$md_build/eduke32"
+}
 
 function install_eduke32() {
-    printHeading "Downloading eDuke core"
-    wget http://downloads.petrockblock.com/retropiearchives/eduke32_2.0.0rpi+svn2789_armhf.deb
-    printHeading "Downloading eDuke32 Shareware files"
-    wget http://downloads.petrockblock.com/retropiearchives/duke3d-shareware_1.3d-23_all.deb
-    dpkg -i ./*.deb
-    rm ./*.deb
-    md_ret_require="/usr/games/eduke32"
+    wget http://downloads.petrockblock.com/retropiearchives/3dduke13.zip -O 3dduke13.zip
+    unzip -L -o 3dduke13.zip dn3dsw13.shr
+    mkdir -p "$md_inst/shareware"
+    unzip -L -o dn3dsw13.shr -d "$md_inst/shareware" duke3d.grp duke.rts
+    md_ret_files=(
+        'eduke32'
+        'mapster32'
+    )
 }
 
 function configure_eduke32() {
     mkRomDir "ports/duke3d"
 
-    cp /usr/share/games/eduke32/DUKE.RTS "$romdir/ports/duke3d/"
-    cp /usr/share/games/eduke32/duke3d.grp "$romdir/ports/duke3d/"
+    local file
+    local file_bn
+    for file in "$md_inst/shareware/"*; do
+        file_bn=${file##*/}
+        rm -f "$romdir/ports/duke3d/$file_bn"
+        ln -snv "$file" "$romdir/ports/duke3d/$file_bn"
+    done
 
     cat > "$romdir/ports/Duke3D Shareware.sh" << _EOF_
 #!/bin/bash
-$rootdir/supplementary/runcommand/runcommand.sh 0 "/usr/games/eduke32 -j$romdir/ports/duke3d" "$md_id"
+$rootdir/supplementary/runcommand/runcommand.sh 0 "$md_inst/eduke32 -j$romdir/ports/duke3d" "$md_id"
 _EOF_
-    chmod +x "$romdir/ports/Duke3D Shareware.sh"   
+    chmod +x "$romdir/ports/Duke3D Shareware.sh"
+
     setESSystem 'Ports' 'ports' '~/RetroPie/roms/ports' '.sh .SH' '%ROM%' 'pc' 'ports' 
 }
