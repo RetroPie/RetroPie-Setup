8462a5c043d271d4cad8c43b15e03230e0cfc794
gizmo98 <gizmo98@users.noreply.github.com>
2015-02-23 17:06:16 +0100

    retroarchjoyconfig: Use new "set hotkeys" function

--- scriptmodules/supplementary/retroarchjoyconfig.sh
+++ scriptmodules/supplementary/retroarchjoyconfig.sh
@@ -16,7 +16,7 @@ function configure_retroarchjoyconfig() {
     configfname=$(echo ${configfname:16:-1} | tr -d ' ')
     mv "$emudir/retroarch/configs/tempconfig.cfg" "$emudir/retroarch/configs/$configfname.cfg"
     # Add hotkeys
-    $scriptdir/supplementary/setAutoconfHotkeys.py
+    rp_callModule retroarchautoconf configure
     chown -R $user:$user "$emudir/retroarch/configs"
     dialog --backtitle "$__backtitle" --msgbox "The configuration file has been saved as $configfname.cfg and will be used by RetroArch from now on whenever that controller is connected." 22 76
 }
