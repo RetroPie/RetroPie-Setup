9d2071619b0836922b58c754909aff12f5bb8c57
Jools Wills <jools@oxfordinspire.co.uk>
2015-02-26 02:56:04 +0000

    clean up audiosettings module - add option to launch alsamixer

--- scriptmodules/supplementary/audiosettings.sh
+++ scriptmodules/supplementary/audiosettings.sh
@@ -8,30 +8,30 @@ function configure_audiosettings() {
     options=(1 "Auto"
              2 "Headphones - 3.5mm jack"
              3 "HDMI"
-             4 "Reset to default")
+             4 "Mixer - adjust output volume"
+             R "Reset to default")
     choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
     if [[ -n "$choices" ]]; then
         case $choices in
             1) amixer cset numid=3 0
                alsactl store
-               ###set
-               dialog --backtitle "$__backtitle" --msgbox "Set audio output to auto" 22 76
-                            ;;
+               printMsgs "dialog" "Set audio output to auto"
+               ;;
             2) amixer cset numid=3 1
                alsactl store
-               ###set
-               dialog --backtitle "$__backtitle" --msgbox "Set audio output to headphones / 3.5mm jack " 22 76
-                            ;;
+               printMsgs "dialog" "Set audio output to headphones / 3.5mm jack"
+               ;;
             3) amixer cset numid=3 2
                alsactl store
-               ###set
-               dialog --backtitle "$__backtitle" --msgbox "Set audio output to HDMI" 22 76
-                            ;;
-            4) /etc/init.d/alsa-utils reset
+               printMsgs "dialog" "Set audio output to HDMI"
+               ;;
+            4) alsamixer
                alsactl store
-                 ###set
-               dialog --backtitle "$__backtitle" --msgbox "Audio settings reset to defaults" 22 76
-                            ;;
+               ;;
+            R) /etc/init.d/alsa-utils reset
+               alsactl store
+               printMsgs "dialog" "Audio settings reset to defaults"
+               ;;
         esac
     fi
 }
