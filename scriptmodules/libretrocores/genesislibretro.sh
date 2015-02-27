cff605bc0578e1452e82fa1f3f5aa9b0e17b9a38
gizmo98 <gizmo98@users.noreply.github.com>
2015-02-27 23:52:19 +0100

    Genesis: Keep old module name and rom folders

--- scriptmodules/libretrocores/genesislibretro.sh
+++ scriptmodules/libretrocores/genesislibretro.sh
@@ -1,18 +1,18 @@
-rp_module_id="libretro_genesis-plus-gx"
+rp_module_id="genesislibretro"
 rp_module_desc="GameGear/MasterSystem/Genesis LibretroCore genesis-plus-gx"
 rp_module_menus="2+"
 
-function sources_libretro_genesis-plus-gx() {
+function sources_genesislibretro() {
     gitPullOrClone "$md_build" git://github.com/libretro/Genesis-Plus-GX.git
 }
 
-function build_libretro_genesis-plus-gx() {
+function build_genesislibretro() {
     make -f Makefile.libretro clean
     make -f Makefile.libretro
     md_ret_require="$md_build/genesis_plus_gx_libretro.so"
 }
 
-function install_libretro_genesis-plus-gx() {
+function install_genesislibretro() {
     md_ret_files=(
         'genesis_plus_gx_libretro.so'
         'HISTORY.txt'
@@ -21,14 +21,14 @@ function install_libretro_genesis-plus-gx() {
     )
 }
 
-function configure_libretro_genesis-plus-gx() {
+function configure_genesislibretro() {
     mkRomDir "gamegear"
-    mkRomDir "mastersystem-genesis-plus-gx"
-    mkRomDir "megadrive-genesis-plus-gx"
+    mkRomDir "mastersystem-genesis"
+    mkRomDir "megadrive-genesis"
     
     ensureSystemretroconfig "gamegear"
-    ensureSystemretroconfig "mastersystem-genesis-plus-gx"
-    ensureSystemretroconfig "megadrive-genesis-plus-gx"
+    ensureSystemretroconfig "mastersystem-genesis"
+    ensureSystemretroconfig "megadrive-genesis"
     
     # system-specific shaders, gamegear
     iniConfig " = " "" "$configdir/gamegear/retroarch.cfg"
@@ -37,15 +37,15 @@ function configure_libretro_genesis-plus-gx() {
 
     # system-specific shaders, mastersystem
     iniConfig " = " "" "$configdir/mastersystem-genesis/retroarch.cfg"
-    iniSet "savefile_directory" "~/RetroPie/roms/mastersystem-genesis-plus-gx"
-    iniSet "savestate_directory" "~/RetroPie/roms/mastersystem-genesis-plus-gx"
+    iniSet "savefile_directory" "~/RetroPie/roms/mastersystem-genesis"
+    iniSet "savestate_directory" "~/RetroPie/roms/mastersystem-genesis"
     
     # system-specific shaders, megadrive
-    iniConfig " = " "" "$configdir/megadrive-genesis-plus-gx/retroarch.cfg"
-    iniSet "savefile_directory" "~/RetroPie/roms/megadrive-genesis-plus-gx"
-    iniSet "savestate_directory" "~/RetroPie/roms/megadrive-genesis-plus-gx"
+    iniConfig " = " "" "$configdir/megadrive-genesis/retroarch.cfg"
+    iniSet "savefile_directory" "~/RetroPie/roms/megadrive-genesis"
+    iniSet "savestate_directory" "~/RetroPie/roms/megadrive-genesis"
     
-    setESSystem "Sega Master System / Mark III" "mastersystem-genesis" "~/RetroPie/roms/mastersystem-genesis" ".sms .SMS .zip .ZIP" "$rootdir/supplementary/runcommand/runcommand.sh 1 \"$emudir/retroarch/bin/retroarch -L $md_inst/genesis_plus_gx_libretro.so --config $configdir/mastersystem/retroarch.cfg %ROM%\" \"$md_id\"" "mastersystem" "mastersystem"
+    setESSystem "Sega Master System / Mark III" "mastersystem-genesis" "~/RetroPie/roms/mastersystem-genesis" ".sms .SMS .zip .ZIP" "$rootdir/supplementary/runcommand/runcommand.sh 1 \"$emudir/retroarch/bin/retroarch -L $md_inst/genesis_plus_gx_libretro.so --config $configdir/mastersystem-genesis/retroarch.cfg %ROM%\" \"$md_id\"" "mastersystem" "mastersystem"
     setESSystem "Sega Game Gear" "gamegear" "~/RetroPie/roms/gamegear" ".gg .GG .zip .ZIP" "$rootdir/supplementary/runcommand/runcommand.sh 1 \"$emudir/retroarch/bin/retroarch -L $md_inst/genesis_plus_gx_libretro.so --config $configdir/gamegear/retroarch.cfg  %ROM%\" \"$md_id\"" "gamegear" "gamegear"
-	setESSystem "Sega Mega Drive / Genesis" "megadrive-genesis-plus-gx" "~/RetroPie/roms/megadrive-genesis-plus-gx" ".smd .SMD .bin .BIN .gen .GEN .md .MD .zip .ZIP" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"$emudir/retroarch/bin/retroarch -L $md_inst/genesis_plus_gx_libretro.so --config $configdir/megadrive-genesis-plus-gx/retroarch.cfg %ROM%\" \"$md_id\"" "genesis,megadrive" "megadrive"
+    setESSystem "Sega Mega Drive / Genesis" "megadrive-genesis" "~/RetroPie/roms/megadrive-genesis" ".smd .SMD .bin .BIN .gen .GEN .md .MD .zip .ZIP" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"$emudir/retroarch/bin/retroarch -L $md_inst/genesis_plus_gx_libretro.so --config $configdir/megadrive-genesis/retroarch.cfg %ROM%\" \"$md_id\"" "genesis,megadrive" "megadrive"
 }
