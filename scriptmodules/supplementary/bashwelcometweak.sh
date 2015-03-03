90c93686867fb30b64276c4d1d1ac32a55b14d87
Jools Wills <jools@oxfordinspire.co.uk>
2015-03-03 10:00:56 +0000

    fiddle with bashwelcometweak -  * make it a function with local variables so we don't pollute outside  * some error checking / logic so it works in a emulated chroot  * removed unused uptime code

--- scriptmodules/supplementary/bashwelcometweak.sh
+++ scriptmodules/supplementary/bashwelcometweak.sh
@@ -9,38 +9,49 @@ function install_bashwelcometweak() {
 # RETROPIE PROFILE START
 # Thanks to http://blog.petrockblock.com/forums/topic/retropie-mushroom-motd/#post-3965
 
-let upSeconds="$(/usr/bin/cut -d. -f1 /proc/uptime)"
-let secs=$((upSeconds%60))
-let mins=$((upSeconds/60%60))
-let hours=$((upSeconds/3600%24))
-let days=$((upSeconds/86400))
-UPTIME=$(printf "%d days, %02dh%02dm%02ds" "$days" "$hours" "$mins" "$secs")
+function retropie_welcome() {
+    local upSeconds="$(/usr/bin/cut -d. -f1 /proc/uptime)"
+    local secs=$((upSeconds%60))
+    local mins=$((upSeconds/60%60))
+    local hours=$((upSeconds/3600%24))
+    local days=$((upSeconds/86400))
+    local UPTIME=$(printf "%d days, %02dh%02dm%02ds" "$days" "$hours" "$mins" "$secs")
 
-# calculate rough CPU and GPU temperatures:
-cpuTempC=$(($(cat /sys/class/thermal/thermal_zone0/temp)/1000))
-cpuTempF=$((cpuTempC*9/5+32))
+    # calculate rough CPU and GPU temperatures:
+    local cpuTempC
+    local cpuTempF
+    local gpuTempC
+    local gpuTempF
+    if [[ -f "/sys/class/thermal/thermal_zone0/temp" ]]; then
+        cpuTempC=$(($(cat /sys/class/thermal/thermal_zone0/temp)/1000)) && cpuTempF=$((cpuTempC*9/5+32))
+    fi
 
-gpuTempC=$(/opt/vc/bin/vcgencmd measure_temp)
-gpuTempC=${gpuTempC:5:2}
-gpuTempF=$((gpuTempC*9/5+32))
-
-# get the load averages
-read one five fifteen rest < /proc/loadavg
+    if [[ -f "/opt/vc/bin/vcgencmd" ]]; then
+        if gpuTempC=$(/opt/vc/bin/vcgencmd measure_temp); then
+            gpuTempC=${gpuTempC:5:2}
+            gpuTempF=$((gpuTempC*9/5+32))
+        else
+            gpuTempC=""
+        fi
+    fi
 
 echo "$(tput setaf 2)
    .~~.   .~~.    $(date +"%A, %e %B %Y, %r")
   '. \ ' ' / .'   $(uname -srmo)$(tput setaf 1)
    .~ .~~~..~.   
   : .~.'~'.~. :   $(tput setaf 3)$(df -h | grep Filesystem)$(tput setaf 1)
- ~ (   ) (   ) ~  $(tput setaf 7)$(df -h|grep rootfs)$(tput setaf 1)
+ ~ (   ) (   ) ~  $(tput setaf 7)$(df -h / | tail -1)$(tput setaf 1)
 ( : '~'.~.'~' : ) Uptime.............: ${UPTIME}
  ~ .~       ~. ~  Memory.............: $(grep MemFree /proc/meminfo | awk {'print $2'})kB (Free) / $(grep MemTotal /proc/meminfo | awk {'print $2'})kB (Total)$(tput setaf 7)
   (  $(tput setaf 4) |   | $(tput setaf 7)  )  $(tput setaf 1) Running Processes..: $(ps ax | wc -l | tr -d " ")$(tput setaf 7)
-  '~         ~'  $(tput setaf 1) IP Address.........: $(ip route get 8.8.8.8 | head -1 | cut -d' ' -f8) $(tput setaf 7)
+  '~         ~'  $(tput setaf 1) IP Address.........: $(ip route get 8.8.8.8 2>/dev/null | head -1 | cut -d' ' -f8) $(tput setaf 7)
     *--~-~--*    $(tput setaf 1) Temperature........: CPU: $cpuTempC°C/$cpuTempF°F GPU: $gpuTempC°C/$gpuTempF°F
-                 $(tput setaf 7) The RetroPie Project, www.petrockblock.com
+                 $(tput setaf 7) The RetroPie Project, http://www.petrockblock.com
 
 $(tput sgr0)"
+}
+
+retropie_welcome
 
 # RETROPIE PROFILE END
 _EOF_
