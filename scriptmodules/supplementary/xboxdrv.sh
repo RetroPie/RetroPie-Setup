abd3b9269680e1921b91ef67e7a4f3636e4bfcc1
Jools Wills <jools@oxfordinspire.co.uk>
2015-03-03 11:01:16 +0000

    fix reverse logic for adding xboxdrv to /etc/rc.local which happened after our styleguide changes - #697

--- scriptmodules/supplementary/xboxdrv.sh
+++ scriptmodules/supplementary/xboxdrv.sh
@@ -5,7 +5,7 @@ rp_module_flags="nobin"
 
 function install_xboxdrv() {
     getDepends xboxdrv
-    if grep -q "xboxdrv" /etc/rc.local; then
+    if ! grep -q "xboxdrv" /etc/rc.local; then
         sed -i -e '13,$ s|exit 0|xboxdrv --daemon --id 0 --led 2 --deadzone 4000 --silent --trigger-as-button --next-controller --id 1 --led 3 --deadzone 4000 --silent --trigger-as-button --dbus disabled --detach-kernel-driver \&\nexit 0|g' /etc/rc.local
     fi
     iniConfig "=" "" "/boot/config.txt"
