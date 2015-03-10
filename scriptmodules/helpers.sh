6a378501c0e01af553e601d6c2d14775c626ba84
Jools Wills <jools@oxfordinspire.co.uk>
2015-03-10 00:30:47 +0000

    iniGet needs to match from start of line in helpers function too

--- scriptmodules/helpers.sh
+++ scriptmodules/helpers.sh
@@ -165,7 +165,7 @@ function iniGet() {
     local delim_strip=${delim// /}
     # if the stripped delimiter is empty - such as in the case of a space, just use the delimiter instead
     [[ -z "$delim_strip" ]] && delim_strip="$delim"
-    ini_value=$(sed -rn "s/[\s]*$key\s*$delim_strip\s*$quote(.+)$quote.*/\1/p" $file)
+    ini_value=$(sed -rn "s/^[\s]*$key\s*$delim_strip\s*$quote(.+)$quote.*/\1/p" $file)
 }
 
 function hasPackage() {
