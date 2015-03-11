9af98078cde36b430eb19abaa63d015519e0a399
Jools Wills <jools@oxfordinspire.co.uk>
2015-03-11 22:31:11 +0000

    fixed lr-mednafen-pce-fast - left over old config - and wrong .so path

--- scriptmodules/libretrocores/lr-mednafen-pce-fast.sh
+++ scriptmodules/libretrocores/lr-mednafen-pce-fast.sh
@@ -26,7 +26,5 @@ function configure_lr-mednafen-pce-fast() {
     mkRomDir "pcengine"
     ensureSystemretroconfig "pcengine"
 
-    addSystem 0 "$md_id" "pcengine" $md_inst/libretro.so
-
-    setESSystem "TurboGrafx 16 (PC Engine)" "pcengine" "~/RetroPie/roms/pcengine" ".pce .PCE .zip .ZIP" "$rootdir/supplementary/runcommand/runcommand.sh 1 \"$emudir/retroarch/bin/retroarch -L $md_inst/mednafen_pce_fast_libretro.so --config $configdir/all/retroarch.cfg --appendconfig $configdir/pcengine/retroarch.cfg %ROM%\" \"$md_id\"" "pcengine" "pcengine"
+    addSystem 0 "$md_id" "pcengine" "$md_inst/mednafen_pce_fast_libretro.so"
 }
