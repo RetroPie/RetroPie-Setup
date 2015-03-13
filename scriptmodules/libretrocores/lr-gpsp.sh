dab3161805dce7d44532f6ec0862eaca4fc8c304
Jools Wills <jools@oxfordinspire.co.uk>
2015-03-13 21:50:28 +0000

    add rpSwap on 512 for libretro gpsp

--- scriptmodules/libretrocores/lr-gpsp.sh
+++ scriptmodules/libretrocores/lr-gpsp.sh
@@ -9,7 +9,9 @@ function sources_lr-gpsp() {
 
 function build_lr-gpsp() {
     make clean
+    rpSwap on 512
     CFLAGS="$CFLAGS -DARM_MEMORY_DYNAREC" make platform=armv HAVE_DYNAREC=1
+    rpSwap off
     md_ret_require="$md_build/gpsp_libretro.so"
 }
 
