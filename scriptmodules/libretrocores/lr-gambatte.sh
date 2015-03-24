ec8e117be6e722c3dbe8e59f49e3952dc90c548f
Jools Wills <buzz@exotica.org.uk>
2015-03-24 18:32:36 +0000

    move gambette over to addSystem

--- scriptmodules/libretrocores/lr-gambatte.sh
+++ scriptmodules/libretrocores/lr-gambatte.sh
@@ -30,6 +30,6 @@ function configure_lr-gambatte() {
     ensureSystemretroconfig "gb" "hq4x.glslp"
     ensureSystemretroconfig "gbc" "hq4x.glslp"
 
-    setESSystem "Game Boy" "gb" "~/RetroPie/roms/gb" ".gb .GB .zip .ZIP" "$rootdir/supplementary/runcommand/runcommand.sh 0 \"$emudir/retroarch/bin/retroarch -L $md_inst/gambatte_libretro.so --config $configdir/gb/retroarch.cfg %ROM%\" \"$md_id\"" "gb" "gb"
-    setESSystem "Game Boy Color" "gbc" "~/RetroPie/roms/gbc" ".gbc .GBC .zip .ZIP" "$rootdir/supplementary/runcommand/runcommand.sh 0 \"$emudir/retroarch/bin/retroarch -L $md_inst/gambatte_libretro.so --config $configdir/gbc/retroarch.cfg %ROM%\" \"$md_id\"" "gbc" "gbc"
+    addSystem 1 "$md_id" "gb" "$md_inst/gambatte_libretro.so"
+    addSystem 1 "$md_id" "gbc" "$md_inst/gambatte_libretro.so"
 }
