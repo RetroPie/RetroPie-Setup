9bcc82cf61a22d66df6f51145e05af813a291503
Jools Wills <buzz@exotica.org.uk>
2015-03-27 19:26:49 +0000

    use own branch of mupen64plus video-rice plugin until upstream is fixed.

--- scriptmodules/emulators/mupen64plus.sh
+++ scriptmodules/emulators/mupen64plus.sh
@@ -30,7 +30,7 @@ function sources_mupen64plus() {
         #'ricrpi rsp-hle'
         'mupen64plus rsp-hle'
         #'ricrpi video-gles2rice'
-        'mupen64plus video-rice'
+        'joolswills video-rice rpi'
         'ricrpi video-gles2n64'
     )
     local repo
