b15c8a778db882934a03099a5814cc6b2efc4f23
Mforty Twoi <mfortytwoi@gmail.com>
2015-04-01 17:21:21 +0200

    Add check for already registered but untrusted controllers
      For now they're only listed, handling it might require more user
      interaction.

--- scriptmodules/supplementary/bluezps3.sh
+++ scriptmodules/supplementary/bluezps3.sh
@@ -26,6 +26,23 @@ EOF
 }
 
 function configure_bluezps3() {
+    # Check existing PS3 controllers
+    # This might happen by pressing the PS button before starting inotifywait
+    for file in $(find /var/lib/bluetooth/*/*/ -name info); do
+        local bt_info=$(grep -e Name -e Trusted $file)
+        # Ignore everything that isn't a PS3 controller
+        if [[ $(echo $bt_info | grep "Name=PLAYSTATION(R)3 Controller") ]]; then
+            if [[ $(echo $bt_info | grep "Trusted=false") ]]; then
+                local ps3_mac=$(echo $file | cut -d"/" -f6)
+                local not_trusted_macs="$ps3_mac\n$not_trusted_macs"
+            fi
+        fi
+    done
+    if [[ -n $not_trusted_macs ]]; then
+        printMsgs "dialog" "The following Playstation(R) 3 controllers are not trusted. Please trust them manually using bluetoothctl.
+$not_trusted_macs"
+    fi
+
     register_bluezps3
     printMsgs "dialog" "Successfully registered PS3 controller"
 }
