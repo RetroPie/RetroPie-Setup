5fe589804fe873eaa76f63314eed71aea876833f
Mforty Twoi <mfortytwoi@gmail.com>
2015-04-01 13:20:44 +0200

    Make bluezps3 script more reliable by using bluetoothctl

--- scriptmodules/supplementary/bluezps3.sh
+++ scriptmodules/supplementary/bluezps3.sh
@@ -7,14 +7,25 @@ function depends_bluezps3() {
     getDepends inotify-tools
 }
 
-function configure_bluezps3() {  
-    printMsgs "dialog" "Please connect your PS3 controller via USB-CABLE, press PS button and ENTER."
-    # Wait max 60s until directories appear
-    inotifywait -e create -r -t 60 /var/lib/bluetooth
-    # Trust every ps3 controller
-    for file in $(grep -l "Name=PLAYSTATION(R)3 Controller" /var/lib/bluetooth/*/*/info); do
-        sed -i "s/Trusted=false/Trusted=true/g" $file
-    done
+function register_bluezps3() {
+        printMsgs "dialog" "Please connect your PS3 controller via USB-CABLE"
+        # Wait max 60s until directories appear
+        printMsgs "console" "Disconnect USB cable and press PS button. Timeout is 60 seconds."
+        ps3_file=$(inotifywait -e create -r -t 60 /var/lib/bluetooth --format %w%f)
 
-    printMsgs "dialog" "Please restart."
+        if [[ -n $ps3_file ]]; then
+            # Check if new device is a PS3 controller
+            if [[ $(grep "Name=PLAYSTATION(R)3 Controller" "$ps3_file/info") ]]; then
+                bluetoothctl << EOF
+trust ${ps3_file##*/}
+EOF
+            fi
+        else
+            printMsgs "console" "No controller found after 60 seconds. Try unlpugging before pressing the PS button."
+        fi
+}
+
+function configure_bluezps3() {
+    register_bluezps3
+    printMsgs "dialog" "Successfully registered PS3 controller"
 }
