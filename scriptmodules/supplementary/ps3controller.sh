85af33f31a444f9bf7e1fb125cfc69850655e1a8
gizmo98 <gizmo98@users.noreply.github.com>
2015-04-04 11:56:43 +0200

    ps3controller: Add #!/bin/bash to scripts

--- scriptmodules/supplementary/ps3controller.sh
+++ scriptmodules/supplementary/ps3controller.sh
@@ -48,7 +48,8 @@ function install_ps3controller() {
     update-rc.d sixad defaults
 
 # If a bluetooth dongle is connected set state up and enable pscan
-    cat > $md_inst/bluetooth.sh << _EOF_  
+    cat > $md_inst/bluetooth.sh << _EOF_
+#!/bin/bash
 /usr/bin/hciconfig hci0 up
 if ! hciconfig | grep -q "BR/EDR"; then
     /usr/bin/hciconfig hci0 pscan
@@ -59,6 +60,7 @@ chmod +x "$md_inst/bluetooth.sh"
 
 # If a PS3 controller is connected over usb check if bluetooth dongle exits and start sixpair
     cat > $md_inst/ps3pair.sh << _EOF_  
+#!/bin/bash
 if ! hciconfig | grep -q "BR/EDR"; then
     /usr/bin/hciconfig hci0 pscan
     $md_inst/sixpair
