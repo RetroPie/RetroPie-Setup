fe1dee232288444e327f644fea209f4f500b0972
gizmo98 <gizmo98@users.noreply.github.com>
2015-04-16 17:20:59 +0200

    Add default sixad settings
    Disable sensitive buttons:
    http://blog.petrockblock.com/forums/topic/ps3-controller-too-sensitive-to-map/#post-95059

--- scriptmodules/supplementary/ps3controller.sh
+++ scriptmodules/supplementary/ps3controller.sh
@@ -84,7 +84,29 @@ _EOF_
 # Pair if PS3 controller is connected
 DRIVER=="usb", SUBSYSTEM=="usb", ATTR{idVendor}=="054c", ATTR{idProduct}=="0268", RUN+="$md_inst/ps3pair.sh"
 _EOF_
-
+    
+    # add default sixad settings
+    cat > /var/lib/sixad/profiles/default << _EOF_
+enable_leds 1
+enable_joystick 1
+enable_input 0
+enable_remote 0
+enable_rumble 0
+enable_timeout 0
+led_n_auto 0
+led_n_number 0
+led_anim 1
+enable_buttons 1
+enable_sbuttons 0
+enable_axis 1
+enable_accel 0
+enable_accon 0
+enable_speed 0
+enable_pos 0
+_EOF_
+    
+    sudo chmod 777 /var/lib/sixad/profiles/default
+    
     # Start sixad daemon
     /etc/init.d/sixad start
 
