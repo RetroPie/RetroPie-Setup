b9159c9b12a1865a54523e048b4a721df729c8eb
Jools Wills <buzz@exotica.org.uk>
2015-05-31 15:02:37 +0100

    fixed internal module name of lr-mednafen-ws to lr-mednafen-wswan

--- scriptmodules/libretrocores/lr-mednafen-wswan.sh
+++ scriptmodules/libretrocores/lr-mednafen-wswan.sh
@@ -8,31 +8,30 @@
 # at https://raw.githubusercontent.com/petrockblog/RetroPie-Setup/master/LICENSE.md.
 #
 
-rp_module_id="lr-mednafen-ws"
+rp_module_id="lr-mednafen-wswan"
 rp_module_desc="Wonderswan emu - Mednafen WonderSwan core port for libretro"
 rp_module_menus="4+"
 
-function sources_lr-mednafen-ws() {
+function sources_lr-mednafen-wswan() {
     gitPullOrClone "$md_build" https://github.com/libretro/beetle-wswan-libretro.git
 }
 
-function build_lr-mednafen-ws() {
+function build_lr-mednafen-wswan() {
     make clean
     make
     md_ret_require="$md_build/mednafen_wswan_libretro.so"
 }
 
-function install_lr-mednafen-ws() {
+function install_lr-mednafen-wswan() {
     md_ret_files=(
         'README.md'
         'mednafen_wswan_libretro.so'
     )
 }
 
-function configure_lr-mednafen-ws() {
-
+function configure_lr-mednafen-wswan() {
     mkRomDir "wonderswan"
     ensureSystemretroconfig "wonderswan"
-    
+
     addSystem 1 "$md_id" "wonderswan" "$md_inst/mednafen_wswan_libretro.so"
 }
