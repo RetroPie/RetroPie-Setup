ce8ede4defe4d2338255a202eb8e7b69be5bd7b4
Jools Wills <buzz@exotica.org.uk>
2015-06-16 16:41:34 +0100

    updated licence url (to RetroPie org). Remove . at end of licence url to avoid any confusion

--- scriptmodules/supplementary/retroarchautoconf.sh
+++ scriptmodules/supplementary/retroarchautoconf.sh
@@ -5,7 +5,7 @@
 # (c) Copyright 2012-2015  Florian Müller (contact@petrockblock.com)
 # 
 # See the LICENSE.md file at the top-level directory of this distribution and 
-# at https://raw.githubusercontent.com/petrockblog/RetroPie-Setup/master/LICENSE.md.
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
 rp_module_id="retroarchautoconf"
