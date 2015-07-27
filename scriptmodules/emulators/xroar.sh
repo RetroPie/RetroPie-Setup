7a234e8a89b9e0e2f9d6066764edd00c1ab52dc4
Jools Wills <buzz@exotica.org.uk>
2015-07-27 20:06:51 +0100

    Added xroar emulator to experimental for Dragon 32 / TRS-80 (CoCo) emulation - #913

---
+++ scriptmodules/emulators/xroar.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# This file is part of RetroPie.
+# 
+# (c) Copyright 2012-2015  Florian Müller (contact@petrockblock.com)
+# 
+# See the LICENSE.md file at the top-level directory of this distribution and 
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="xroar"
+rp_module_desc="Dragon / CoCo emulator XRoar"
+rp_module_menus="4+"
+rp_module_flags="dispmanx"
+
+function depends_xroar() {
+    getDepends libraspberrypi-dev libraspberrypi-doc
+}
+
+function sources_xroar() {
+    gitPullOrClone "$md_build" http://www.6809.org.uk/git/xroar.git rasppi
+    sed -i "s/-lopenmaxil/-lopenmaxil -lpthread -lm/g" configure
+}
+
+function build_xroar() {
+    cd /opt/vc/src/hello_pi/libs/ilclient
+    make clean
+    make
+    cd "$md_build"
+    ./configure --enable-rasppi --prefix="$md_inst"
+    make clean
+    make
+    md_ret_require="$md_build/xroar"
+}
+
+function install_xroar() {
+    make install
+}
+
+function configure_xroar() {
+    mkRomDir "dragon32"
+    mkRomDir "coco"
+
+    mkdir -p "$md_inst/share/xroar"
+    ln -snf "$biosdir" "$md_inst/share/xroar/roms"
+
+    addSystem 1 "$md_id-dragon32" "dragon32" "$md_inst/bin/xroar -machine dragon32 -run %ROM%" "Dragon 32" ".cas .wav .bas .asc .dmk .jvc .os9 .dsk .vdk .rom .ccc .sna"
+    addSystem 0 "$md_id-coco" "coco" "$md_inst/bin/xroar -machine coco -run %ROM%" "CoCo" ".cas .wav .bas .asc .dmk .jvc .os9 .dsk .vdk .rom .ccc .sna"
+
+    __INFMSGS+=("For emulator $md_id you need to copy system/basic roms such as d32.rom (Dragon 32) and bas13.rom (CoCo) to '$biosdir'.")
+}
