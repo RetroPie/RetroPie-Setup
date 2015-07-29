b9ece8f183def2d4bb24fc367d050eac42b3fe61
Jools Wills <buzz@exotica.org.uk>
2015-07-29 17:12:30 +0100

    add dispmanx flag to zesarux emulator and enable by default

--- scriptmodules/emulators/zesarux.sh
+++ scriptmodules/emulators/zesarux.sh
@@ -11,6 +11,7 @@
 rp_module_id="zesarux"
 rp_module_desc="ZX Spectrum emulator ZEsarUX"
 rp_module_menus="4+"
+rp_module_flags="dispmanx"
 
 function depends_zesarux() {
     getDepends libssl-dev libpthread-stubs0-dev libsdl1.2-dev libasound2-dev
@@ -74,6 +75,8 @@ _EOF_
 _EOF_
 
     chown $user:$user "$configdir/zxspectrum/.zesaruxrc"
+
+    setDispmanx "$md_id" 1
 
     addSystem 1 "$md_id" "zxspectrum" "$romdir/zxspectrum/+Start\ ZEsarUX.sh %ROM%" "" ".sh"
 }
