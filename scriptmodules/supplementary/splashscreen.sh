268287a61a8812b854d919b4894f955ca350a316
Jools Wills <buzz@exotica.org.uk>
2015-08-09 18:09:23 +0100

    rewrite splashscreen module to use external repository - and cleanup code

--- scriptmodules/supplementary/splashscreen.sh
+++ scriptmodules/supplementary/splashscreen.sh
@@ -17,84 +17,76 @@ function depends_splashscreen() {
     getDepends fbi omxplayer
 }
 
-function enable_splashscreen()
-{
-    clear
-    printHeading "Enabling custom splashscreen on boot."
-
+function install_splashscreen() {
     cp "$scriptdir/scriptmodules/$md_type/$md_id/asplashscreen" "/etc/init.d/"
-    chmod +x "/etc/init.d/asplashscreen"
+    gitPullOrClone "$md_inst" https://github.com/RetroPie/retropie-splashscreens.git
+}
 
-    find $scriptdir/supplementary/splashscreens/retropie2015-blue/ -type f > /etc/splashscreen.list
+function default_splashscreen() {
+    find "$md_inst/retropie2015-blue" -type f >/etc/splashscreen.list
+}
 
+function enable_splashscreen() {
     # This command installs the init.d script so it automatically starts on boot
-    update-rc.d asplashscreen start 00 S
+    update-rc.d asplashscreen start 00 S >/dev/null
 
     # not-so-elegant hack for later re-enabling the splashscreen
-    update-rc.d asplashscreen enable
+    update-rc.d asplashscreen enable >/dev/null
 }
 
-function disable_splashscreen()
-{
-    clear
-    printHeading "Disabling custom splashscreen on boot."
-
-    update-rc.d asplashscreen disable
+function disable_splashscreen() {
+    update-rc.d asplashscreen disable >/dev/null
 }
-
 
 function choose_splashscreen() {
-    printHeading "Configuring splashscreen"
-
-    local options
-    local ctr
-
-    ctr=0
-    pushd $scriptdir/supplementary/splashscreens/ > /dev/null
-    options=()
-    dirlist=()
-    for splashdir in $(find . -type d | sort) ; do
-        if [[ $splashdir != "." ]]; then
-            options+=($ctr "${splashdir:2}")
-            dirlist+=(${splashdir:2})
-            ((ctr++))
-        fi
-    done
-    popd > /dev/null
-    cmd=(dialog --backtitle "$__backtitle" --menu "Choose splashscreen." 22 76 16)
-    choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-    splashdir=${dirlist[$choices]}
-    if [[ -n "$choices" ]]; then
-        rm /etc/splashscreen.list
-        find $scriptdir/supplementary/splashscreens/$splashdir/ -type f | sort | while read line; do
-            echo $line >> /etc/splashscreen.list
-        done
+    local options=()
+    local i=0
+    local splashdir
+    while read splashdir; do
+        splashdir=${splashdir/$md_inst\//}
+        options+=("$i" "$splashdir")
+        ((i++))
+    done < <(find "$md_inst" -mindepth 1 -maxdepth 1 -type d | sort)
+    local cmd=(dialog --backtitle "$__backtitle" --menu "Choose splashscreen." 22 76 16)
+    local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+    if [[ -n "$choice" ]]; then
+        choice=$((choice*2+1))
+        splashdir=${options[choice]}
+        find "$md_inst/$splashdir" -type f >/etc/splashscreen.list
         printMsgs "dialog" "Splashscreen set to '$splashdir'."
     fi
 }
 
 
 function configure_splashscreen() {
-    cmd=(dialog --backtitle "$__backtitle" --menu "Choose the desired boot behaviour." 22 86 16)
-    options=(
+    if [[ ! -d "$md_inst" ]]; then
+        rp_callModule splashscreen install
+    fi
+    local cmd=(dialog --backtitle "$__backtitle" --menu "Choose the desired boot behaviour." 22 86 16)
+    local options=(
         1 "Enable custom splashscreen on boot"
         2 "Disable custom splashscreen on boot"
         3 "Choose splashscreen"
     )
-    choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-    if [[ -n "$choices" ]]; then
-        case $choices in
-            1)
-                enable_splashscreen
-                printMsgs "dialog" "Enabled custom splashscreen on boot."
-                ;;
-            2)
-                disable_splashscreen
-                printMsgs "dialog" "Disabled custom splashscreen on boot."
-                ;;
-            3)
-                choose_splashscreen
-                ;;
-        esac
-    fi
+    while true; do
+    local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+        if [[ -n "$choice" ]]; then
+            case $choice in
+                1)
+                    [[ ! -f /etc/splashscreen.list ]] && rp_CallModule splashscreen default
+                    enable_splashscreen
+                    printMsgs "dialog" "Enabled custom splashscreen on boot."
+                    ;;
+                2)
+                    disable_splashscreen
+                    printMsgs "dialog" "Disabled custom splashscreen on boot."
+                    ;;
+                3)
+                    choose_splashscreen
+                    ;;
+            esac
+        else
+            break
+        fi
+    done
 }
