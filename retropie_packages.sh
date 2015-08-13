9e9d35b67670cc4e42524da995a26fee77445fa7
Jools Wills <buzz@exotica.org.uk>
2015-08-13 20:22:36 +0100

    don't throw errors if keyboard unplugged whilst joy2key running in background (and when it's not found when killing)

--- retropie_packages.sh
+++ retropie_packages.sh
@@ -53,7 +53,7 @@ fi
 # if joy2key.py is installed run it with cursor keys for axis, and enter + space for buttons 0 and 1
 __joy2key_dev=$(ls -1 /dev/input/js* 2>/dev/null | head -n1)
 if [[ -f "$rootdir/supplementary/runcommand/joy2key.py" && -n "$__joy2key_dev" ]] && ! pgrep -f joy2key.py >/dev/null; then
-    "$rootdir/supplementary/runcommand/joy2key.py" "$__joy2key_dev" 1b5b44 1b5b43 1b5b41 1b5b42 0a 20 &
+    "$rootdir/supplementary/runcommand/joy2key.py" "$__joy2key_dev" 1b5b44 1b5b43 1b5b41 1b5b42 0a 20 & 2>/dev/null
     __joy2key_pid=$!
 fi
 
@@ -79,5 +79,5 @@ fi
 printMsgs "console" "${__INFMSGS[@]}"
 
 if [[ -n $__joy2key_pid ]]; then
-    kill -INT $__joy2key_pid
+    kill -INT $__joy2key_pid 2>/dev/null
 fi
