2b96d23d93a2a85c860960f4f2e62c5cc0b1d85f
Jools Wills <buzz@exotica.org.uk>
2015-09-10 22:12:12 +0100

    updated sdl1 module for building on jessie

--- scriptmodules/supplementary/sdl1.sh
+++ scriptmodules/supplementary/sdl1.sh
@@ -13,23 +13,28 @@ rp_module_desc="SDL 1.2.15 with rpi fixes and dispmanx"
 rp_module_menus=""
 rp_module_flags="!odroid nobin"
 
+function get_ver_sdl1() {
+    [[ "$__raspbian_ver" == "7" ]] && echo "8"
+    [[ "$__raspbian_ver" == "8" ]] && echo "11"
+}
+
 function depends_sdl1() {
-    getDepends devscripts libx11-dev libxext-dev libxt-dev libxv-dev x11proto-core-dev libts-dev libaudiofile-dev libpulse-dev libgl1-mesa-dev libasound2-dev libcaca-dev libdirectfb-dev libglu1-mesa-dev
+    getDepends debhelper dh-autoreconf devscripts libx11-dev libxext-dev libxt-dev libxv-dev x11proto-core-dev libaudiofile-dev libpulse-dev libgl1-mesa-dev libasound2-dev libcaca-dev libdirectfb-dev libglu1-mesa-dev
+    [[ "$__raspbian_ver" == "7" ]] && getDepends libts-dev
 }
 
 function sources_sdl1() {
-    local src="deb-src http://mirrordirector.raspbian.org/raspbian/ wheezy main contrib non-free rpi"
-    if ! grep -q "$src" /etc/apt/sources.list; then
-        addLineToFile "$src" /etc/apt/sources.list
-    fi
+    local src="deb-src http://ftp.debian.org/debian $__raspbian_name main"
+    echo "$src" >"/etc/apt/sources.list.d/src.list"
     apt-get update
-    apt-get source libsdl1.2-dev
+    apt-get source -y --allow-unauthenticated libsdl1.2-dev
+    rm "/etc/apt/sources.list.d/src.list"
     cd libsdl1.2-1.2.15
     
     # add fixes from https://github.com/RetroPie/sdl1/compare/master...rpi
     wget https://github.com/RetroPie/sdl1/compare/master...rpi.diff -O debian/patches/rpi.diff
     echo "rpi.diff" >>debian/patches/series
-    DEBEMAIL="Jools Wills <buzz@exotica.org.uk>" dch -v 1.2.15-8rpi "Added rpi fixes and dispmanx support from https://github.com/RetroPie/sdl1/compare/master...rpi"
+    DEBEMAIL="Jools Wills <buzz@exotica.org.uk>" dch -v 1.2.15-$(get_ver_sdl1)rpi "Added rpi fixes and dispmanx support from https://github.com/RetroPie/sdl1/compare/master...rpi"
 }
 
 function build_sdl1() {
@@ -39,7 +44,7 @@ function build_sdl1() {
 
 function install_sdl1() {
     # if the packages don't install completely due to missing dependencies the apt-get -y -f install will correct it
-    if ! dpkg -i libsdl1.2debian_1.2.15-8rpi_armhf.deb libsdl1.2-dev_1.2.15-8rpi_armhf.deb; then
+    if ! dpkg -i libsdl1.2debian_1.2.15-$(get_ver_sdl1)rpi_armhf.deb libsdl1.2-dev_1.2.15-$(get_ver_sdl1)rpi_armhf.deb; then
         apt-get -y -f install
     fi
     echo "libsdl1.2-dev hold" | dpkg --set-selections
@@ -49,8 +54,8 @@ function install_sdl1() {
 
 function install_bin_sdl1() {
     isPlatform "rpi" || fatalError "$mod_id is only available as a binary package for platform rpi"
-    wget "$__binary_url/libsdl1.2debian_1.2.15-8rpi_armhf.deb"
-    wget "$__binary_url/libsdl1.2-dev_1.2.15-8rpi_armhf.deb"
+    wget "$__binary_url/libsdl1.2debian_1.2.15-$(get_ver_sdl1)rpi_armhf.deb"
+    wget "$__binary_url/libsdl1.2-dev_1.2.15-$(get_ver_sdl1)rpi_armhf.deb"
     install_sdl1
     rm ./*.deb
 }
