7dbd13bb747a19a4f6bb3edecf13af109385ba42
Jools Wills <buzz@exotica.org.uk>
2015-09-15 02:26:38 +0100

    renamed autostartemustat to autostart

--- scriptmodules/supplementary/autostart.sh
+++ scriptmodules/supplementary/autostart.sh
@@ -8,12 +8,12 @@
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
-rp_module_id="autostartemustat"
+rp_module_id="autostart"
 rp_module_desc="Auto-start EmulationStation"
 rp_module_menus="3+"
 rp_module_flags="nobin"
 
-function enable_autostartemustat() {
+function enable_autostart() {
     if [[ "$__raspbian_ver" -lt "8" ]]; then
         sed -i "s|^1:2345:.*|1:2345:respawn:/bin/login -f $user tty1 </dev/tty1 >/dev/tty1 2>\&1|g" /etc/inittab
         update-rc.d lightdm disable 2 # taken from /usr/bin/raspi-config
@@ -37,7 +37,7 @@ done
 _EOF_
 }
 
-function disable_autostartemustat() {
+function disable_autostart() {
     if [[ "$__raspbian_ver" -lt "8" ]]; then
         sed -i "s|^1:2345:.*|1:2345:respawn:/sbin/getty --noclear 38400 tty1|g" /etc/inittab
         sed -i "/emulationstation/d" /etc/profile
@@ -47,7 +47,7 @@ function disable_autostartemustat() {
     rm -f /etc/profile.d/10-emulationstation.sh
 }
 
-function configure_autostartemustat() {
+function configure_autostart() {
     cmd=(dialog --backtitle "$__backtitle" --menu "Choose the desired boot behaviour." 22 76 16)
     options=(
         1 "Original boot behaviour"
@@ -57,11 +57,11 @@ function configure_autostartemustat() {
     if [[ -n "$choices" ]]; then
         case $choices in
             1)
-                disable_autostartemustat
+                disable_autostart
                 printMsgs "dialog" "Enabled original boot behaviour. ATTENTION: If you still have the custom splash screen enabled (via this script), you need to jump between consoles after booting via Ctrl+Alt+F2 and Ctrl+Alt+F1 to see the login prompt. You can restore the original boot behavior of the RPi by disabling the custom splash screen with this script."
                 ;;
             2)
-                enable_autostartemustat
+                enable_autostart
                 printMsgs "dialog" "Emulation Station is now starting on boot."
                 ;;
         esac
