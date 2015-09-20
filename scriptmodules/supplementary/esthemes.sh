fe511bd8aaf6f5027cab7e388661ad1de49103f3
Jools Wills <buzz@exotica.org.uk>
2015-09-20 20:54:50 +0100

    add support for themes on 3rd party repositories (following our naming convention) add themes from robertybob #1004 / #1005

--- scriptmodules/supplementary/esthemes.sh
+++ scriptmodules/supplementary/esthemes.sh
@@ -14,10 +14,12 @@ rp_module_menus="3"
 rp_module_flags="nobin"
 
 function install_theme_esthemes() {
-    local theme="$1"
+    local theme"$1"
+    local repo="$2"
+    [[ -z "$repo" ]] && repo="RetroPie"
     [[ -z "$theme" ]] && theme="simple"
     mkdir -p "/etc/emulationstation/themes"
-    gitPullOrClone "/etc/emulationstation/themes/$theme" "https://github.com/RetroPie/es-theme-$theme.git"
+    gitPullOrClone "/etc/emulationstation/themes/$theme" "https://github.com/$repo/es-theme-$theme.git"
 }
 
 function uninstall_theme_esthemes() {
@@ -29,19 +31,24 @@ function uninstall_theme_esthemes() {
 
 function configure_esthemes() {
     local themes=(
-        'simple'
-        'simple-dark'
-        'color-pi'
-        'simplified-static-canela'
-        'zoid'
-        'nbba'
+        'RetroPie simple'
+        'RetroPie simple-dark'
+        'RetroPie color-pi'
+        'RetroPie simplified-static-canela'
+        'RetroPie zoid'
+        'RetroPie nbba'
+        'robertybob space'
+        'robertybob simplebigart'
     )
     while true; do
         local theme
+        local repo
         local options=()
         local status=()
         local i=1
         for theme in "${themes[@]}"; do
+            theme=($theme)
+            theme="${theme[1]}"
             if [[ -d "/etc/emulationstation/themes/$theme" ]]; then
                 status+=("i")
                 options+=("$i" "Update or Uninstall $theme (installed)")
@@ -54,21 +61,23 @@ function configure_esthemes() {
         local cmd=(dialog --backtitle "$__backtitle" --menu "Choose an option" 22 76 16)
         local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
         if [[ -n "$choice" ]]; then
-            theme="${themes[choice-1]}"
+            theme=(${themes[choice-1]})
+            repo="${theme[0]}"
+            theme="${theme[1]}"
             if [[ "${status[choice-1]}" == "i" ]]; then
                 options=(1 "Update $theme" 2 "Uninstall $theme")
                 cmd=(dialog --backtitle "$__backtitle" --menu "Choose an option for theme" 12 40 06)
                 local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
                 case "$choice" in
                     1)
-                        rp_callModule esthemes install_theme "$theme"
+                        rp_callModule esthemes install_theme "$theme" "$repo"
                         ;;
                     2)
                         rp_callModule esthemes uninstall_theme "$theme"
                         ;;
                 esac
             else
-                rp_callModule esthemes install_theme "$theme"
+                rp_callModule esthemes install_theme "$theme" "$repo"
             fi
         else
             break
