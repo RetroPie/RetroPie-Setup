7278522624e4d29013ed6633b2d634bcba24d08a
Jools Wills <buzz@exotica.org.uk>
2015-09-21 10:13:36 +0100

    add the option to remove usbmount to usbromservice

--- scriptmodules/supplementary/usbromservice.sh
+++ scriptmodules/supplementary/usbromservice.sh
@@ -10,7 +10,7 @@
 
 rp_module_id="usbromservice"
 rp_module_desc="USB ROM Service"
-rp_module_menus="3+"
+rp_module_menus="3+configure"
 rp_module_flags="nobin"
 
 function depends_usbromservice() {
@@ -27,23 +27,38 @@ function disable_usbromservice() {
     rm -f etc/usbmount/mount.d/01_retropie_copyroms
 }
 
+function remove_usbromservice() {
+    disable_usbromservice
+    apt-get remove -y usbmount
+}
+
 function configure_usbromservice() {
-    cmd=(dialog --backtitle "$__backtitle" --menu "Choose from an option below." 22 86 16)
-    options=(
-        1 "Enable USB ROM Service"
-        2 "Disable USB ROM Service"
-    )
-    choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-    if [[ -n "$choices" ]]; then
-        case $choices in
-            1)
-                enable_usbromservice
-                printMsgs "dialog" "Enabled $md_desc"
-                ;;
-            2)
-                disable_usbromservice
-                printMsgs "dialog" "Disabled $md_desc"
-                ;;
-        esac
-    fi
+    while true; do
+        cmd=(dialog --backtitle "$__backtitle" --menu "Choose from an option below." 22 86 16)
+        options=(
+            1 "Enable USB ROM Service"
+            2 "Disable USB ROM Service"
+            3 "Remove usbmount daemon"
+        )
+        choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+        if [[ -n "$choices" ]]; then
+            case $choices in
+                1)
+                    rp_callModule "$md_id" depends
+                    rp_callModule "$md_id" enable
+                    printMsgs "dialog" "Enabled $md_desc"
+                    ;;
+                2)
+                    rp_callModule "$md_id" disable
+                    printMsgs "dialog" "Disabled $md_desc"
+                    ;;
+                3)
+                    rp_callModule "$md_id" remove
+                    printMsgs "dialog" "Removed $md_desc"
+                    ;;
+            esac
+        else
+            break
+        fi
+    done
 }
