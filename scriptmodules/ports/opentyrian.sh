d7293f60b45fc53cafb409067278fd37b6f3b383
Jools Wills <buzz@exotica.org.uk>
2015-10-20 22:24:04 +0100

    added opentyrian - in experimental for now. Works well on my system - thanks to Herb / dudleydes #1050

---
+++ scriptmodules/ports/opentyrian.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+# 
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+# 
+# See the LICENSE.md file at the top-level directory of this distribution and 
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="opentyrian"
+rp_module_desc="Open Tyrian, port of the DOS shoot-em-up Tyrian"
+rp_module_menus="4+"
+rp_module_flags="dispmanx"
+
+function depends_opentyrian() {
+    getDepends libsdl1.2-dev libsdl-net1.2-dev mercurial
+}
+
+function sources_opentyrian() {
+    hg clone https://bitbucket.org/opentyrian/opentyrian "$md_build"
+    # don't replace our CFLAGS
+    sed -i "s/CFLAGS := -pedantic/CFLAGS += -pedantic/" Makefile
+}
+
+function build_opentyrian() {
+    make clean
+    make
+    md_ret_require="$md_build/opentyrian"
+}
+
+function install_opentyrian() {
+    make install prefix="$md_inst"
+}
+
+function configure_opentyrian() {
+    mkRomDir "ports/opentyrian"
+
+    # get Tyrian 2.1 (freeware game data)
+    wget -q -O tyrian21.zip http://www.camanis.net/tyrian/tyrian21.zip
+    unzip -j tyrian21.zip -d "$romdir/ports/opentyrian/data"
+    rm -f tyrian21.zip
+
+    cat > "$romdir/ports/OpenTyrian.sh" << _EOF_
+#!/bin/bash
+$rootdir/supplementary/runcommand/runcommand.sh 0 "$md_inst/bin/opentyrian --data $romdir/ports/opentyrian/data" "$md_id"
+_EOF_
+
+    # Set startup script permissions
+    chmod +x "$romdir/ports/OpenTyrian.sh"
+    chown $user:$user "$romdir/ports/OpenTyrian.sh"
+
+    # Enable dispmanx by default.
+    setDispmanx "$md_id" 1
+
+    # Add OpenTyrian to EmulationStation
+    setESSystem 'Ports' 'ports' '~/RetroPie/roms/ports' '.sh .SH' '%ROM%' 'pc' 'ports'
+}
