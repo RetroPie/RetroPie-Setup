80eef740a910759dfc5a457323d9c5dfbd605604
Jools Wills <buzz@exotica.org.uk>
2015-11-07 15:32:23 +0000

    switch back to main libretro repository for lr-imame4all - has had some updates, and now has our fixes in. Update core library name and remove old core. we will keep our module name lr-imame4all despite upstream name change for backward compatibility etc.

--- scriptmodules/libretrocores/lr-imame4all.sh
+++ scriptmodules/libretrocores/lr-imame4all.sh
@@ -14,29 +14,31 @@ rp_module_desc="Arcade emu - iMAME4all (based on MAME 0.37b5) port for libretro"
 rp_module_menus="2+"
 
 function sources_lr-imame4all() {
-    gitPullOrClone "$md_build" https://github.com/RetroPie/imame4all-libretro.git retropie
+    gitPullOrClone "$md_build" https://github.com/libretro/mame2000-libretro.git
 }
 
 function build_lr-imame4all() {
-    make -f makefile.libretro clean
-    make -f makefile.libretro ARM=1
-    md_ret_require="$md_build/libretro.so"
+    make clean
+    make ARM=1
+    md_ret_require="$md_build/mame2000_libretro.so"
 }
 
 function install_lr-imame4all() {
     md_ret_files=(
-        'libretro.so'
+        'mame2000_libretro.so'
         'Readme.txt'
     )
 }
 
 function configure_lr-imame4all() {
-    # remove old install folder
+    # remove old install folder  / es config
     rm -rf "$rootdir/$md_type/mamelibretro"
+    delSystem "$md_id" "mame-libretro"
+    # remove old core library
+    rm -f "$md_inst/libretro.so"
 
     mkRomDir "mame-mame4all"
     ensureSystemretroconfig "mame-mame4all"
 
-    delSystem "$md_id" "mame-libretro"
-    addSystem 0 "$md_id" "mame-mame4all arcade mame" "$md_inst/libretro.so"
+    addSystem 0 "$md_id" "mame-mame4all arcade mame" "$md_inst/mame2000_libretro.so"
 }
