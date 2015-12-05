35b4a314e83f4de490ef9e931c3b5edf830f74a1
Jools Wills <buzz@exotica.org.uk>
2015-12-05 03:38:34 +0000

    removed old/unused scripts

--- tools/tarbz2.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# Excludes MacOS utility files
-
-export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
-export COPYFILE_DISABLE=true
-tar -c --exclude='._*' --exclude='.svn' --exclude='.DS_Store' --exclude='*.bak' --exclude='*~' -vjf "$@"
