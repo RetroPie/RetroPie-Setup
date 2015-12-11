54171f2131ef104bd9186d2701cf6e30a1575d5c
Jools Wills <buzz@exotica.org.uk>
2015-12-11 00:59:49 +0000

    remove cpc4rpi port of caprice32, and add capriceRPI which is a much better port and includes many useful features such as an OSK.

---
+++ scriptmodules/emulators/capricerpi.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+# 
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+# 
+# See the LICENSE.md file at the top-level directory of this distribution and 
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="capricerpi"
+rp_module_desc="Amstrad CPC emulator - port of Caprice32 for the RPI"
+rp_module_menus="2+"
+rp_module_flags="dispmanx"
+
+function sources_capricerpi() {
+    gitPullOrClone "$md_build" https://github.com/KaosOverride/CapriceRPI.git
+}
+
+function build_capricerpi() {
+    cd src
+    make clean
+
+    make RELEASE=TRUE
+    md_ret_require="$md_build/src/capriceRPI"
+}
+
+function install_capricerpi() {
+    cp -Rv "$md_build/"README*.txt "$md_inst/"
+    cp -Rv "$md_build/src/capriceRPI" "$md_inst/"
+}
+
+function configure_capricerpi() {
+    mkRomDir "amstradcpc"
+
+    addSystem 0 "$md_id" "amstradcpc" "$md_inst/capricerRPI %ROM%"
+}
