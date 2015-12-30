683115fddffb12e2b5aad82b90f6548a984f88e4
Jools Wills <buzz@exotica.org.uk>
2015-12-30 14:56:07 +0000

    switch back to udev driver - too many problems with name mismatches when using sdl2 by default. for the controllers that have different button mapping, we can either work around it manually, or put up instructions on the wiki for switching to sdl2 driver

--- scriptmodules/emulators/retroarch.sh
+++ scriptmodules/emulators/retroarch.sh
@@ -116,7 +116,7 @@ function configure_retroarch() {
     iniSet "input_autodetect_enable" "true"
     iniSet "joypad_autoconfig_dir" "$configdir/all/retroarch-joypads/"
     iniSet "auto_remaps_enable" "true"
-    iniSet "input_joypad_driver" "sdl2"
+    iniSet "input_joypad_driver" "udev"
 
     chown $user:$user "$config"
 }
