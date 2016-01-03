7127618090dd7c36866a8c5b5d130d1603b608fd
Stefan <gizmo98@users.noreply.github.com>
2016-01-03 20:42:40 +0100

    retroarch.sh: refine 8Bitdo controller udev workaround

--- scriptmodules/supplementary/emulationstation/configscripts/retroarch.sh
+++ scriptmodules/supplementary/emulationstation/configscripts/retroarch.sh
@@ -241,17 +241,23 @@ function map_retroarch_joystick() {
                 fi
                 ;;
             *)
+                key+="_btn"
                 # workaround for mismatched controller mappings
                 iniGet "input_driver"
                 if [[ "$ini_value" == "udev" ]]; then
                     case "$device_name" in 
                         "8Bitdo FC30 Pro"|"8Bitdo FC30 GamePad"|"8Bitdo FC30 II"|"8Bitdo NES30 GamePad"|"8Bitdo SFC30 GamePad"|"8Bitdo SNES30 GamePad"|"8Bitdo Zero GamePad")
-                            input_id=$(($input_id+11))
+                            if [[ "$input_id" -lt "17" ]]; then
+                                value=$(($input_id+11))
+                            fi
+                            ;;
+                        *)
+                            value="$input_id"
                             ;;
                     esac
+                else
+                    value="$input_id"
                 fi
-                key+="_btn"
-                value="$input_id"
                 ;;
         esac
         iniSet "$key" "$value"
