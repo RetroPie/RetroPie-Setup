014f380bbb20971d40858cf03f13581715e47d3d
gizmo98 <atz.SE@gmx.de>
2016-01-10 14:43:00 +0100

    lr-ppsspp.sh: add platform x86

--- scriptmodules/libretrocores/lr-ppsspp.sh
+++ scriptmodules/libretrocores/lr-ppsspp.sh
@@ -12,15 +12,19 @@
 rp_module_id="lr-ppsspp"
 rp_module_desc="PlayStation Portable emu - PPSSPP port for libretro"
 rp_module_menus="2+"
-rp_module_flags="!rpi1 !x86"
+rp_module_flags="!rpi1"
 
 function depends_lr-ppsspp() {
-    getDepends libraspberrypi-dev
+    isPlatform "rpi2" && getDepends libraspberrypi-dev
     [[ "$__default_gcc_version" == "4.7" ]] && getDepends gcc-4.8 g++-4.8
 }
 
 function sources_lr-ppsspp() {
-    gitPullOrClone "$md_build" https://github.com/joolswills/ppsspp.git libretro_rpi_fix
+    if isPlatform "rpi2"; then
+        gitPullOrClone "$md_build" https://github.com/joolswills/ppsspp.git libretro_rpi_fix
+    else
+        gitPullOrClone "$md_build" https://github.com/libretro/libretro-ppsspp.git
+    fi
     git submodule update --init
     # remove the lines that trigger the ffmpeg build script functions - we will just use the variables from it
     sed -i "/^build_ARMv6$/,$ d" ffmpeg/linux_arm.sh
@@ -33,10 +37,12 @@ function build_lr-ppsspp() {
     cd "$md_build"
     
     make -C libretro clean
+    local params=()
+    isPlatform "rpi2" && params+=("platform=rpi2")
     if [[ "$__default_gcc_version" == "4.7" ]]; then
-        make -C libretro platform=rpi2 CC=gcc-4.8 CXX=g++-4.8
+        make -C libretro "${params[@]}" CC=gcc-4.8 CXX=g++-4.8
     else
-        make -C libretro platform=rpi2
+        make -C libretro "${params[@]}"
     fi
     md_ret_require="$md_build/libretro/ppsspp_libretro.so"
 }
