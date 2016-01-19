142a307a9500fdd2645f048f92ffd9910f6553a2
Jools Wills <buzz@exotica.org.uk>
2016-01-19 06:26:55 +0000

    use isPlatform x11 for deciding on installing gnome-terminal

--- scriptmodules/supplementary/emulationstation.sh
+++ scriptmodules/supplementary/emulationstation.sh
@@ -18,7 +18,7 @@ function depends_emulationstation() {
         libboost-locale-dev libboost-system-dev libboost-filesystem-dev libboost-date-time-dev \
         libfreeimage-dev libfreetype6-dev libeigen3-dev libcurl4-openssl-dev \
         libasound2-dev cmake libsdl2-dev libsm-dev
-    isPlatform "x86" && getDepends gnome-terminal
+    isPlatform "x11" && getDepends gnome-terminal
 }
 
 function sources_emulationstation() {
