be661b8b6e33ce29c887eef7abcc9e00ec5d2b97
Jools Wills <buzz@exotica.org.uk>
2016-02-29 18:35:40 +0000

    added lr-mame experimental module (to go with lr-mess) - mostly for use on faster platforms

---
+++ scriptmodules/libretrocores/lr-mame.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+# 
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+# 
+# See the LICENSE.md file at the top-level directory of this distribution and 
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="lr-mame"
+rp_module_desc="MAME emulator - MAME (current) port for libretro"
+rp_module_menus="4+"
+
+function sources_lr-mame() {
+    gitPullOrClone "$md_build" https://github.com/libretro/MAME.git
+}
+
+function build_lr-mame() {
+    make -f Makefile.libretro clean
+    make -f Makefile.libretro
+    md_ret_require="$md_build/mame_libretro.so"
+}
+
+function install_lr-mame() {
+    md_ret_files=(
+        'mame_libretro.so'
+    )
+}
+
+function configure_lr-mame() {
+    mkRomDir "arcade"
+    mkRomDir "mame-libretro"
+    ensureSystemretroconfig "arcade"
+    ensureSystemretroconfig "mame-libretro"
+
+    addSystem 0 "$md_id" "arcade" "$md_inst/mame_libretro.so"
+    addSystem 0 "$md_id" "mame-libretro arcade mame" "$md_inst/mame_libretro.so"
+}
