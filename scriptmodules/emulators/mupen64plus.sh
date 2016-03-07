c7ca10eba34ec8126932e246e8c0fb21abd32290
Stefan <gizmo98@users.noreply.github.com>
2016-03-07 19:16:32 +0100

    mupen64plus.sh: fix isPlatform

--- scriptmodules/emulators/mupen64plus.sh
+++ scriptmodules/emulators/mupen64plus.sh
@@ -108,7 +108,7 @@ function install_mupen64plus() {
         if [[ -f "$source/projects/unix/Makefile" ]]; then
             # optflags is needed due to the fact the core seems to rebuild 2 files and relink during install stage most likely due to a buggy makefile
             local params=()
-            ifPlatform "rpi" && params+=("VC=1")
+            isPlatform "rpi" && params+=("VC=1")
             make -C "$source/projects/unix" PREFIX="$md_inst" OPTFLAGS="$CFLAGS" "${params[@]}" install
         fi
     done
