72a5b9df3988b9b7ddf3548388e7d3624cb7cd45
Jools Wills <buzz@exotica.org.uk>
2016-04-11 20:10:52 +0100

    update to 2.0.4 again, but with https://hg.libsdl.org/SDL/rev/6c86d3216f6a reverted as it breaks PS3/Wii U Pro, and possibly other controllers. #1297

--- scriptmodules/supplementary/sdl2.sh
+++ scriptmodules/supplementary/sdl2.sh
@@ -15,7 +15,7 @@ rp_module_menus=""
 rp_module_flags="nobin !x86"
 
 function get_ver_sdl2() {
-    local ver="2.0.3+1"
+    local ver="2.0.4+2"
     isPlatform "rpi" && ver+="rpi"
     isPlatform "mali" && ver+="mali"
     echo "$ver"
@@ -31,11 +31,12 @@ function depends_sdl2() {
 }
 
 function sources_sdl2() {
-    local branch="retropie-2.0.3"
-    isPlatform "mali" && branch="mali-2.0.3"
+    local branch="release-2.0.4"
+    isPlatform "rpi" && branch="retropie-2.0.4"
+    isPlatform "mali" && branch="mali-2.0.4"
     gitPullOrClone "$md_build/$(get_ver_sdl2)" https://github.com/RetroPie/SDL-mirror.git "$branch"
     cd $(get_ver_sdl2)
-    DEBEMAIL="Jools Wills <buzz@exotica.org.uk>" dch -v $(get_ver_sdl2) "SDL 2.0.3 configured for the $__platform"
+    DEBEMAIL="Jools Wills <buzz@exotica.org.uk>" dch -v $(get_ver_sdl2) "SDL 2.0.4 configured for the $__platform"
 }
 
 function build_sdl2() {
