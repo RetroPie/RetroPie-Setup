4e062f19231fa03451eaddf853b2534fc72d2b2d
Jools Wills <buzz@exotica.org.uk>
2016-06-09 18:08:55 +0100

    ti99 - move archive / fix config dir and cleanup

--- scriptmodules/emulators/ti99sim.sh
+++ scriptmodules/emulators/ti99sim.sh
@@ -19,27 +19,25 @@ function depends_ti99sim() {
 }
 
 function sources_ti99sim() {
-    wget http://www.mrousseau.org/programs/ti99sim/archives/ti99sim-0.13.0.src.tar.gz
-    tar -zxvf ./ti99sim-0.13.0.src.tar.gz
-    rm ./ti99sim-0.13.0.src.tar.gz
+    wget -q -O- "$__archive_url/ti99sim-0.13.0.src.tar.gz" | tar -xvz --strip-components=1
 }
 
 function build_ti99sim() {
-    cd ./ti99sim-0.13.0/
     make
 }
 
 function install_ti99sim() {
     md_ret_files=(
-        'ti99sim-0.13.0/src/sdl/Release/ti99sim-sdl'
+        'src/sdl/Release/ti99sim-sdl'
     )
 }
 
 function configure_ti99sim() {
     mkRomDir "ti99"
-    moveConfigDir "$home/.ti99sim" "$md_conf_root/$md_id/"
+    moveConfigDir "$home/.ti99sim" "$md_conf_root/ti99/"
 
     addSystem 1 "$md_id" "ti99" "pushd $romdir/ti99; $md_inst/ti99sim-sdl -f %ROM%; popd" "TI99" ".ctg .CTG"
+
     __INFMSGS+=("You will need to place your BIOS ROMs/carts into $romdir/ti99. Make sure that your TI-994A.ctg file is cased as shown here as the emulator is case-sensitive.")
 }
 
