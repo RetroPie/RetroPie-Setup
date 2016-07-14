RetroPie/RetroPie-Setup

commit cbd919a1827d8a6cf0caa7f04014de05a74ee45c

advmame changes - #1554
* Moved all advmame dirs to the right place.
* Use symlinks rather than duped directories.

scriptmodules/emulators/advmame.sh
@@ -164,7 +164,15 @@ function install_advmame() {
 
 function configure_advmame() {
     mkRomDir "arcade"
+    mkRomDir "arcade/advmame"
     mkRomDir "mame-advmame"
+
+    local mame_sub_dir
+    for mame_sub_dir in artwork diff hi inp memcard nvram sample snap sta
+    do
+         mkRomDir "mame-advmame/$mame_sub_dir"
+         ln -sf "$romdir/mame-advmame/$mame_sub_dir" "$romdir/arcade/advmame/$mame_sub_dir"
+    done
 
     # delete old install files
     rm -rf "$md_inst/"{bin,man,share}
@@ -180,9 +188,17 @@ function configure_advmame() {
         iniConfig " " "" "$md_conf_root/mame-advmame/advmame-$version.rc"
 
         iniSet "misc_quiet" "yes"
-        iniSet "dir_rom" "$romdir/mame-advmame:$romdir/arcade"
-        iniSet "dir_artwork" "$romdir/mame-advmame/artwork:$romdir/arcade/artwork"
-        iniSet "dir_sample" "$romdir/mame-advmame/samples:$romdir/arcade/sample"
+        iniSet "dir_rom" "$romdir/mame-advmame"
+        iniSet "dir_artwork" "$romdir/mame-advmame/artwork"
+        iniSet "dir_sample" "$romdir/mame-advmame/samples"
+        iniSet "dir_diff" "$romdir/mame-advmame/diff"
+        iniSet "dir_hi" "$romdir/mame-advmame/hi"
+        iniSet "dir_image" "$romdir/mame-advmame"
+        iniSet "dir_inp" "$romdir/mame-advmame/inp"
+        iniSet "dir_memcard" "$romdir/mame-advmame/memcard"
+        iniSet "dir_nvram" "$romdir/mame-advmame/nvram"
+        iniSet "dir_snap" "$romdir/mame-advmame/snap"
+        iniSet "dir_sta" "$romdir/mame-advmame/nvram"
 
         if isPlatform "rpi"; then
             iniSet "device_video" "fb"

commit 677637ecf0f8c639ed763c7566ab553a4b451459

advmame - fix up broken symlinks caused by #1554

scriptmodules/emulators/advmame.sh
@@ -172,6 +172,8 @@ function configure_advmame() {
     do
         mkRomDir "mame-advmame/$mame_sub_dir"
         ln -sf "$romdir/mame-advmame/$mame_sub_dir" "$romdir/arcade/advmame"
+        # fix for older broken symlink generation
+        rm -f "$romdir/mame-advmame/$mame_sub_dir/$mame_sub_dir"
     done
 
     # delete old install files