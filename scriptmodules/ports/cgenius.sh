29306f9c83832dc6d638f074a89b5b9a95a1f55e
Jools Wills <buzz@exotica.org.uk>
2016-07-21 04:42:15 +0100

    cgenius - make experimental again - module needs work

--- scriptmodules/ports/cgenius.sh
+++ scriptmodules/ports/cgenius.sh
@@ -11,7 +11,7 @@
 
 rp_module_id="cgenius"
 rp_module_desc="Commander Genius - Modern Interpreter for the Commander Keen Games (Vorticon and Galaxy Games)"
-rp_module_section="opt"
+rp_module_section="exp"
 
 function depends_cgenius() {
     getDepends build-essential libvorbis-dev libogg-dev libsdl2-dev libsdl2-image-dev libgl1-mesa-dev libboost-dev
