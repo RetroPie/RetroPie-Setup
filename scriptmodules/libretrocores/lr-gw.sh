e385457a7b267a371dd102f49e31d0f3f9c4291c
Jools Wills <buzz@exotica.org.uk>
2016-08-11 03:23:18 +0100

    cosmetic - remove trailing spaces in copyright

--- scriptmodules/libretrocores/lr-gw.sh
+++ scriptmodules/libretrocores/lr-gw.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # This file is part of The RetroPie Project
-# 
+#
 # The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
-# 
-# See the LICENSE.md file at the top-level directory of this distribution and 
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
--- scriptmodules/supplementary/emulationstation/inputconfiguration.sh
+++ scriptmodules/supplementary/emulationstation/inputconfiguration.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # This file is part of The RetroPie Project
-# 
+#
 # The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
-# 
-# See the LICENSE.md file at the top-level directory of this distribution and 
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
@@ -23,10 +23,10 @@
 #
 # function onstart_<filename without extension>_<inputtype>()
 # is run at the start of the input configuration
-# 
+#
 # function onend_<filename without extension>_<inputtype>()
 # is run at the end of the input configuration
-# 
+#
 # Arguments for the above two functions are
 #   $1 - device type
 #   $2 - device name
