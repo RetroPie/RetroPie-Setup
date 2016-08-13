2df694c637a76d8ebdf408aba57ffb743982e4a6
Stefan <gizmo98@users.noreply.github.com>
2016-08-13 16:36:07 +0200

    lr-glupen64: allow platform x86

--- scriptmodules/libretrocores/lr-glupen64.sh
+++ scriptmodules/libretrocores/lr-glupen64.sh
@@ -13,7 +13,7 @@ rp_module_id="lr-glupen64"
 rp_module_desc="N64 emu - GLupeN64 for libretro"
 rp_module_help="ROM Extensions: .z64 .n64 .v64\n\nCopy your N64 roms to $romdir/n64"
 rp_module_section="opt"
-rp_module_flags="!x86 !mali"
+rp_module_flags="!mali"
 
 function sources_lr-glupen64() {
     gitPullOrClone "$md_build" https://github.com/loganmc10/GLupeN64.git
