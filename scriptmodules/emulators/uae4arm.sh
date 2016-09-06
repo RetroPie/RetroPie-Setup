0c38d8100e21e543870853d945defd9a8ea7ed61
Jools Wills <buzz@exotica.org.uk>
2016-09-06 13:54:13 +0100

    uae4arm - updated dependencies

--- scriptmodules/emulators/uae4arm.sh
+++ scriptmodules/emulators/uae4arm.sh
@@ -16,7 +16,7 @@ rp_module_section="opt"
 rp_module_flags="!x86 !mali"
 
 function depends_uae4arm() {
-    getDepends libsdl1.2-dev libsdl-gfx1.2-dev libsdl-ttf2.0-dev libguichan-dev libmpg123-dev
+    getDepends libsdl1.2-dev libsdl-gfx1.2-dev libsdl-ttf2.0-dev libguichan-dev libmpg123-dev libxml2-dev libflac-dev
 }
 
 function sources_uae4arm() {
