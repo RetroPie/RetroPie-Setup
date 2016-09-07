295d3c91b2483d7b886433072ebbb932844c0106
vbs <nxxs@gmx.net>
2016-09-08 01:06:28 +0200

    fs-uae - add support for multi-disk games filenames have to conform to TOSEC naming conventions (http://www.tosecdev.org/tosec-naming-convention) When starting a multi-disk game, all disks starting with the same name until the "(Disk" part will be loaded

--- scriptmodules/emulators/fs-uae/fs-uae.sh
+++ scriptmodules/emulators/fs-uae/fs-uae.sh
@@ -23,8 +23,26 @@ romdir="$datadir/roms"
 biosdir="$datadir/BIOS"
 kickfile="$biosdir/kick13.rom"
 
+# this function takes one argument like "/home/vbs/RetroPie/roms/amiga/0-B/Barbarian II (1991)(Psygnosis)[cr FLT](Disk 1 of 2).adf"
+# then it finds all files that start with the same prefix "/home/vbs/RetroPie/roms/amiga/0-B/Barbarian II (1991)(Psygnosis)[cr FLT](Disk"
+# if the initial filename does not contain "(Disk" then it returns just the initial file
+function getFiles() {
+    DIR=$(dirname "$1")
+    BASE=$(basename "$1")
+    PATTERN=$(echo "$BASE" | sed s/\(Disk.*/\(Disk/g)
+    find "$DIR" | grep -F "$PATTERN" | sort
+}
+
+FLOPPYIMAGES=()
+n=0
+while read i
+do
+    FLOPPYIMAGES+=("--floppy_image_$n=$i")
+    (( n += 1))
+done < <(getFiles "$ROM")
+
 if [[ -f "$kickfile" ]]; then
-    fs-uae "$config" --floppy_drive_0="$ROM" --kickstart_file="$kickfile" --floppies_dir="$romdir/amiga" --save_states_dir="$romdir/amiga"
+    fs-uae "$config" --floppy_drive_0="$ROM" "${FLOPPYIMAGES[@]}" --kickstart_file="$kickfile" --floppies_dir="$romdir/amiga" --save_states_dir="$romdir/amiga"
 else
     dialog --msgbox "You need to copy the Amiga kickstart file (kick13.rom) to the folder $biosdir to boot the Amiga emulator." 22 76
 fi
