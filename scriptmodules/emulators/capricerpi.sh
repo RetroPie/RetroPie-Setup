7a39241b2a1e84414bee25936f49d73bdf33233b
Jools Wills <buzz@exotica.org.uk>
2016-10-18 18:43:17 +0100

    capricerpi - missing libsdl-image1.2-dev dependency

--- scriptmodules/emulators/capricerpi.sh
+++ scriptmodules/emulators/capricerpi.sh
@@ -16,7 +16,7 @@ rp_module_section="opt"
 rp_module_flags="dispmanx !xx86 !mali"
 
 function depends_capricerpi() {
-    getDepends libsdl1.2-dev libsdl-ttf2.0-dev zlib1g-dev
+    getDepends libsdl1.2-dev libsdl-image1.2-dev libsdl-ttf2.0-dev zlib1g-dev
 }
 
 function sources_capricerpi() {
