5ef4225d2a912502b134bf14272c7e4995196885
Jools Wills <buzz@exotica.org.uk>
2016-10-23 00:26:11 +0100

    snes9x2010 - enable for armv6 (but not as default).

--- scriptmodules/libretrocores/lr-snes9x2010.sh
+++ scriptmodules/libretrocores/lr-snes9x2010.sh
@@ -13,7 +13,6 @@ rp_module_id="lr-snes9x2010"
 rp_module_desc="Super Nintendo emu - Snes9x 1.52 based port for libretro"
 rp_module_help="Previously called lr-snes9x-next\n\nROM Extensions: .bin .smc .sfc .fig .swc .mgd .zip\n\nCopy your SNES roms to $romdir/snes"
 rp_module_section="main"
-rp_module_flags="!armv6"
 
 function _update_hook_lr-snes9x2010() {
     # move from old location and update emulators.cfg
@@ -45,5 +44,7 @@ function configure_lr-snes9x2010() {
     mkRomDir "snes"
     ensureSystemretroconfig "snes"
 
-    addSystem 1 "$md_id" "snes" "$md_inst/snes9x2010_libretro.so"
+    local def=1
+    isPlatform "armv6" && def=0
+    addSystem $def "$md_id" "snes" "$md_inst/snes9x2010_libretro.so"
 }
