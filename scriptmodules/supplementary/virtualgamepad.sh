b1acbd4b1c80abbc500424c6225696816d326a17
Jools Wills <buzz@exotica.org.uk>
2016-11-05 19:33:42 +0000

    *gamepad - rework virtualgamepad / mobile gamepad modules

--- scriptmodules/supplementary/virtualgamepad.sh
+++ scriptmodules/supplementary/virtualgamepad.sh
@@ -14,25 +14,48 @@ rp_module_desc="Virtual Gamepad for Smartphone"
 rp_module_section="exp"
 
 function depends_virtualgamepad() {
-    getDepends nodejs npm
-    if isPlatform "arm"; then
-        wget -qO "$__tmpdir/node_latest_armhf.deb" http://node-arm.herokuapp.com/node_latest_armhf.deb
-        dpkg -i "$__tmpdir/node_latest_armhf.deb"
-        rm "$__tmpdir/node_latest_armhf.deb"
+    getDepends nodejs
+    # if the system version of nodejs is old, we will install manually for armv6 or use nodesource
+    if hasPackage nodejs 4.6 lt; then
+        if isPlatform "armv6"; then
+            getDepends npm
+            if ! hasPackage node; then
+                wget -qO "$__tmpdir/node_latest_armhf.deb" http://node-arm.herokuapp.com/node_latest_armhf.deb
+                dpkg -i "$__tmpdir/node_latest_armhf.deb"
+                rm "$__tmpdir/node_latest_armhf.deb"
+            fi
+        else
+            getDepends curl
+            # remove any old node package - we will use nodesource
+            hasPackage node && aptRemove node
+            curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+            # force aptInstall to get a fresh list before installing
+            __apt_update=0
+            aptInstall nodejs
+            fi
+        fi
     fi
-    npm install --unsafe-perm pm2 -g
+}
+
+function remove_virtualgamepad() {
+    pm2 stop main
+    pm2 delete main
+    rm -f /etc/apt/sources.list.d/nodesource.list
 }
 
 function sources_virtualgamepad() {
     gitPullOrClone "$md_inst" https://github.com/miroof/node-virtual-gamepads.git
+    chown -R $user:$user "$md_inst"
 }
 
-function build_virtualgamepad() {
+function install_virtualgamepad() {
+    npm install pm2 -g --unsafe-perm
     cd "$md_inst"
-    npm install --unsafe-perm
+    sudo -u $user npm install
 }
 
 function configure_virtualgamepad() {
+    [[ "$md_mode" == "remove" ]] && return
     pm2 start main.js
     pm2 startup
     pm2 save
