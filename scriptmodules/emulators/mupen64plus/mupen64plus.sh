RetroPie/RetroPie-Setup

commit a3b6b88c39a22d3f96fe34c8e98c50dfc04e69ad

mupen64plus - remove unused vars from launch script

scriptmodules/emulators/mupen64plus/mupen64plus.sh
@@ -16,13 +16,10 @@ RES="$3"
 RSP_PLUGIN="$4"
 [[ -n "$RES" ]] && RES="--resolution $RES"
 [[ -z "$RSP_PLUGIN" ]] && RSP_PLUGIN="mupen64plus-rsp-hle"
+
 rootdir="/opt/retropie"
 configdir="$rootdir/configs"
 config="$configdir/n64/mupen64plus.cfg"
-
-user="$SUDO_USER"
-[[ -z "$user" ]] && user=$(id -un)
-home="$(eval echo ~$user)"
 datadir="$home/RetroPie"
 romdir="$datadir/roms"
 

commit 5d136e21ae4073a42ba4a1a8e78f72cc4e1e5cd8

mupen64plus - use HOME variable in script - broken in a3b6b88c39a22d3f96fe34c8e98c50dfc04e69ad (script is not run as sudo, but need the home var). #1748

scriptmodules/emulators/mupen64plus/mupen64plus.sh
@@ -20,7 +20,7 @@ RSP_PLUGIN="$4"
 rootdir="/opt/retropie"
 configdir="$rootdir/configs"
 config="$configdir/n64/mupen64plus.cfg"
-datadir="$home/RetroPie"
+datadir="$HOME/RetroPie"
 romdir="$datadir/roms"
 
 source "$rootdir/lib/inifuncs.sh"