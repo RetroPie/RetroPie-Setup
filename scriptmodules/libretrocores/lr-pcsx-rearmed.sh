5aef8d06c1d2c5d4f84ab0ca98b0ee1be57ae327
Jools Wills <buzz@exotica.org.uk>
2016-11-26 05:03:22 +0000

    Revert "lr-pcsx-rearmed - temporary fix for https://github.com/libretro/pcsx_rearmed/issues/78 until patch is upstream"
    This reverts commit 679b6c417aa27199d708d289627dfe8e6e4e9cb4.
    
    now fixed uptream

--- scriptmodules/libretrocores/lr-pcsx-rearmed.sh
+++ scriptmodules/libretrocores/lr-pcsx-rearmed.sh
@@ -19,7 +19,7 @@ function depends_lr-pcsx-rearmed() {
 }
 
 function sources_lr-pcsx-rearmed() {
-    gitPullOrClone "$md_build" https://github.com/joolswills/pcsx_rearmed.git dynarec_enable
+    gitPullOrClone "$md_build" https://github.com/libretro/pcsx_rearmed.git
 }
 
 function build_lr-pcsx-rearmed() {
