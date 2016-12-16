acba46e0953ac74055a085b40e6e4f2b9eb67a65
Jools Wills <buzz@exotica.org.uk>
2016-12-16 04:30:57 +0000

    solaris / love - disable on aarch64 (only armhf libluajit-5.1-dev currently available in odroid-c2 repo)

--- scriptmodules/ports/love.sh
+++ scriptmodules/ports/love.sh
@@ -13,6 +13,7 @@ rp_module_id="love"
 rp_module_desc="Love - 2d Game Engine"
 rp_module_help="Copy your Love roms to $romdir/love"
 rp_module_section="opt"
+rp_module_flags="!aarch64"
 
 function depends_love() {
     local depends=(mercurial autotools-dev automake libtool pkg-config libdevil-dev libfreetype6-dev libluajit-5.1-dev libphysfs-dev libsdl2-dev libopenal-dev libogg-dev libtheora-dev libvorbis-dev libflac-dev libflac++-dev libmodplug-dev libmpg123-dev libmng-dev)
