c2535402125d5b935000ca96ea5cc6d147c92515
harryzimm <stevie.m.whyte@gmail.com>
2016-12-23 22:48:52 +0000

    Update system.sh

--- scriptmodules/system.sh
+++ scriptmodules/system.sh
@@ -173,6 +173,9 @@ function get_platform() {
             ODROID-C2)
                 __platform="odroid-c2"
                 ;;
+             sun8i)
+                __platform="armv7-mali"
+                ;;
             "Freescale i.MX6 Quad/DualLite (Device Tree)")
                 __platform="imx6"
                 ;;
@@ -269,7 +272,7 @@ function platform_generic-x11() {
 function platform_armv7-mali() {
     __default_cflags="-O2 -march=armv7-a -mfpu=neon-vfpv4 -mfloat-abi=hard -ftree-vectorize -funsafe-math-optimizations"
     __default_asflags=""
-    __default_makeflags="-j$(nproc)"
+    __default_makeflags="-j2"
     __platform_flags="arm armv7 neon mali"
     __has_binaries=0
 }
