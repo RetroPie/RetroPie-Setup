5cdf80edb46b22022ec5242fc326f807e584fd9e
Jools Wills <buzz@exotica.org.uk>
2017-01-10 02:18:33 +0000

    lr-beetle-pcfx - update bios filename - fixes #1830

--- scriptmodules/libretrocores/lr-beetle-pcfx.sh
+++ scriptmodules/libretrocores/lr-beetle-pcfx.sh
@@ -11,7 +11,7 @@
 
 rp_module_id="lr-beetle-pcfx"
 rp_module_desc="PCFX emulator - Mednafen PCFX Port for libretro"
-rp_module_help="ROM Extensions: .img .iso .ccd .cue\n\nCopy the required BIOS file pcfx.bios to $biosdir"
+rp_module_help="ROM Extensions: .img .iso .ccd .cue\n\nCopy the required BIOS file pcfx.rom to $biosdir"
 rp_module_section="exp"
 
 function sources_lr-beetle-pcfx() {
