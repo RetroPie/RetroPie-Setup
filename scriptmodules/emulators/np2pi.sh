ccfb4945cd5fd30301c0c4904ec6f1df885eb496
NAKATA Maho <maho.nakata@gmail.com>
2017-01-29 06:56:55 +0000

    Fixed issues pointed out by joolswills.

--- scriptmodules/emulators/np2pi.sh
+++ scriptmodules/emulators/np2pi.sh
@@ -11,37 +11,27 @@
 
 rp_module_id="np2pi"
 rp_module_desc="NEC PC-9801 emulator"
-rp_module_help="ROM Extensions: .d88 .d98 .88d .98d .fdi .xdf .hdm .dup .2hd .tfd .hdi .thd .nhd .hdd .D88 .D98 .88D .98D .FDI .XDF .HDM .DUP .2HD .TFD .HDI .THD .NHD .HDD"
+rp_module_help="ROM Extensions: .d88 .d98 .88d .98d .fdi .xdf .hdm .dup .2hd .tfd .hdi .thd .nhd .hdd"
 rp_module_section="exp"
 
 function depends_np2pi() {
-    local depends=(libasound2-dev libsdl2-ttf-dev ttf-sazanami-gothic)
+    local depends=(libsdl1.2-dev libasound2-dev libsdl-ttf2.0-dev ttf-sazanami-gothic)
     getDepends "${depends[@]}"
 }
 
 function sources_np2pi() {
-    git clone -b np2pi https://github.com/irori/SDL12-kms-dispmanx.git
-    cd SDL12-kms-dispmanx
-    git checkout d96f55822880ac26eecc8684ca44194b7220b1de
-    cd ..
     git clone https://github.com/eagle0wl/np2pi.git
-    cd np2pi 
-    git checkout dabcc56e1fbf5f78b18a490810e93732681f36c7
 }
 
 function build_np2pi() {
-    cd SDL12-kms-dispmanx
-    ./MAC_ConfigureDISPMANX.sh
-    make -j 1 ; sudo make install 
-    cd ..
     cd np2pi/sdl
     make -j 1 -f makefile.rpi
 }
 
 function install_np2pi() {
-    sudo mkdir -p $md_inst
-    sudo cp np2pi/bin/np2 $md_inst
-    sudo ln -s /usr/share/fonts/truetype/sazanami/sazanami-gothic.ttf $md_inst/default.ttf
+    mkdir -p "$md_inst"
+    cp np2pi/bin/np2 "$md_inst"
+    ln -s /usr/share/fonts/truetype/sazanami/sazanami-gothic.ttf "$md_inst/default.ttf"
 }
 
 function configure_np2pi() {
@@ -52,9 +42,9 @@ function configure_np2pi() {
     for bios in 2608_bd.wav 2608_hh.wav 2608_rim.wav 2608_sd.wav 2608_tom.wav 2608_top.wav bios.rom FONT.ROM sound.rom; do
         ln -sf "$biosdir/np2pi/$bios" "$md_inst/$bios"
     done
-    sudo touch $md_conf_root/np2pi/np2.cfg
-    sudo chmod 666 $md_conf_root/np2pi/np2.cfg
-    sudo ln -s $md_conf_root/np2pi/np2.cfg "$md_inst/np2.cfg"
+    touch "$md_conf_root/np2pi/np2.cfg"
+    chmod 666 "$md_conf_root/np2pi/np2.cfg"
+    ln -s "$md_conf_root/np2pi/np2.cfg" "$md_inst/np2.cfg"
     setDispmanx "$md_id" 0
     addEmulator 1 "np2pi" "np2pi" "$md_inst/np2pi %ROM%"
     addSystem "np2pi"
