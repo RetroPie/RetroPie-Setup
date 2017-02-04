a56b2f73f4757b06b78a8646c86ea660b49aed97
Jools Wills <buzz@exotica.org.uk>
2017-02-04 15:24:34 +0000

    mupen64plus - use pushd/popd again since md_ret_require paths are relative

--- scriptmodules/emulators/mupen64plus.sh
+++ scriptmodules/emulators/mupen64plus.sh
@@ -76,12 +76,13 @@ function build_mupen64plus() {
 
     # build GLideN64
     "$md_build/GLideN64/src/getRevision.sh"
-    cd "$md_build/GLideN64/projects/cmake"
+    pushd "$md_build/GLideN64/projects/cmake"
     params=("-DMUPENPLUSAPI=On" "-DVEC4_OPT=On")
     isPlatform "neon" && params+=("-DNEON_OPT=On")
     isPlatform "rpi3" && params+=("-DCRC_ARMV8=On")
     cmake "${params[@]}" ../../src/
     make
+    popd
 
     rpSwap off
     md_ret_require=(
