005a5ee9307958a840356e0102ad3b6fb3b44698
Jools Wills <buzz@exotica.org.uk>
2017-02-07 16:48:14 +0000

    openbor - exclude from x11 targets for now (will re-visit after openbor update).

--- scriptmodules/ports/openbor.sh
+++ scriptmodules/ports/openbor.sh
@@ -13,7 +13,7 @@ rp_module_id="openbor"
 rp_module_desc="OpenBOR - Beat 'em Up Game Engine"
 rp_module_help="OpenBOR games need to be extracted to function properly. Place your pak files in $romdir/ports/openbor and then run $rootdir/ports/openbor/extract.sh. When the script is done, your original pak files will be found in $romdir/ports/openbor/originals and can be deleted."
 rp_module_section="exp"
-rp_module_flags="!mali"
+rp_module_flags="!mali !x11"
 
 function depends_openbor() {
     getDepends libsdl1.2-dev libsdl-gfx1.2-dev libogg-dev libvorbisidec-dev libvorbis-dev libpng12-dev zlib1g-dev
