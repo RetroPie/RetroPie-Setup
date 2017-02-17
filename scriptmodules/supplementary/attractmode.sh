f7d3545ca34c57c118cc52f9eb4bfbcc74a35f57
Jools Wills <buzz@exotica.org.uk>
2017-02-17 19:45:42 +0000

    attractmode - add remove_ function / check for launch script in case configs are in place but attractmode is removed

--- scriptmodules/supplementary/attractmode.sh
+++ scriptmodules/supplementary/attractmode.sh
@@ -20,7 +20,7 @@ function _get_configdir_attractmode() {
 
 function _add_system_attractmode() {
     local attract_dir="$(_get_configdir_attractmode)"
-    [[ ! -d "$attract_dir" ]] && return 0
+    [[ ! -d "$attract_dir" && -f /usr/bin/attract ]] && return 0
 
     local fullname="$1"
     local name="$2"
@@ -161,6 +161,9 @@ function install_attractmode() {
     cp -R attract/config/* "$md_inst/share/attract"
 }
 
+function remove_attractmode() {
+    rm -f /usr/bin/attract
+}
 
 function configure_attractmode() {
     moveConfigDir "$home/.attract" "$md_conf_root/all/attractmode"
