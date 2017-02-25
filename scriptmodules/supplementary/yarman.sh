91ad563f3d1335a2ef493e0e8b2a8b7659fb3eef
daeks <daeks@gmx.de>
2017-02-25 08:45:53 +0100

    rebrand

--- scriptmodules/supplementary/yarman.sh
+++ scriptmodules/supplementary/yarman.sh
@@ -10,20 +10,20 @@
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
-rp_module_id="webgui"
-rp_module_desc="RetroPie WebGUI on port 8080"
+rp_module_id="yarman"
+rp_module_desc=" YARMan Web (Yet Another RetroPie Manager) on port 8080"
 rp_module_help="PHP and JQuery based web frontend for managing your retropie installation"
 rp_module_section="exp"
 
-function depends_webgui() {
+function depends_yarman() {
     getDepends sqlite3 php5 php5-sqlite
 }
 
-function install_bin_webgui() {
-    gitPullOrClone "$md_inst" "https://github.com/daeks/RetroPie-WebGui"
+function install_bin_yarman() {
+    gitPullOrClone "$md_inst" "https://github.com/daeks/yarman"
 }
 
-function configure_webgui() {
+function configure_yarman() {
     killall php
     php -S "$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f8):8080" -t "$md_inst" > /dev/null 2>&1 &
 
@@ -31,7 +31,7 @@ function configure_webgui() {
     sed -i "s|^exit 0$|${config}\\nexit 0|" /etc/rc.local
 }
 
-function remove_webgui() {
+function remove_yarman() {
     killall php
     sed -i "/php/d" /etc/rc.local
     rm -R "$md_inst"
