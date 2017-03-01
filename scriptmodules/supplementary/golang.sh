7af1f6413ddbca472e64917773dc3f01dbbe500d
Jools Wills <buzz@exotica.org.uk>
2017-03-01 16:49:13 +0000

    goland - show untar progress as with other modules

--- scriptmodules/supplementary/golang.sh
+++ scriptmodules/supplementary/golang.sh
@@ -36,5 +36,5 @@ function install_bin_golang() {
         fi
     fi
     printMsgs "console" "Downloading go1.8.linux-$arch.tar.gz"
-    wget -q -O- "https://storage.googleapis.com/golang/go1.8.linux-$arch.tar.gz" | tar -xz -C "$md_inst"
+    wget -q -O- "https://storage.googleapis.com/golang/go1.8.linux-$arch.tar.gz" | tar -xvz -C "$md_inst"
 }
