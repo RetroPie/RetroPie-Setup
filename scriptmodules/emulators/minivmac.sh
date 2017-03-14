98670a8326ec77c04d8decc65dba3c03830159c7
Jools Wills <buzz@exotica.org.uk>
2017-03-14 21:27:12 +0000

    minivmac - add sdl2 dependency and enable for mali

--- scriptmodules/emulators/minivmac.sh
+++ scriptmodules/emulators/minivmac.sh
@@ -14,7 +14,11 @@ rp_module_desc="Macintosh Plus Emulator"
 rp_module_help="ROM Extensions: .dsk \n\nCopy your Macintosh Plus disks to $romdir/macintoshplus \n\n You need to copy the Macintosh bios file vMac.ROM into "$biosdir" and System Tools.dsk to $romdir"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/vanfanel/minivmac_sdl2/master/COPYING.txt"
 rp_module_section="exp"
-rp_module_flags="!mali"
+rp_module_flags=""
+
+function depends_minivmac() {
+    getDepends libsdl2-dev
+}
 
 function sources_minivmac() {
     gitPullOrClone "$md_build" https://github.com/vanfanel/minivmac_sdl2.git
