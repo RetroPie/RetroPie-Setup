4c8d0bfc1b09ac233c1e655d971139174bdf6302
gleam2003 <gleam2003@msn.com>
2017-04-15 10:32:24 +0200

    Add Replarform H3-mali
    Add support for Orange Pi device basend on Allwinner H3 CPU

--- scriptmodules/system.sh
+++ scriptmodules/system.sh
@@ -236,6 +236,9 @@ function get_platform() {
             ODROID-C2)
                 __platform="odroid-c2"
                 ;;
+            sun8i)
+                __platform="H3-mali"
+                ;;
             "Freescale i.MX6 Quad/DualLite (Device Tree)")
                 __platform="imx6"
                 ;;
@@ -332,4 +335,12 @@ function platform_imx6() {
     __default_asflags=""
     __default_makeflags="-j2"
     __platform_flags="arm armv7 neon"
+}
+
+function platform_H3-mali() {
+    __default_cflags="-O2 -march=armv7-a -mfpu=neon-vfpv4 -mfloat-abi=hard -ftree-vectorize -funsafe-math-optimizations"
+    __default_asflags=""
+    __default_makeflags="-j2"
+    __platform_flags="arm armv7 neon mali H3"
+    __has_binaries=0
 }
