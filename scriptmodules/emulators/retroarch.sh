574278822a8eda7a4e4c58113d0615a178fcf767
alerinoreis <alerino@gmail.com>
2017-05-03 11:39:08 +0200

    Updated retroarch.sh

--- scriptmodules/emulators/retroarch.sh
+++ scriptmodules/emulators/retroarch.sh
@@ -11,15 +11,21 @@
 
 rp_module_id="retroarch"
 rp_module_desc="RetroArch - frontend to the libretro emulator cores - required by all lr-* emulators"
+rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/RetroArch/master/COPYING"
 rp_module_section="core"
 
 function depends_retroarch() {
-    local depends=(libudev-dev libxkbcommon-dev libsdl2-dev libasound2-dev)
+    local depends=(libudev-dev libxkbcommon-dev libsdl2-dev libasound2-dev libusb-1.0-0-dev)
     isPlatform "rpi" && depends+=(libraspberrypi-dev)
     #isPlatform "mali" && depends+=(mali-fbdev)
-    isPlatform "x86" && depends+=(nvidia-cg-toolkit)
-    isPlatform "x11" && depends+=(libpulse-dev libavcodec-dev libavformat-dev libavdevice-dev)
-    compareVersions "$__os_release" ge 8  && depends+=(libusb-1.0-0-dev)
+    isPlatform "x11" && depends+=(libx11-xcb-dev libpulse-dev libavcodec-dev libavformat-dev libavdevice-dev)
+
+    # only install nvidia-cg-toolkit if it is available (as the non-free repo may not be enabled)
+    if isPlatform "x86"; then
+        if [[ -n "$(apt-cache search --names-only nvidia-cg-toolkit)" ]]; then
+            depends+=(nvidia-cg-toolkit)
+        fi
+    fi
 
     getDepends "${depends[@]}"
 
@@ -27,12 +33,15 @@ function depends_retroarch() {
 }
 
 function sources_retroarch() {
-    gitPullOrClone "$md_build" https://github.com/libretro/RetroArch.git
-    #if isPlatform "mali"; then
-        #sed -i 's|struct mali_native_window native_window|fbdev_window native_window|' gfx/drivers_context/mali_fbdev_ctx.c
-   # fi
+    gitPullOrClone "$md_build" https://github.com/libretro/RetroArch.git v1.5.0
     applyPatch "$md_data/01_hotkey_hack.diff"
     applyPatch "$md_data/02_disable_search.diff"
+    if isPlatform "rpi"; then
+        applyPatch "$md_data/03_dispmanx.diff"
+    fi
+    if isPlatform "mali"; then
+        applyPatch "$md_data/04_mali_struct.diff"
+    fi
 }
 
 function build_retroarch() {
@@ -40,7 +49,7 @@ function build_retroarch() {
     ! isPlatform "x11" && params+=(--disable-x11 --enable-opengles --disable-ffmpeg --disable-sdl --enable-sdl2 --disable-oss --disable-pulse --disable-al --disable-jack)
     isPlatform "rpi" && params+=(--enable-dispmanx)
     #isPlatform "mali" && params+=(--enable-mali_fbdev)
-    if isPlatform "arm" && ! isPlatform "64bit"; then
+    if isPlatform "arm"; then
         params+=(--enable-floathard)
         isPlatform "neon" && params+=(--enable-neon)
     fi
