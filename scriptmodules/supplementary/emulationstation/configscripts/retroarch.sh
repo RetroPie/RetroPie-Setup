786a044ce31b687fb1d415da9107a16080eb6536
Jools Wills <buzz@exotica.org.uk>
2017-06-02 09:18:20 +0100

    inputconfiguration - only set hotkey enable if select is set in when no hotkeyenable button is set.  * additional comments added to describe behaviour

--- scriptmodules/supplementary/emulationstation/configscripts/retroarch.sh
+++ scriptmodules/supplementary/emulationstation/configscripts/retroarch.sh
@@ -348,9 +348,12 @@ function map_retroarch_keyboard() {
 }
 
 function onend_retroarch_joystick() {
+    # if $_retroarch_select_hotkey is set here, then there was no hotkeyenable button
+    # in the configuration, so use the old behaviour of setting the select button to
+    # the hotkey enable button if set.
     if [[ "$_retroarch_select_hotkey" -eq 1 ]]; then
         iniGet "input_select_${_retroarch_select_type}"
-        iniSet "input_enable_hotkey_${_retroarch_select_type}" "$ini_value"
+        [[ -n "$ini_value" ]] && iniSet "input_enable_hotkey_${_retroarch_select_type}" "$ini_value"
     fi
 
     # hotkey sanity check
