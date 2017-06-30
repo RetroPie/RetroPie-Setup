62753c3b94fc39a497e09b4ac869d49e2a6418f3
meleu <meleu@users.noreply.github.com>
2017-07-01 00:50:41 +0100

    added option to update all installed themes

--- scriptmodules/supplementary/esthemes.sh
+++ scriptmodules/supplementary/esthemes.sh
@@ -96,6 +96,7 @@ function gui_esthemes() {
     )
     while true; do
         local theme
+        local installed_themes=()
         local repo
         local options=()
         local status=()
@@ -109,12 +110,15 @@ function gui_esthemes() {
             options+=("$i" "Download Theme Gallery")
         fi
         ((i++))
+        options+=("$((i++))" "Update all installed themes")
         for theme in "${themes[@]}"; do
             theme=($theme)
+            repo="${theme[0]}"
             theme="${theme[1]}"
             if [[ -d "/etc/emulationstation/themes/$theme" ]]; then
                 status+=("i")
                 options+=("$i" "Update or Uninstall $theme (installed)")
+                installed_themes+=("$theme $repo")
             else
                 status+=("n")
                 options+=("$i" "Install $theme (not installed)")
@@ -123,8 +127,8 @@ function gui_esthemes() {
         done
         local cmd=(dialog --backtitle "$__backtitle" --menu "Choose an option" 22 76 16)
         local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-        if [[ "$choice" > 1 ]]; then
-            theme=(${themes[choice-2]})
+        if [[ "$choice" > 2 ]]; then
+            theme=(${themes[choice-3]})
             repo="${theme[0]}"
             theme="${theme[1]}"
             if [[ "${status[choice-1]}" == "i" ]]; then
@@ -168,6 +172,11 @@ function gui_esthemes() {
             else
                 gitPullOrClone "$gallerydir" "http://github.com/wetriner/es-theme-gallery"
             fi
+        elif [[ "$choice" == 2 ]]; then
+            for theme in "${installed_themes[@]}"; do
+                theme=($theme)
+                rp_callModule esthemes install_theme "${theme[0]}" "${theme[1]}"
+            done
         else
             break
         fi
