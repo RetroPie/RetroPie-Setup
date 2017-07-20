55d143660c1df7ea94b9bb7eb2f8f58cef4316cb
Jools Wills <buzz@exotica.org.uk>
2017-07-20 21:45:34 +0100

    esthemes - use case statement to match other menus

--- scriptmodules/supplementary/esthemes.sh
+++ scriptmodules/supplementary/esthemes.sh
@@ -135,56 +135,60 @@ function gui_esthemes() {
         local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
         default="$choice"
         [[ -z "$choice" ]] && break
-        if [[ "$choice" == "G" ]]; then
-            if [[ "${status[0]}" == "i" ]]; then
-                options=(1 "View Theme Gallery" 2 "Update Theme Gallery" 3 "Remove Theme Gallery")
-                cmd=(dialog --backtitle "$__backtitle" --menu "Choose an option for gallery" 12 40 06)
-                local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-                case "$choice" in
-                    1)
-                        cd "$gallerydir"
-                        if isPlatform "x11"; then
-                            feh --info "echo %f" --slideshow-delay 6 --fullscreen --auto-zoom --filelist images.list
-                        else
-                            fbi --timeout 6 --once --autozoom --list images.list
-                        fi
-                        ;;
-                    2)
-                        gitPullOrClone "$gallerydir" "http://github.com/wetriner/es-theme-gallery"
-                        ;;
-                    3)
-                        if [[ -d "$gallerydir" ]]; then
-                            rm -rf "$gallerydir"
-                        fi
-                        ;;
-                esac
-            else
-                gitPullOrClone "$gallerydir" "http://github.com/wetriner/es-theme-gallery"
-            fi
-        elif [[ "$choice" == "U" ]]; then
-            for theme in "${installed_themes[@]}"; do
-                theme=($theme)
-                rp_callModule esthemes install_theme "${theme[0]}" "${theme[1]}"
-            done
-        else
-            theme=(${themes[choice-1]})
-            repo="${theme[0]}"
-            theme="${theme[1]}"
-            if [[ "${status[choice]}" == "i" ]]; then
-                options=(1 "Update $theme" 2 "Uninstall $theme")
-                cmd=(dialog --backtitle "$__backtitle" --menu "Choose an option for theme" 12 40 06)
-                local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-                case "$choice" in
-                    1)
-                        rp_callModule esthemes install_theme "$theme" "$repo"
-                        ;;
-                    2)
-                        rp_callModule esthemes uninstall_theme "$theme"
-                        ;;
-                esac
-            else
-                rp_callModule esthemes install_theme "$theme" "$repo"
-            fi
-        fi
+        case "$choice" in
+            G)
+                if [[ "${status[0]}" == "i" ]]; then
+                    options=(1 "View Theme Gallery" 2 "Update Theme Gallery" 3 "Remove Theme Gallery")
+                    cmd=(dialog --backtitle "$__backtitle" --menu "Choose an option for gallery" 12 40 06)
+                    local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+                    case "$choice" in
+                        1)
+                            cd "$gallerydir"
+                            if isPlatform "x11"; then
+                                feh --info "echo %f" --slideshow-delay 6 --fullscreen --auto-zoom --filelist images.list
+                            else
+                                fbi --timeout 6 --once --autozoom --list images.list
+                            fi
+                            ;;
+                        2)
+                            gitPullOrClone "$gallerydir" "http://github.com/wetriner/es-theme-gallery"
+                            ;;
+                        3)
+                            if [[ -d "$gallerydir" ]]; then
+                                rm -rf "$gallerydir"
+                            fi
+                            ;;
+                    esac
+                else
+                    gitPullOrClone "$gallerydir" "http://github.com/wetriner/es-theme-gallery"
+                fi
+                ;;
+            U)
+                for theme in "${installed_themes[@]}"; do
+                    theme=($theme)
+                    rp_callModule esthemes install_theme "${theme[0]}" "${theme[1]}"
+                done
+                ;;
+            *)
+                theme=(${themes[choice-1]})
+                repo="${theme[0]}"
+                theme="${theme[1]}"
+                if [[ "${status[choice]}" == "i" ]]; then
+                    options=(1 "Update $theme" 2 "Uninstall $theme")
+                    cmd=(dialog --backtitle "$__backtitle" --menu "Choose an option for theme" 12 40 06)
+                    local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+                    case "$choice" in
+                        1)
+                            rp_callModule esthemes install_theme "$theme" "$repo"
+                            ;;
+                        2)
+                            rp_callModule esthemes uninstall_theme "$theme"
+                            ;;
+                    esac
+                else
+                    rp_callModule esthemes install_theme "$theme" "$repo"
+                fi
+                ;;
+        esac
     done
 }
