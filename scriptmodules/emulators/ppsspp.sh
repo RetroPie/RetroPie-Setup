686d2133dbfd974779fd704db6801412c854cd9d
Jools Wills <buzz@exotica.org.uk>
2017-08-16 21:57:52 +0100

    ppsspp - fix cmake extract path after downloadAndExtract changes

--- scriptmodules/emulators/ppsspp.sh
+++ scriptmodules/emulators/ppsspp.sh
@@ -36,7 +36,7 @@ function sources_ppsspp() {
     if hasPackage cmake 3.6 lt; then
         cd ..
         mkdir -p cmake
-        downloadAndExtract "$__archive_url/cmake-3.6.2.tar.gz" "$md_build/ppsspp/cmake" 1
+        downloadAndExtract "$__archive_url/cmake-3.6.2.tar.gz" "$md_build/cmake" 1
     fi
 }
 
