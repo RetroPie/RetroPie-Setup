8b8bca2d99d194aba590acb4e63faac247966deb
gizmo98 <atz.SE@gmx.de>
2017-08-23 18:58:28 +0200

    Fix typo

--- scriptmodules/emulators/retroarch.sh
+++ scriptmodules/emulators/retroarch.sh
@@ -41,7 +41,7 @@ function sources_retroarch() {
     applyPatch "$md_data/01_hotkey_hack.diff"
     applyPatch "$md_data/02_disable_search.diff"
 
-    isPlatform "rpi" && && sed -i 's/#extension GL_OES_standard_derivatives : enable//g' "$md_build/gfx/drivers/gl_shaders/shaders_common.h"
+    isPlatform "rpi" && sed -i 's/#extension GL_OES_standard_derivatives : enable//g' "$md_build/gfx/drivers/gl_shaders/shaders_common.h"
 }
 
 function build_retroarch() {
