65d143377f659a45aa0398be6261e683fad8b5f6
gizmo98 <atz.SE@gmx.de>
2017-08-27 12:02:29 +0200

    reicast: try to remove function calls only

--- scriptmodules/emulators/reicast.sh
+++ scriptmodules/emulators/reicast.sh
@@ -26,8 +26,8 @@ function sources_reicast() {
         gitPullOrClone "$md_build" https://github.com/reicast/reicast-emulator.git
         sed -i "s|LIBS += -L/opt/vc/lib/  -L../linux-deps/lib -lbcm_host|USE_SDL := 1|g" "$md_build/shell/linux/Makefile"
         sed -i "s|INCS += -I/opt/vc/include/ -I/opt/vc/include/interface/vmcs_host/linux -I/opt/vc/include/interface/vcos/pthreads -I../linux-deps/include||g" "$md_build/shell/linux/Makefile"
-        sed -i 's|enable_runfast()|//enable_runfast()|g' "$md_build/core/linux/common.cpp"
-        sed -i 's|linux_rpi2_init()|//linux_rpi2_init()|g' "$md_build/core/linux/common.cpp"
+        sed -i 's|enable_runfast();|//enable_runfast();|g' "$md_build/core/linux/common.cpp"
+        sed -i 's|linux_rpi2_init();|//linux_rpi2_init();|g' "$md_build/core/linux/common.cpp"
     else
         gitPullOrClone "$md_build" https://github.com/RetroPie/reicast-emulator.git retropie
     fi
