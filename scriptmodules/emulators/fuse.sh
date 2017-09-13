c7003a3a2c54bf22789435cf742b2264cb07a0d6
Jools Wills <buzz@exotica.org.uk>
2017-09-13 18:24:01 +0100

    fuse - update to v1.4.0

--- scriptmodules/emulators/fuse.sh
+++ scriptmodules/emulators/fuse.sh
@@ -21,9 +21,9 @@ function depends_fuse() {
 }
 
 function sources_fuse() {
-    downloadAndExtract "$__archive_url/fuse-1.3.7.tar.gz" "$md_build" 1
+    downloadAndExtract "$__archive_url/fuse-1.4.0.tar.gz" "$md_build" 1
     mkdir libspectrum
-    downloadAndExtract "$__archive_url/libspectrum-1.3.5.tar.gz" "$md_build/libspectrum" 1
+    downloadAndExtract "$__archive_url/libspectrum-1.4.0.tar.gz" "$md_build/libspectrum" 1
     if ! isPlatform "x11"; then
         applyPatch cursor.diff <<\_EOF_
 --- a/ui/sdl/sdldisplay.c	2015-02-18 22:39:05.631516602 +0000
@@ -45,7 +45,7 @@ function build_fuse() {
     make clean
     make
     popd
-    CFLAGS+=" -I$md_build/libspectrum" LDFLAGS+=" -L$md_build/libspectrum/.libs" ./configure --prefix="$md_inst" --without-libao --without-gpm --without-gtk --without-libxml2 --with-sdl
+    ./configure --prefix="$md_inst" --without-libao --without-gpm --without-gtk --without-libxml2 --with-sdl LIBSPECTRUM_CFLAGS="-I$md_build/libspectrum" LIBSPECTRUM_LIBS="-L$md_build/libspectrum/.libs -lspectrum"
     make clean
     make
     md_ret_require="$md_build/fuse"
