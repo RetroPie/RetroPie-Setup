af49cdc0822f82638ac8c085722d04cdc28e757b
Jools Wills <buzz@exotica.org.uk>
2017-09-24 11:30:15 +0100

    xm7 - rework very broken module module  * added missing dependencies  * use downloadAndExtract  * fix up sources    * deprecated _BSD_SOURCE define that breaks configure    * fix needed for psg.cpp    * use first frame of icon in conversion script to avoid having wrong output filenames    * missing X11 link library  * various other code cleanups

--- scriptmodules/emulators/xm7.sh
+++ scriptmodules/emulators/xm7.sh
@@ -17,41 +17,48 @@ rp_module_section="exp"
 rp_module_flags="dispmanx !mali"
 
 function depends_xm7() {
-    getDepends libjpeg-dev libsdl1.2-dev libtool libpng12-dev libfreetype6-dev gawk fonts-takao
+    getDepends libjpeg-dev libsdl1.2-dev libsdl-mixer1.2-dev libtool libpng12-dev libuim-dev libfreetype6-dev libfontconfig1-dev gawk fonts-takao libxinerama-dev libx11-dev imagemagick
 }
 
 function sources_xm7() {
-    wget http://stable.hypertriton.com/agar/agar-1.5.0.tar.gz
-    gitPullOrClone "${md_build}/XM7-for-SDL" https://github.com/nakatamaho/XM7-for-SDL.git
+    gitPullOrClone "$md_build" https://github.com/nakatamaho/XM7-for-SDL.git
+    mkdir -p "$md_build/agar"
+    downloadAndExtract "http://stable.hypertriton.com/agar/agar-1.5.0.tar.gz" "$md_build/agar" 1
+    # _BSD_SOURCE is deprecated and will throw an error during configure
+    sed -i "s/_BSD_SOURCE/_DEFAULT_SOURCE/g" "$md_build/agar/configure"
+    # needs libx11 to link
+    applyPatch "$md_data/01_fix_build.diff"
 }
 
 function build_xm7() {
-    tar xvfz agar-1.5.0.tar.gz
-    cd agar-1.5.0
-    ./configure --prefix="$md_build"
-    make -j1 depend
-    make -j1 all
-    make -j1 install
-    cd "$md_build"/XM7-for-SDL
+    cd agar
+    ./configure --disable-shared --prefix="$md_build/libagar"
+    make -j1 depend all install
+    cd "$md_build"
     mkdir linux-sdl/build
     cd linux-sdl/build
-    cmake -DCMAKE_CXX_FLAGS="-DSHAREDIR='\"${md_inst}/share/xm7\"'" -DCMAKE_INSTALL_PREFIX:PATH="$md_inst" -DCMAKE_BUILD_TYPE=Release -DUSE_OPENCL=No -DUSE_OPENGL=No -DWITH_LIBAGAR_PREFIX="$md_build" -DWITH_AGAR_STATIC=yes ..
+    cmake -DCMAKE_CXX_FLAGS="-DSHAREDIR='\"${md_inst}/share/xm7\"'" -DCMAKE_INSTALL_PREFIX:PATH="$md_inst" -DCMAKE_BUILD_TYPE=Release -DUSE_OPENCL=No -DUSE_OPENGL=No -DWITH_LIBAGAR_PREFIX="$md_build/libagar" -DWITH_AGAR_STATIC=yes ..
     make
+    md_ret_require="$md_build/linux-sdl/build/sdl/xm7"
 }
 
 function install_xm7() {
-    cd XM7-for-SDL/linux-sdl/build
-    make install
+    cd linux-sdl/build
+    make -j1 install VERBOSE=1
 }
 
 function configure_xm7() {
     mkRomDir "fm7"
+
     moveConfigDir "$home/.xm7" "$md_conf_root/fm7"
+
     mkUserDir "$biosdir/fm7"
+
     local bios
     for bios in DICROM.ROM EXTSUB.ROM FBASIC30.ROM INITIATE.ROM KANJI1.ROM KANJI2.ROM SUBSYS_A.ROM SUBSYS_B.ROM SUBSYSCG.ROM SUBSYS_C.ROM fddseek.wav relayoff.wav relay_on.wav; do
         ln -sf "$biosdir/fm7/$bios" "$md_conf_root/fm7/$bios"
     done
+
     addEmulator 1 "$md_id" "fm7" "$md_inst/bin/xm7 %ROM%"
     addSystem "fm7"
 }
