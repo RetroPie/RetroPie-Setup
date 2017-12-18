37ab2a9210bb4452c7ba1d5df7cfb5f09c5509ee
sikotik <33843126+sikotik@users.noreply.github.com>
2017-12-18 13:07:37 -0800

    Update system.sh
    enabled kms changed -mcpu to cortex-a15 added -mtune=cortex-a15.cortex-a7

--- scriptmodules/system.sh
+++ scriptmodules/system.sh
@@ -300,12 +300,12 @@ function platform_odroid-c2() {
 }
 
 function platform_odroid-xu() {
-    __default_cflags="-O2 -mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard -ftree-vectorize -funsafe-math-optimizations"
+    __default_cflags="-O2 -mcpu=cortex-a15 -mtune=cortex-a15.cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard -ftree-vectorize -funsafe-math-optimizations"
     # required for mali-fbdev headers to define GL functions
     __default_cflags+=" -DGL_GLEXT_PROTOTYPES"
     __default_asflags=""
     __default_makeflags="-j2"
-    __platform_flags="arm armv7 neon mali gles"
+    __platform_flags="arm armv7 neon kms gles"
 }
 
 function platform_tinker() {
