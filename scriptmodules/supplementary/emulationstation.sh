3b9eca6770827a9f8dcdc0b94f23b4088c48437b
sikotik <33843126+sikotik@users.noreply.github.com>
2017-12-18 13:47:49 -0800

    Update emulationstation.sh
    pull from our repo

--- scriptmodules/supplementary/emulationstation.sh
+++ scriptmodules/supplementary/emulationstation.sh
@@ -136,7 +136,7 @@ function depends_emulationstation() {
 function sources_emulationstation() {
     local repo="$1"
     local branch="$2"
-    [[ -z "$repo" ]] && repo="https://github.com/RetroPie/EmulationStation"
+    [[ -z "$repo" ]] && repo="https://github.com/Odroid-RetroArena/EmulationStation"
     [[ -z "$branch" ]] && branch="stable"
     gitPullOrClone "$md_build" "$repo" "$branch"
 }
