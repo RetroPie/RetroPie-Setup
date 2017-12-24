3a81d27f23419eb39b1739411872103b051b1482
Conn O'Griofa <connogriofa@gmail.com>
2017-12-24 08:15:35 +0000

    dosbox: update to r4006 & cleanups
    Changes:
    * Update to SVN r4006
    * Fetch original source via subversion repository directly
    * Clean up functions to allow support for dosbox variants,
      ensure launcher script is removed and prevent config file from
      being generated during uninstall phase.
    * Ensure .sh scripts have timidity daemon running when needed

--- scriptmodules/emulators/dosbox.sh
+++ scriptmodules/emulators/dosbox.sh
@@ -11,24 +11,31 @@
 
 rp_module_id="dosbox"
 rp_module_desc="DOS emulator"
-rp_module_help="ROM Extensions: .bat .com .exe .sh\n\nCopy your DOS games to $romdir/pc"
+rp_module_help="ROM Extensions: .bat .com .exe .sh .conf\n\nCopy your DOS games to $romdir/pc"
 rp_module_licence="GPL2 https://sourceforge.net/p/dosbox/code-0/HEAD/tree/dosbox/trunk/COPYING"
 rp_module_section="opt"
 rp_module_flags="dispmanx !mali !kms"
 
 function depends_dosbox() {
-    local depends=(libsdl1.2-dev libsdl-net1.2-dev libsdl-sound1.2-dev libasound2-dev libpng12-dev automake autoconf zlib1g-dev)
+    local depends=(libsdl1.2-dev libsdl-net1.2-dev libsdl-sound1.2-dev libasound2-dev libpng12-dev automake autoconf zlib1g-dev subversion "$@")
     isPlatform "rpi" && depends+=(timidity freepats)
     getDepends "${depends[@]}"
 }
 
 function sources_dosbox() {
-    downloadAndExtract "$__archive_url/dosbox-r3876.tar.gz" "$md_build" 1
+    local revision="$1"
+    [[ -z "$revision" ]] && revision="4006"
+
+    svn checkout https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk "$md_build" -r "$revision"
 }
 
 function build_dosbox() {
     local params=()
+
     ! isPlatform "x11" && params+=(--disable-opengl)
+    # add or override params from calling function
+    params+=("$@")
+
     ./autogen.sh
     ./configure --prefix="$md_inst" "${params[@]}"
     if isPlatform "arm"; then
@@ -52,14 +59,17 @@ function install_dosbox() {
 }
 
 function configure_dosbox() {
-    local needs_synth
-
-    # needs software synth for midi
-    isPlatform "rpi" && needs_synth=1
+    local def="1"
+    local launcher_name="+Start DOSBox.sh"
+    # needs software synth for midi; limit to Pi for now
+    if isPlatform "rpi"; then
+        local needs_synth="1"
+    fi
 
     mkRomDir "pc"
-    rm -f "$romdir/pc/Start DOSBox.sh"
-    cat > "$romdir/pc/+Start DOSBox.sh" << _EOF_
+    rm -f "$romdir/pc/$launcher_name"
+    if [[ "$md_mode" == "install" ]]; then
+        cat > "$romdir/pc/$launcher_name" << _EOF_
 #!/bin/bash
 
 [[ ! -n "\$(aconnect -o | grep -e TiMidity -e FluidSynth)" ]] && needs_synth="$needs_synth"
@@ -86,7 +96,9 @@ params=("\$@")
 if [[ -z "\${params[0]}" ]]; then
     params=(-c "@MOUNT C $romdir/pc" -c "@C:")
 elif [[ "\${params[0]}" == *.sh ]]; then
+    midi_synth start
     bash "\${params[@]}"
+    midi_synth stop
     exit
 elif [[ "\${params[0]}" == *.conf ]]; then
     params=(-userconf -conf "\${params[@]}")
@@ -98,24 +110,25 @@ midi_synth start
 "$md_inst/bin/dosbox" "\${params[@]}"
 midi_synth stop
 _EOF_
-    chmod +x "$romdir/pc/+Start DOSBox.sh"
-    chown $user:$user "$romdir/pc/+Start DOSBox.sh"
+        chmod +x "$romdir/pc/$launcher_name"
+        chown $user:$user "$romdir/pc/$launcher_name"
 
-    local config_path=$(su "$user" -c "\"$md_inst/bin/dosbox\" -printconf")
-    if [[ -f "$config_path" ]]; then
-        iniConfig "=" "" "$config_path"
-        iniSet "usescancodes" "false"
-        iniSet "core" "dynamic"
-        iniSet "cycles" "max"
-        iniSet "scaler" "none"
-        if isPlatform "rpi" || [[ -n "$(aconnect -o | grep -e TiMidity -e FluidSynth)" ]]; then
-            iniSet "mididevice" "alsa"
-            iniSet "midiconfig" "128:0"
+        local config_path=$(su "$user" -c "\"$md_inst/bin/dosbox\" -printconf")
+        if [[ -f "$config_path" ]]; then
+            iniConfig "=" "" "$config_path"
+            iniSet "usescancodes" "false"
+            iniSet "core" "dynamic"
+            iniSet "cycles" "max"
+            iniSet "scaler" "none"
+            if isPlatform "rpi" || [[ -n "$(aconnect -o | grep -e TiMidity -e FluidSynth)" ]]; then
+                iniSet "mididevice" "alsa"
+                iniSet "midiconfig" "128:0"
+            fi
         fi
     fi
 
-    moveConfigDir "$home/.dosbox" "$md_conf_root/pc"
+    moveConfigDir "$home/.$md_id" "$md_conf_root/pc"
 
-    addEmulator 1 "$md_id" "pc" "bash $romdir/pc/+Start\ DOSBox.sh %ROM%"
+    addEmulator "$def" "$md_id" "pc" "bash $romdir/pc/${launcher_name// /\\ } %ROM%"
     addSystem "pc"
 }
