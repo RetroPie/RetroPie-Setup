640af36633162d2be9d749d32531c83f376e91fc
Jools Wills <buzz@exotica.org.uk>
2017-12-28 10:07:47 +0000

    retroarch - update to v1.7.0  * update our hotkey/disable search patches to apply to v1.7.0

--- scriptmodules/emulators/retroarch.sh
+++ scriptmodules/emulators/retroarch.sh
@@ -33,7 +33,7 @@ function depends_retroarch() {
 }
 
 function sources_retroarch() {
-    gitPullOrClone "$md_build" https://github.com/libretro/RetroArch.git v1.6.9
+    gitPullOrClone "$md_build" https://github.com/libretro/RetroArch.git v1.7.0
     applyPatch "$md_data/01_hotkey_hack.diff"
     applyPatch "$md_data/02_disable_search.diff"
 }
