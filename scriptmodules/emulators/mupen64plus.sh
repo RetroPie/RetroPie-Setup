c29e5d41b58dfd9219c429b72fc726395d7897b1
sikotik <33843126+sikotik@users.noreply.github.com>
2018-01-07 18:47:49 -0800

    Add files via upload
    Enabled for Mali. Needs work

--- scriptmodules/emulators/mupen64plus.sh
+++ scriptmodules/emulators/mupen64plus.sh
@@ -14,7 +14,7 @@ rp_module_desc="N64 emulator MUPEN64Plus"
 rp_module_help="ROM Extensions: .z64 .n64 .v64\n\nCopy your N64 roms to $romdir/n64"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/mupen64plus/mupen64plus-core/master/LICENSES"
 rp_module_section="main"
-rp_module_flags="!mali !kms"
+rp_module_flags=" !kms"
 
 function depends_mupen64plus() {
     local depends=(cmake libsamplerate0-dev libspeexdsp-dev libsdl2-dev libpng12-dev)
@@ -31,19 +31,21 @@ function sources_mupen64plus() {
         'mupen64plus input-sdl'
         'mupen64plus rsp-hle'
     )
-    if isPlatform "rpi"; then
+    if isPlatform "mali"; then
         repos+=(
-            'gizmo98 audio-omx'
+            
             'ricrpi video-gles2rice pandora-backport'
-            'ricrpi video-gles2n64'
-        )
-    else
-        repos+=(
             'mupen64plus video-glide64mk2'
             'mupen64plus rsp-cxd4'
             'mupen64plus rsp-z64'
+			'mupen64plus video-rice'
+			'mupen64plus video-arachnoid'
+			'mupen64plus video-z64'
+			'mupen64plus video-glide64'
         )
-    fi
+    
+           
+         fi
     local repo
     local dir
     for repo in "${repos[@]}"; do
@@ -66,8 +68,7 @@ function build_mupen64plus() {
         if [[ -f "$dir/projects/unix/Makefile" ]]; then
             make -C "$dir/projects/unix" clean
             params=()
-            isPlatform "rpi1" && params+=("VFP=1" "VFP_HARD=1" "HOST_CPU=armv6")
-            isPlatform "rpi" && params+=("VC=1")
+            isPlatform "mali" && params+=("VFP=1" "VFP_HARD=1" "HOST_CPU=armv7a" "USE_GLES=1" )
             isPlatform "neon" && params+=("NEON=1")
             isPlatform "x11" && params+=("OSD=1" "PIE=1")
             isPlatform "x86" && params+=("SSE=SSE2")
@@ -82,7 +83,7 @@ function build_mupen64plus() {
     pushd "$md_build/GLideN64/projects/cmake"
     params=("-DMUPENPLUSAPI=On" "-DVEC4_OPT=On")
     isPlatform "neon" && params+=("-DNEON_OPT=On")
-    isPlatform "rpi" && params+=("-DUSE_SYSTEM_LIBS=On")
+    isPlatform "mali" && params+=("-DUSE_SYSTEM_LIBS=On" "-DODROID=ON" "-DGLES2=ON" "-DEGL=ON")
     if isPlatform "rpi3"; then 
         params+=("-DCRC_ARMV8=On")
     else
@@ -101,11 +102,14 @@ function build_mupen64plus() {
         'mupen64plus-rsp-hle/projects/unix/mupen64plus-rsp-hle.so'
         'GLideN64/projects/cmake/plugin/release/mupen64plus-video-GLideN64.so'
     )
-    if isPlatform "rpi"; then
+    if isPlatform "mali"; then
         md_ret_require+=(
             'mupen64plus-video-gles2rice/projects/unix/mupen64plus-video-rice.so'
-            'mupen64plus-video-gles2n64/projects/unix/mupen64plus-video-n64.so'
-            'mupen64plus-audio-omx/projects/unix/mupen64plus-audio-omx.so'
+            'mupen64plus-video-glide64mk2/projects/unix/mupen64plus-video-glide64mk2.so'
+            'mupen64plus-rsp-z64/projects/unix/mupen64plus-rsp-z64.so'
+			'mupen64plus-video-arachnoid/projects/unix/mupen64plus-video-arachnoid.so'
+			'mupen64plus-video-z64/projects/unix/mupen64plus-video-z64.so'
+			'mupen64plus-video-glide64/projects/unix/mupen64plus-video-glide64.so'
         )
     else
         md_ret_require+=(
@@ -125,8 +129,8 @@ function install_mupen64plus() {
         if [[ -f "$source/projects/unix/Makefile" ]]; then
             # optflags is needed due to the fact the core seems to rebuild 2 files and relink during install stage most likely due to a buggy makefile
             local params=()
-            isPlatform "armv6" && params+=("VFP=1" "HOST_CPU=armv6")
-            isPlatform "rpi" && params+=("VC=1")
+            isPlatform "armv7" && params+=("VFP=1" "HOST_CPU=armv7")
+            isPlatform "mali" && params+=("VFP=1" "VFP_HARD=1" "HOST_CPU=armv7")
             isPlatform "neon" && params+=("NEON=1")
             isPlatform "x86" && params+=("SSE=SSE2")
             make -C "$source/projects/unix" PREFIX="$md_inst" OPTFLAGS="$CFLAGS -O3 -flto" "${params[@]}" install
@@ -140,13 +144,14 @@ function install_mupen64plus() {
 }
 
 function configure_mupen64plus() {
-    if isPlatform "rpi"; then
+    if isPlatform "mali"; then
         local res
         for res in "320x240" "640x480"; do
             local name=""
             [[ "$res" == "640x480" ]] && name="-highres"
             addEmulator 0 "${md_id}-GLideN64$name" "n64" "$md_inst/bin/mupen64plus.sh mupen64plus-video-GLideN64 %ROM% $res"
             addEmulator 0 "${md_id}-gles2rice$name" "n64" "$md_inst/bin/mupen64plus.sh mupen64plus-video-rice %ROM% $res"
+			
         done
         addEmulator 0 "${md_id}-gles2n64" "n64" "$md_inst/bin/mupen64plus.sh mupen64plus-video-n64 %ROM%"
         addEmulator 1 "${md_id}-auto" "n64" "$md_inst/bin/mupen64plus.sh AUTO %ROM%"
