9418066b696ceac0eabdf25dd88e263835fea13f
sikotik <33843126+sikotik@users.noreply.github.com>
2018-01-19 14:57:52 -0800

    Add files via upload

---
+++ scriptmodules/libretrocores/lr-mame2003-plus.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="lr-mame2003-plus"
+rp_module_desc="Arcade emu - updated MAME 0.78 port for libretro with added game support"
+rp_module_help="ROM Extension: .zip\n\nCopy your MAME roms to either $romdir/mame-libretro or\n$romdir/arcade"
+rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro-mirrors/mame2003-plus-libretro/master/docs/mame.txt"
+rp_module_section="exp"
+
+function sources_lr-mame2003-plus() {
+    gitPullOrClone "$md_build" https://github.com/libretro-mirrors/mame2003-plus-libretro.git
+}
+
+function build_lr-mame2003-plus() {
+    build_lr-mame2003
+}
+
+function install_lr-mame2003-plus() {
+    install_lr-mame2003
+}
+
+function configure_lr-mame2003-plus() {
+    configure_lr-mame2003
+}
