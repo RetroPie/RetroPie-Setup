e99bab6281d625c369279b094080887e11e479a5
sikotik <33843126+sikotik@users.noreply.github.com>
2018-01-20 11:51:47 -0800

    Update emulationstation-ora.sh

--- scriptmodules/supplementary/emulationstation-ora.sh
+++ scriptmodules/supplementary/emulationstation-ora.sh
@@ -9,8 +9,8 @@
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
-rp_module_id="emulationstation"
-rp_module_desc="EmulationStation - Frontend used by RetroPie for launching emulators"
+rp_module_id="emulationstation-ora"
+rp_module_desc="EmulationStation - Frontend used by RetroPie for launching emulators ora edition"
 rp_module_licence="MIT https://raw.githubusercontent.com/RetroPie/EmulationStation/master/LICENSE.md"
 rp_module_section="core"
 rp_module_flags="frontend"
