aa73dc8193582c67d9d22a00a1ec0b569dd7995b
Jools Wills <buzz@exotica.org.uk>
2018-01-27 13:04:15 +0000

    ppsspp - fix building on stretch (egl library name fix from psyke83)

--- scriptmodules/emulators/ppsspp.sh
+++ scriptmodules/emulators/ppsspp.sh
@@ -26,6 +26,8 @@ function sources_ppsspp() {
     gitPullOrClone "$md_build/ppsspp" https://github.com/hrydgard/ppsspp.git v1.5.4
     cd ppsspp
 
+    applyPatch "$md_data/01_egl_name.diff"
+
     # remove the lines that trigger the ffmpeg build script functions - we will just use the variables from it
     sed -i "/^build_ARMv6$/,$ d" ffmpeg/linux_arm.sh
 
