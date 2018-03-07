db4ec6e4fcf90ead58a6f1baaaf9b1d266b54bcf
Jools Wills <buzz@exotica.org.uk>
2018-03-07 19:19:23 +0000

    helpers / downloadAndExtract - make destination folder for tar archives

--- scriptmodules/helpers.sh
+++ scriptmodules/helpers.sh
@@ -954,6 +954,7 @@ function downloadAndExtract() {
     esac
 
     if [[ "$is_tar" -eq 1 ]]; then
+        mkdir -p "$dest"
         cmd+=(-C "$dest")
         [[ -n "$opts" ]] && cmd+=(--strip-components "$opts")
 
