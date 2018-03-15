609bbaaa956886427939bb363ccc2e3920882933
Jools Wills <buzz@exotica.org.uk>
2018-03-15 01:33:42 +0000

    image - remove RetroPie-Setup/tmp folder after main install

--- scriptmodules/admin/image.sh
+++ scriptmodules/admin/image.sh
@@ -125,6 +125,8 @@ for module in "\${modules[@]}"; do
     # rpi1 platform would use QEMU_CPU set to arm1176, but it seems buggy currently (lots of segfaults)
     sudo QEMU_CPU=cortex-a15 __platform=$platform __nodialog=1 ./retropie_packages.sh \$module
 done
+
+rm -rf tmp
 sudo apt-get clean
 _EOF_
 
