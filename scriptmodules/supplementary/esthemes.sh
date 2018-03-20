1e1679a065c86bfea71dac593d3b6e87b892a229
Jools Wills <buzz@exotica.org.uk>
2018-03-20 15:37:06 +0000

    esthemes - added SuperMagicom nostalgic theme - fixes #2332

--- scriptmodules/supplementary/esthemes.sh
+++ scriptmodules/supplementary/esthemes.sh
@@ -129,6 +129,7 @@ function gui_esthemes() {
         'RetroHursty69 tributeSWars'        
         'Saracade scv720'
         'chicueloarcade Chicuelo'
+        'SuperMagicom nostalgic'
     )
     while true; do
         local theme
