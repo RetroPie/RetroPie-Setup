721775acc87cc94711a94c1398a178f0a8bddb68
Jools Wills <buzz@exotica.org.uk>
2018-03-20 02:54:25 +0000

    mupen64plus - add the parameters to the "make clean" so no dependency errors are thrown during clean operation

--- scriptmodules/emulators/mupen64plus.sh
+++ scriptmodules/emulators/mupen64plus.sh
@@ -68,7 +68,6 @@ function build_mupen64plus() {
     local params=()
     for dir in *; do
         if [[ -f "$dir/projects/unix/Makefile" ]]; then
-            make -C "$dir/projects/unix" clean
             params=()
             isPlatform "rpi1" && params+=("VFP=1" "VFP_HARD=1" "HOST_CPU=armv6")
             isPlatform "rpi" && params+=("VC=1")
@@ -76,6 +75,7 @@ function build_mupen64plus() {
             isPlatform "x11" && params+=("OSD=1" "PIE=1")
             isPlatform "x86" && params+=("SSE=SSE2")
             [[ "$dir" == "mupen64plus-ui-console" ]] && params+=("COREDIR=$md_inst/lib/" "PLUGINDIR=$md_inst/lib/mupen64plus/")
+            make -C "$dir/projects/unix" "${params[@]}" clean
             # MAKEFLAGS replace removes any distcc from path, as it segfaults with cross compiler and lto
             MAKEFLAGS="${MAKEFLAGS/\/usr\/lib\/distcc:/}" make -C "$dir/projects/unix" all "${params[@]}" OPTFLAGS="$CFLAGS -O3 -flto"
         fi
