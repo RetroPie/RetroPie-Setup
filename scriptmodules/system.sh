b2f023b83e4b27fcba6ba378920e33eeca0686fd
JyuHo <37599940+JyuHo@users.noreply.github.com>
2018-05-13 00:18:53 +0200

    Update system.sh
    Update platform tinker

--- scriptmodules/system.sh
+++ scriptmodules/system.sh
@@ -310,7 +310,7 @@ function platform_odroid-xu() {
 }
 
 function platform_tinker() {
-    __default_cflags="-O2 -mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard -ftree-vectorize -funsafe-math-optimizations"
+    __default_cflags="-O2 -marm -march=armv7-a -mtune=cortex-a17 -mfpu=neon-vfpv4 -mfloat-abi=hard -ftree-vectorize -funsafe-math-optimizations"
     # required for mali headers to define GL functions
     __default_cflags+=" -DGL_GLEXT_PROTOTYPES"
     __default_asflags=""
