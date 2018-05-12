202f4e35a8cb6c26955dfec9b5eacdd9a5cc9df7
JyuHo <37599940+JyuHo@users.noreply.github.com>
2018-05-13 00:22:37 +0200

    Update lr-mupen64plus.sh
    Add params for tinker

--- scriptmodules/libretrocores/lr-mupen64plus.sh
+++ scriptmodules/libretrocores/lr-mupen64plus.sh
@@ -54,6 +54,7 @@ function build_lr-mupen64plus() {
         isPlatform "arm" && params+=(WITH_DYNAREC=arm)
         isPlatform "neon" && params+=(HAVE_NEON=1)
         isPlatform "gles" && params+=(FORCE_GLES=1)
+        isPlatform "kms" && params+=(FORCE_GLES3=1)
     fi
     make clean
     make "${params[@]}"
