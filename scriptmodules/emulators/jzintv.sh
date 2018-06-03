67f287ebaa67501e050ce3b0a709bbc0948dc80c
sikotik <33843126+sikotik@users.noreply.github.com>
2018-06-03 13:30:27 -0700

    enabled mali
    removed !mali flag. Set resolution to 1080p

--- scriptmodules/emulators/jzintv.sh
+++ scriptmodules/emulators/jzintv.sh
@@ -14,7 +14,7 @@ rp_module_desc="Intellivision emulator"
 rp_module_help="ROM Extensions: .int .bin\n\nCopy your Intellivision roms to $romdir/intellivision\n\nCopy the required BIOS files exec.bin and grom.bin to $biosdir"
 rp_module_licence="GPL2 http://spatula-city.org/%7Eim14u2c/intv/"
 rp_module_section="opt"
-rp_module_flags="dispmanx !mali !kms"
+rp_module_flags="dispmanx  !kms"
 
 function depends_jzintv() {
     getDepends libsdl1.2-dev
@@ -50,6 +50,6 @@ function configure_jzintv() {
         setDispmanx "$md_id" 1
     fi
 
-    addEmulator 1 "$md_id" "intellivision" "$md_inst/bin/jzintv -p $biosdir -q %ROM%"
+    addEmulator 1 "$md_id" "intellivision" "$md_inst/bin/jzintv -z4 -f1 -p $biosdir -q %ROM%"
     addSystem "intellivision"
 }
