cb62d6af84504c92634b9b2bb4f8ff0d549c16d4
sikotik <33843126+sikotik@users.noreply.github.com>
2018-07-25 20:05:16 -0700

    added super famicom

--- scriptmodules/libretrocores/lr-snes9x.sh
+++ scriptmodules/libretrocores/lr-snes9x.sh
@@ -43,7 +43,11 @@ function install_lr-snes9x() {
 function configure_lr-snes9x() {
     mkRomDir "snes"
     ensureSystemretroconfig "snes"
+    mkRomDir "superfamicom"
+    ensureSystemretroconfig "superfamicom"
 
     addEmulator 0 "$md_id" "snes" "$md_inst/snes9x_libretro.so"
     addSystem "snes"
+    addEmulator 0 "$md_id" "superfamicom" "$md_inst/snes9x_libretro.so"
+    addSystem "superfamicom"
 }
