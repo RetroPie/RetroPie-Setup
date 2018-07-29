0dc1a39d5693dcdad3af2ac30e9264dc622f80aa
sikotik <33843126+sikotik@users.noreply.github.com>
2018-07-29 13:09:01 -0700

    disable LOVE

--- scriptmodules/ports/love-0.10.2.sh
+++ scriptmodules/ports/love-0.10.2.sh
@@ -14,7 +14,7 @@ rp_module_desc="Love - 2d Game Engine v0.10.2"
 rp_module_help="Copy your Love games to $romdir/love"
 rp_module_licence="GPL3 https://bitbucket.org/rude/love/raw/7b520c437317626da2102de1aafdad0e67b54bf5/license.txt"
 rp_module_section="opt"
-rp_module_flags="!aarch64"
+rp_module_flags=" !mali !aarch64"
 
 function depends_love-0.10.2() {
     depends_love
