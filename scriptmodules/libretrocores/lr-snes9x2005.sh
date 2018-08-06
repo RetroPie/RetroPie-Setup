87662be714cded0fce71625e959ba9bcb9c0dac9
sikotik <33843126+sikotik@users.noreply.github.com>
2018-08-06 15:03:14 -0700

    change superfamicom to sfc

--- scriptmodules/libretrocores/lr-snes9x2005.sh
+++ scriptmodules/libretrocores/lr-snes9x2005.sh
@@ -39,11 +39,11 @@ function install_lr-snes9x2005() {
 function configure_lr-snes9x2005() {
     mkRomDir "snes"
     ensureSystemretroconfig "snes"
-    mkRomDir "superfamicom"
-    ensureSystemretroconfig "superfamicom"
+    mkRomDir "sfc"
+    ensureSystemretroconfig "sfc"
 
     addEmulator 0 "$md_id" "snes" "$md_inst/snes9x2005_libretro.so"
     addSystem "snes"
-    addEmulator 0 "$md_id" "superfamicom" "$md_inst/snes9x2005_libretro.so"
-    addSystem "superfamicom"
+    addEmulator 0 "$md_id" "sfc" "$md_inst/snes9x2005_libretro.so"
+    addSystem "sfc"
 }
