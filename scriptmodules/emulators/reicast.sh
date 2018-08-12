cb55bb672780f609c6aa805d7a3d1ffaadc6a15e
sikotik <33843126+sikotik@users.noreply.github.com>
2018-08-12 16:27:03 -0700

    Update reicast.sh

--- scriptmodules/emulators/reicast.sh
+++ scriptmodules/emulators/reicast.sh
@@ -29,7 +29,7 @@ function sources_reicast() {
     elif isPlatform "vero4k"; then
         gitPullOrClone "$md_build" https://github.com/reicast/reicast-emulator.git
     else
-        gitPullOrClone "$md_build" https://github.com/Odroid-RetroArena/reicast-emulator.git
+        gitPullOrClone "$md_build" https://github.com/reicast/reicast-emulator.git
     fi
     sed -i "s/CXXFLAGS += -fno-rtti -fpermissive -fno-operator-names/CXXFLAGS += -fno-rtti -fpermissive -fno-operator-names -D_GLIBCXX_USE_CXX11_ABI=0/g" shell/linux/Makefile
 }
