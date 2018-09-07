bffaac46122099d524617760e99488404aaee6d9
sikotik <33843126+sikotik@users.noreply.github.com>
2018-09-07 12:24:18 -0700

    update

--- scriptmodules/libretrocores/lr-reicast.sh
+++ scriptmodules/libretrocores/lr-reicast.sh
@@ -34,25 +34,39 @@ function install_lr-reicast() {
 function configure_lr-reicast() {
     mkRomDir "dreamcast"
     mkRomDir "naomi"
+    mkRomDir "atomiswave"
     ensureSystemretroconfig "dreamcast"
     ensureSystemretroconfig "naomi"
+    ensureSystemretroconfig "atomiswave"
 
     mkUserDir "$biosdir/dc"
      
     # symlink to JP bios by default
     ln -sf "$biosdir/dc/naomi_boot_jp.bin" "$biosdir/dc/naomi_boot.bin"
           
-    # add naomi as a copy of arcade for showcase theme
+    # add naomi to showcase theme
     if [[ ! -f /etc/emulationstation/themes/showcase/naomi/theme.xml ]]; then
         cp -R /etc/emulationstation/themes/showcase/arcade/. /etc/emulationstation/themes/showcase/naomi/
         wget -O /etc/emulationstation/themes/showcase/naomi/_inc/system.png https://image.ibb.co/kDMSAK/showcase_naomi_system.png
         wget -O /etc/emulationstation/themes/showcase/naomi/_inc/background.png https://image.ibb.co/gLBije/showcase_naomi_background.png
     fi
     
+    # add atomiswave to showcase theme
+    if [[ ! -f /etc/emulationstation/themes/showcase/atomiswave/theme.xml ]]; then
+        cp -R /etc/emulationstation/themes/showcase/arcade/. /etc/emulationstation/themes/showcase/atomiswave/
+        wget -O /etc/emulationstation/themes/showcase/atomiswave/_inc/system.png https://image.ibb.co/f5fCKe/system.png
+        wget -O /etc/emulationstation/themes/showcase/atomiswave/_inc/background.png https://image.ibb.co/kgftsz/background.png
+    fi
+    
     # multibios hack
     if [[ ! -f /opt/retropie/supplementary/runcommand/runcommand_naomi.sh ]]; then
         cp /opt/retropie/supplementary/runcommand/runcommand.sh /opt/retropie/supplementary/runcommand/runcommand_naomi.sh
-        sed -i -e 's:function show_launch() {:function show_launch() {\n    if [[ "$ROM_BN" =~ ^("Capcom vs. SNK 2 - Mark of the Millennium 2001"|"Marvel vs. Capcom 2 - The New Age of Heroes"|"Project Justice"|"Cannon Spike"|"Gun Spike")$ ]];\n    then\n        ln -sf /home/pigaming/RetroPie/BIOS/dc/naomi_boot_us.bin /home/pigaming/RetroPie/BIOS/dc/naomi_boot.bin\n    else\n        ln -sf /home/pigaming/RetroPie/BIOS/dc/naomi_boot_jp.bin /home/pigaming/RetroPie/BIOS/dc/naomi_boot.bin\n    fi\n:g' /opt/retropie/supplementary/runcommand/runcommand_naomi.sh
+        sed -i -e 's:function show_launch() {:function show_launch() {\n    if [[ "$ROM_BN" =~ ^("Capcom vs. SNK 2 - Mark of the Millennium 2001"|"Marvel vs. Capcom 2 - The New Age of Heroes"|"Project Justice"|"Cannon Spike"|"Spawn")$ ]];\n    then\n        ln -sf /home/pigaming/RetroPie/BIOS/dc/naomi_boot_us.bin /home/pigaming/RetroPie/BIOS/dc/naomi_boot.bin\n    else\n        ln -sf /home/pigaming/RetroPie/BIOS/dc/naomi_boot_jp.bin /home/pigaming/RetroPie/BIOS/dc/naomi_boot.bin\n    fi\n:g' /opt/retropie/supplementary/runcommand/runcommand_naomi.sh
+    fi
+    
+    if [[ ! -f /opt/retropie/supplementary/runcommand/runcommand_atomiswave.sh ]]; then
+        cp /opt/retropie/supplementary/runcommand/runcommand.sh /opt/retropie/supplementary/runcommand/runcommand_atomiswave.sh
+        sed -i -e 's:function show_launch() {:function show_launch() {\n    if [[ "$ROM_BN" =~ ^("Capcom vs. SNK 2 - Mark of the Millennium 2001"|"Marvel vs. Capcom 2 - The New Age of Heroes"|"Project Justice"|"Cannon Spike"|"Spawn")$ ]];\n    then\n        ln -sf /home/pigaming/RetroPie/BIOS/dc/naomi_boot_us.bin /home/pigaming/RetroPie/BIOS/dc/naomi_boot.bin\n    else\n        ln -sf /home/pigaming/RetroPie/BIOS/dc/naomi_boot_jp.bin /home/pigaming/RetroPie/BIOS/dc/naomi_boot.bin\n    fi\n:g' /opt/retropie/supplementary/runcommand/runcommand_atomiswave.sh
     fi
        
     # system-specific
@@ -61,17 +75,21 @@ function configure_lr-reicast() {
 
     iniConfig " = " "" "$configdir/naomi/retroarch.cfg"
     iniSet "video_shared_context" "true"
+    
+    iniConfig " = " "" "$configdir/atomiswave/retroarch.cfg"
+    iniSet "video_shared_context" "true"
 
     addEmulator 0 "$md_id" "dreamcast" "$md_inst/reicast_libretro.so"
     addEmulator 1 "$md_id" "naomi" "$md_inst/reicast_libretro.so"
+    addEmulator 2 "$md_id" "atomiswave" "$md_inst/reicast_libretro.so"
 
     addSystem "dreamcast"
     addSystem "naomi"
+    addSystem "atomiswave"
     
-    # use custom runcommand for naomi
-    if [[ -e /opt/retropie/supplementary/runcommand/runcommand_naomi.sh ]]; then
-        sed -i -e 's/runcommand.sh 0 _SYS_ naomi/runcommand_naomi.sh 0 _SYS_ naomi/g' /etc/emulationstation/es_systems.cfg
-    fi
+    # custom runcommand
+    sed -i -e 's/runcommand.sh 0 _SYS_ naomi/runcommand_naomi.sh 0 _SYS_ naomi/g' /etc/emulationstation/es_systems.cfg
+    sed -i -e 's/runcommand.sh 0 _SYS_ atomiswave/runcommand_atomiswave.sh 0 _SYS_ atomiswave/g' /etc/emulationstation/es_systems.cfg
     
     # set core options
     setRetroArchCoreOption "${dir_name}reicast_audio_buffer_size" "2048"
