6c4aa3b242098af56fdd64538309b149ba2df3da
6alileo <42245908+6alileo@users.noreply.github.com>
2018-09-12 12:54:00 -0700

    Update lr-mame2000.sh

--- scriptmodules/libretrocores/lr-mame2000.sh
+++ scriptmodules/libretrocores/lr-mame2000.sh
@@ -41,8 +41,8 @@ function install_lr-mame2000() {
     )
 }
 
-function install_bin_mame2000() {
-    downloadAndExtract "http://github.com/Retro-Arena/xu4-bins/raw/master/mame2000.tar.gz" "$md_inst" 1
+function install_bin_lr-mame2000() {
+    downloadAndExtract "http://github.com/Retro-Arena/xu4-bins/raw/master/lr-mame2000.tar.gz" "$md_inst" 1
 }
 
 function configure_lr-mame2000() {
