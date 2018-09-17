238aebfb660ade724b580834b06019923931272a
6alileo <42245908+6alileo@users.noreply.github.com>
2018-09-16 17:45:12 -0700

    Create revertgles2headers.sh

---
+++ scriptmodules/revertgles2headers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Reverts to ORA v1.1 GLES2 headers
+
+mv /usr/include/GLES2/gl2.h /usr/include/GLES2/gl2.h.org
+mv /usr/include/GLES2/gl2ext.h /usr/include/GLES2/gl2ext.h.org
+mv /usr/include/GLES2/gl2platform.h /usr/include/GLES2/gl2platform.h.org
+wget -O /usr/include/GLES2/gl2.h https://raw.githubusercontent.com/Retro-Arena/xu4-bins/master/GLES2/gl2.h
+wget -O /usr/include/GLES2/gl2ext.h https://raw.githubusercontent.com/Retro-Arena/xu4-bins/master/GLES2/gl2ext.h
+wget -O /usr/include/GLES2/gl2platform.h https://raw.githubusercontent.com/Retro-Arena/xu4-bins/master/GLES2/gl2platform.h
