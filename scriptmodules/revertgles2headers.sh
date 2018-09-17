fae9f1758bad4a02d0651337edec3f3350661376
6alileo <42245908+6alileo@users.noreply.github.com>
2018-09-16 18:18:04 -0700

    Update revertgles2headers.sh

--- scriptmodules/revertgles2headers.sh
+++ scriptmodules/revertgles2headers.sh
@@ -5,6 +5,6 @@
 mv /usr/include/GLES2/gl2.h /usr/include/GLES2/gl2.h.org
 mv /usr/include/GLES2/gl2ext.h /usr/include/GLES2/gl2ext.h.org
 mv /usr/include/GLES2/gl2platform.h /usr/include/GLES2/gl2platform.h.org
-wget -O /usr/include/GLES2/gl2.h https://raw.githubusercontent.com/Retro-Arena/xu4-bins/master/GLES2/gl2.h
-wget -O /usr/include/GLES2/gl2ext.h https://raw.githubusercontent.com/Retro-Arena/xu4-bins/master/GLES2/gl2ext.h
-wget -O /usr/include/GLES2/gl2platform.h https://raw.githubusercontent.com/Retro-Arena/xu4-bins/master/GLES2/gl2platform.h
+wget -S /usr/include/GLES2/gl2.h https://raw.githubusercontent.com/Retro-Arena/xu4-bins/master/GLES2/gl2.h
+wget -S /usr/include/GLES2/gl2ext.h https://raw.githubusercontent.com/Retro-Arena/xu4-bins/master/GLES2/gl2ext.h
+wget -S /usr/include/GLES2/gl2platform.h https://raw.githubusercontent.com/Retro-Arena/xu4-bins/master/GLES2/gl2platform.h
