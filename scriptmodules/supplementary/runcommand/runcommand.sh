5ffc7de9fcd2b09e90283cba89853fcd9a8239ab
6alileo <42245908+6alileo@users.noreply.github.com>
2018-09-23 15:14:55 -0700

    Update runcommand.sh

--- scriptmodules/supplementary/runcommand/runcommand.sh
+++ scriptmodules/supplementary/runcommand/runcommand.sh
@@ -1045,8 +1045,6 @@ function ogst_emu() {
 }
 
 function ogst_es() {
-    sleep 5
-
     if lsmod | grep -q 'fbtft_device'; then
         sudo mplayer -quiet -nolirc -nosound -vo fbdev2:/dev/fb1 -vf scale=320:240 "/home/pigaming/ogst/ora.gif" &> /dev/null
     else
