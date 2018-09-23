51ef3775be4c8a2fad5f3260f5680cf705ad02fc
6alileo <42245908+6alileo@users.noreply.github.com>
2018-09-23 16:08:36 -0700

    per AreaScout

--- scriptmodules/emulators/ppsspp.sh
+++ scriptmodules/emulators/ppsspp.sh
@@ -17,23 +17,9 @@ rp_module_section="opt"
 rp_module_flags=""
 
 function sources_ppsspp() {
-    # gl2ext.h fix
-    if [[ -e /usr/include/GLES2/gl2ext.h.org ]]; then
-        cp -p /usr/include/GLES2/gl2ext.h.org /usr/include/GLES2/gl2ext.h
-    else
-        cp -p /usr/include/GLES2/gl2ext.h /usr/include/GLES2/gl2ext.h.org
-    fi
-    sed -i -e 's:GL_APICALL void GL_APIENTRY glBindFragDataLocationIndexedEXT://GL_APICALL void GL_APIENTRY glBindFragDataLocationIndexedEXT:g' /usr/include/GLES2/gl2ext.h
-    sed -i -e 's:GL_APICALL void GL_APIENTRY glBindFragDataLocationEXT://GL_APICALL void GL_APIENTRY glBindFragDataLocationEXT:g' /usr/include/GLES2/gl2ext.h
-    sed -i -e 's:GL_APICALL GLint GL_APIENTRY glGetProgramResourceLocationIndexEXT://GL_APICALL GLint GL_APIENTRY glGetProgramResourceLocationIndexEXT:g' /usr/include/GLES2/gl2ext.h
-    sed -i -e 's:GL_APICALL GLint GL_APIENTRY glGetFragDataIndexEXT://GL_APICALL GLint GL_APIENTRY glGetFragDataIndexEXT:g' /usr/include/GLES2/gl2ext.h
-    sed -i -e 's:GL_APICALL void GL_APIENTRY glBufferStorageEXT://GL_APICALL void GL_APIENTRY glBufferStorageEXT:g' /usr/include/GLES2/gl2ext.h
-    sed -i -e 's:GL_APICALL void GL_APIENTRY glCopyImageSubDataOES://GL_APICALL void GL_APIENTRY glCopyImageSubDataOES:g' /usr/include/GLES2/gl2ext.h
-
-    # git
     gitPullOrClone "$md_build/ppsspp" https://github.com/hrydgard/ppsspp.git
     
-	  # CMakeLists.txt changes
+    # CMakeLists.txt changes
     sed -i -e 's:set(ARM ON):set(ARM ON)\n    add_definitions(-mfloat-abi=hard -marm -mtune=cortex-a15.cortex-a7 -mcpu=cortex-a15 -mfpu=neon-vfpv4 -fomit-frame-pointer -ftree-vectorize -mvectorize-with-neon-quad -ffast-math -DARM_NEON):g' "$md_build/ppsspp/CMakeLists.txt"
     sed -i -e 's:set(VULKAN ON):set(VULKAN OFF):g' "$md_build/ppsspp/CMakeLists.txt"
 	
