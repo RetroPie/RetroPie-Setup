f0d0cc3df0f6c643180079fdcd402e979c7b030f
6alileo <42245908+6alileo@users.noreply.github.com>
2018-09-25 09:09:01 -0700

    Update lr-genesis-plus-gx.sh

--- scriptmodules/libretrocores/lr-genesis-plus-gx.sh
+++ scriptmodules/libretrocores/lr-genesis-plus-gx.sh
@@ -41,7 +41,7 @@ function install_bin_lr-genesis-plus-gx() {
 function configure_lr-genesis-plus-gx() {
     local system
     local def
-    for system in gamegear mastersystem megadrive sg-1000 segacd; do
+    for system in gamegear mastersystem genesis megadrive sg-1000 segacd; do
         def=0
         [[ "$system" == "gamegear" || "$system" == "sg-1000" ]] && def=1
         mkRomDir "$system"
