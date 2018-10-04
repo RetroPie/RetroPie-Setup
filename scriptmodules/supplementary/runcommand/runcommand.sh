77efb55c03ca766d89c72c737017d0fdfd045a52
6alileo <42245908+6alileo@users.noreply.github.com>
2018-10-04 10:17:00 -0700

    Update runcommand.sh

--- scriptmodules/supplementary/runcommand/runcommand.sh
+++ scriptmodules/supplementary/runcommand/runcommand.sh
@@ -1013,7 +1013,7 @@ function launch_command() {
 
 function naomi_bios() {
     if [[ "$SYSTEM" =~ ^("naomi"|"atomiswave")$ ]]; then
-        if [[ "$ROM_BN" =~ ^("Capcom vs. SNK 2 - Mark of the Millennium 2001"|"Marvel vs. Capcom 2 - The New Age of Heroes"|"Project Justice"|"Cannon Spike"|"Spawn")$ ]]; then
+        if [[ "$ROM_BN" =~ ^("Capcom vs. SNK 2 - Mark of the Millennium 2001"|"Marvel vs. Capcom 2 - The New Age of Heroes"|"Project Justice"|"Cannon Spike"|"Spawn"|"Sports Jam")$ ]]; then
             ln -sf /home/pigaming/RetroPie/BIOS/dc/naomi_boot_us.bin /home/pigaming/RetroPie/BIOS/dc/naomi_boot.bin
         else
             ln -sf /home/pigaming/RetroPie/BIOS/dc/naomi_boot_jp.bin /home/pigaming/RetroPie/BIOS/dc/naomi_boot.bin
