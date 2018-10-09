1743503b9656277afa6a7126ef1de33cd7a34e0a
6alileo <42245908+6alileo@users.noreply.github.com>
2018-10-08 18:55:17 -0700

    Update retropie_packages.sh

--- retropie_packages.sh
+++ retropie_packages.sh
@@ -55,6 +55,7 @@ source "$scriptdir/scriptmodules/system.sh"
 source "$scriptdir/scriptmodules/helpers.sh"
 source "$scriptdir/scriptmodules/inifuncs.sh"
 source "$scriptdir/scriptmodules/packages.sh"
+source "$scriptdir/scriptmodules/fixes.sh
 
 setup_env
 
