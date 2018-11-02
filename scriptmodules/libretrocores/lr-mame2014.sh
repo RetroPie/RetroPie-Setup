9133a0409304fc198a74efbac28f0de04a1a1277
6alileo <6alileo@users.noreply.github.com>
2018-11-02 12:37:03 -0700

    Update scriptmodules/libretrocores/lr-mame2014.sh

--- scriptmodules/libretrocores/lr-mame2014.sh
+++ scriptmodules/libretrocores/lr-mame2014.sh
@@ -35,7 +35,7 @@ function install_lr-mame2014() {
 }
 
 function install_bin_lr-mame2014() {
-    downloadAndExtract "http://odroidarena.com/pub/binaries/in_use/lr-mame2014.tar.gz" "$md_inst" 1
+    downloadAndExtract "http://github.com/Retro-Arena/xu4-bins/raw/master/lr-mame2014.tar.gz" "$md_inst" 1
 }
 
 function configure_lr-mame2014() {
