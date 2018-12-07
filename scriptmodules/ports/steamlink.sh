87dde1669c29f5707e1cb5fb4bdbbb22bef863bc
Conn O'Griofa <connogriofa@gmail.com>
2018-12-07 18:09:43 +0000

    steamlink: update script for 1.0.7
    * update dependencies
    * update directory locations for new version

--- scriptmodules/ports/steamlink.sh
+++ scriptmodules/ports/steamlink.sh
@@ -17,11 +17,11 @@ rp_module_flags="!mali !x86 !kms !rpi1 !rpi2"
 rp_module_help="Streaming games to your computer with Steam.\n\nIf you experience a black screen during startup, you may need to disable the overscan_scale setting in your Pi's /boot/config.txt."
 
 function depends_steamlink() {
-    getDepends python3-dev curl xz-utils
+    getDepends python3-dev curl xz-utils libinput10 libxkbcommon-x11-0
 }
 
 function install_bin_steamlink() {
-    local ver="1.0.5"
+    local ver="1.0.7"
     local url="http://media.steampowered.com/steamlink/rpi"
     wget -qO "$__tmpdir/steamlink_"$ver"_armhf.deb" ""$url"/steamlink_"$ver"_armhf.deb"
     dpkg -i "$__tmpdir/steamlink_"$ver"_armhf.deb"
@@ -33,16 +33,16 @@ function remove_steamlink() {
 
 function configure_steamlink() {
     local sl_dir="$home/.local/share/SteamLink"
-    local valve_dir="$home/.local/share/Valve Software"
+    local valve_dir="$home/.local/share/Valve Corporation"
 
     mkUserDir "$sl_dir"
     mkUserDir "$valve_dir"
-    mkUserDir "$valve_dir/Streaming Client"
+    mkUserDir "$valve_dir/SteamLink"
 
     # create optional streaming_args.txt for user modification
-    touch "$valve_dir/Streaming Client/streaming_args.txt"
-    chown $user:$user "$valve_dir/Streaming Client/streaming_args.txt"
-    moveConfigFile "$valve_dir/Streaming Client/streaming_args.txt" "$md_conf_root/$md_id/streaming_args.txt"
+    touch "$valve_dir/SteamLink/streaming_args.txt"
+    chown $user:$user "$valve_dir/SteamLink/streaming_args.txt"
+    moveConfigFile "$valve_dir/SteamLink/streaming_args.txt" "$md_conf_root/$md_id/streaming_args.txt"
 
     # RetroPie sets overscan by default, which requires an override
     if grep '^overscan_scale=1' /boot/config.txt &>/dev/null; then
