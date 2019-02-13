589073f8181ebba16c7d45f691393012ba1488ce
Jools Wills <buzz@exotica.org.uk>
2019-02-13 19:36:25 +0000

    renamed lr-mame2014 to lr-mame2015  * upstream repo was renamed as the code was based on 2015 mame code  * update romset version to match upstream  * add hook to rename previous installed configs

--- scriptmodules/libretrocores/lr-mame2015.sh
+++ scriptmodules/libretrocores/lr-mame2015.sh
@@ -9,37 +9,43 @@
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
-rp_module_id="lr-mame2014"
-rp_module_desc="Arcade emu - MAME 0.159 port for libretro"
+rp_module_id="lr-mame2015"
+rp_module_desc="Arcade emu - MAME 0.160 port for libretro"
 rp_module_help="ROM Extension: .zip\n\nCopy your MAME roms to either $romdir/mame-libretro or\n$romdir/arcade"
-rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/mame2014-libretro/master/docs/license.txt"
+rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/mame2015-libretro/master/docs/license.txt"
 rp_module_section="exp"
 
-function sources_lr-mame2014() {
-    gitPullOrClone "$md_build" https://github.com/libretro/mame2014-libretro.git
+function _update_hook_lr-mame2015() {
+    # move from old location and update emulators.cfg
+    renameModule "lr-mame2014" "lr-mame2015"
 }
 
-function build_lr-mame2014() {
+function sources_lr-mame2015() {
+    gitPullOrClone "$md_build" https://github.com/libretro/mame2015-libretro.git
+}
+
+function build_lr-mame2015() {
     rpSwap on 1200
     make clean
     make
     rpSwap off
-    md_ret_require="$md_build/mame2014_libretro.so"
+    md_ret_require="$md_build/mame2015_libretro.so"
 }
 
-function install_lr-mame2014() {
+function install_lr-mame2015() {
     md_ret_files=(
-        'mame2014_libretro.so'
+        'mame2015_libretro.so'
         'docs/README-original.md'
+        'docs/license.txt'
     )
 }
 
-function configure_lr-mame2014() {
+function configure_lr-mame2015() {
     local system
     for system in arcade mame-libretro; do
         mkRomDir "$system"
         ensureSystemretroconfig "$system"
-        addEmulator 0 "$md_id" "$system" "$md_inst/mame2014_libretro.so"
+        addEmulator 0 "$md_id" "$system" "$md_inst/mame2015_libretro.so"
         addSystem "$system"
     done
 }
