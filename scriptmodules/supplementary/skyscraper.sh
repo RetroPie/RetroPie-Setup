ad27518817d374b63889acd9898fe6009fb18404
Cristi Mitrana <31816814+cmitu@users.noreply.github.com>
2019-03-18 09:26:53 +0200

    Purge and Vacuum actions should not be runa as 'root' - but as calling user.

--- scriptmodules/supplementary/skyscraper.sh
+++ scriptmodules/supplementary/skyscraper.sh
@@ -78,9 +78,9 @@ function _clear_platform_skyscraper() {
     [[ ! -d "$configdir/all/skyscraper/$cache_folder/$platform" ]] && return
 
     if [[ $mode == "vacuum" ]]; then
-        $md_inst/Skyscraper --unattend -p "$platform" --cache vacuum
+        sudo -u "$user" stdbuf -o0 $md_inst/Skyscraper --unattend -p "$platform" --cache vacuum
     else
-        $md_inst/Skyscraper --unattend -p "$platform" --cache purge:all
+        sudo -u "$user" stdbuf -o0 $md_inst/Skyscraper --unattend -p "$platform" --cache purge:all
     fi
     sleep 5
 }
