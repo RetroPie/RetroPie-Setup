8563aed983119e68f0e16468140224da2e6831d1
Jools Wills <buzz@exotica.org.uk>
2019-03-27 16:58:06 +0000

    usbromservice - various minor fixes  * build usbmount in $md_build/usbmount so packages in parent stay within $md_build  * don't remove $md_build - packaging will do that. Avoids a popd error on install  * disable is called on uninstall to remove scripts - check for $md_inst before trying to create disable file

--- scriptmodules/supplementary/usbromservice.sh
+++ scriptmodules/supplementary/usbromservice.sh
@@ -27,12 +27,11 @@ function depends_usbromservice() {
     if ! hasPackage usbmount $(_get_ver_usbromservice); then
         depends+=(debhelper devscripts pmount lockfile-progs)
         getDepends "${depends[@]}"
-        gitPullOrClone "$md_build" https://github.com/RetroPie/usbmount.git systemd
-        cd "$md_build"
+        gitPullOrClone "$md_build/usbmount" https://github.com/RetroPie/usbmount.git systemd
+        cd "$md_build/usbmount"
         dpkg-buildpackage
         dpkg -i ../usbmount_*_all.deb
         rm -f ../usbmount_*
-        rm -rf "$md_build"
     fi
 }
 
@@ -63,7 +62,7 @@ function disable_usbromservice() {
         file="/etc/usbmount/mount.d/${file##*/}"
         rm -f "$file"
     done
-    touch "$md_inst/disabled"
+    [[ -d "$md_inst" ]] && touch "$md_inst/disabled"
 }
 
 function remove_usbromservice() {
