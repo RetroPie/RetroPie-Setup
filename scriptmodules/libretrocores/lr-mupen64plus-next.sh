8021f1c23c090e906fe927d24713ef58bf5780ff
Jools Wills <buzz@exotica.org.uk>
2019-06-09 04:01:32 +0100

    lr-mupen64plus-next - disable for armv6  * dynarec contains some armv7 only instructions so it's not compatible.

--- scriptmodules/libretrocores/lr-mupen64plus-next.sh
+++ scriptmodules/libretrocores/lr-mupen64plus-next.sh
@@ -14,6 +14,7 @@ rp_module_desc="N64 emulator - Mupen64Plus + GLideN64 for libretro (next version
 rp_module_help="ROM Extensions: .z64 .n64 .v64\n\nCopy your N64 roms to $romdir/n64"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/mupen64plus-libretro-nx/master/LICENSE"
 rp_module_section="exp"
+rp_module_flags="!armv6"
 
 function depends_lr-mupen64plus-next() {
     local depends=(flex bison libpng-dev)
