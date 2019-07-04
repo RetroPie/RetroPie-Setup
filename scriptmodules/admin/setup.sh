35555bccbe12ca9601ac7be15910e668d8211898
Jools Wills <buzz@exotica.org.uk>
2019-07-04 05:20:14 +0100

    setup - add os description to log header

--- scriptmodules/admin/setup.sh
+++ scriptmodules/admin/setup.sh
@@ -35,7 +35,7 @@ function rps_logInit() {
 function rps_logStart() {
     echo -e "Log started at: $(date -d @$time_start)\n"
     echo "RetroPie-Setup version: $__version ($(git -C "$scriptdir" log -1 --pretty=format:%h))"
-    echo "System: $(uname -a)"
+    echo "System: $__os_desc - $(uname -a)"
 }
 
 function rps_logEnd() {
