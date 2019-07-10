2960ef7c0b09ede84fa95819d0457fbaec2d70e3
Keith F. Kelly <keith.f.kelly@outlook.com>
2019-07-10 11:21:53 -0700

    Pass platform through to make commands (at least for Raspberry Pi).

--- scriptmodules/libretrocores/lr-mame2010.sh
+++ scriptmodules/libretrocores/lr-mame2010.sh
@@ -24,6 +24,7 @@ function build_lr-mame2010() {
     make clean
     local params=()
     isPlatform "arm" && params+=("VRENDER=soft" "ARM_ENABLED=1")
+    isPlatform "rpi" && params+=("platform=$__platform")
     make "${params[@]}" ARCHOPTS="$CFLAGS" buildtools
     make "${params[@]}" ARCHOPTS="$CFLAGS"
     rpSwap off
