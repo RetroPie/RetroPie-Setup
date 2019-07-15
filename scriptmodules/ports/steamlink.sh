b6e786b52c014ac9a01b1c14105bdcf994a2b824
Conn O'Griofa <connogriofa@gmail.com>
2019-07-15 11:55:38 +0100

    steamlink: update for Raspberry Pi 4 & FKMS

--- scriptmodules/ports/steamlink.sh
+++ scriptmodules/ports/steamlink.sh
@@ -10,14 +10,14 @@
 #
 
 rp_module_id="steamlink"
-rp_module_desc="Steam Link for Raspberry Pi 3"
+rp_module_desc="Steam Link for Raspberry Pi 3 or later"
 rp_module_licence="PROP https://steamcommunity.com/app/353380/discussions/0/1743353164093954254/"
 rp_module_section="exp"
-rp_module_flags="!mali !x86 !kms !rpi1 !rpi2"
+rp_module_flags="!mali !x86 !rpi1 !rpi2"
 rp_module_help="Stream games from your computer with Steam"
 
 function depends_steamlink() {
-    getDepends python3-dev curl xz-utils libinput10 libxkbcommon-x11-0 matchbox-window-manager xorg
+    getDepends python3-dev curl xz-utils libinput10 libxkbcommon-x11-0 matchbox-window-manager xorg zenity
 }
 
 function install_bin_steamlink() {
