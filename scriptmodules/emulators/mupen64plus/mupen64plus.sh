e082d09d134fd0e16766dcffd8caa2e6c08100c0
dankcushions <chris.bransden@gmail.com>
2019-10-28 18:14:45 +0000

    Use 128MB cache size for rpi2+, 64MB for pi1/0.

--- scriptmodules/emulators/mupen64plus/mupen64plus.sh
+++ scriptmodules/emulators/mupen64plus/mupen64plus.sh
@@ -253,8 +253,6 @@ function testCompatibility() {
                 config_version=$(<"$configdir/n64/GLideN64_config_version.ini")
             fi
             iniSet "configVersion" "$config_version"
-            # Size of texture cache in megabytes. Good value is VRAM*3/4
-            iniSet "CacheSize" "50"
             # Set native resolution factor of 1
             iniSet "UseNativeResolutionFactor" "1"
             for game in "${GLideN64NativeResolution_blacklist[@]}"; do
