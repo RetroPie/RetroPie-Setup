b69b94dff3e5fab4b51c94bb5abe5ef0d90a47d9
Jools Wills <buzz@exotica.org.uk>
2019-11-06 02:27:32 +0000

    system - enable binaries for Raspbian 10 (buster)  * not all binaries are currently available as support is still being worked on

--- scriptmodules/system.sh
+++ scriptmodules/system.sh
@@ -99,8 +99,8 @@ function get_os_version() {
                 __platform_flags+=" xbian"
             fi
 
-            # we provide binaries for RPI on Raspbian 9 only
-            if isPlatform "rpi" && compareVersions "$__os_debian_ver" gt 8 && compareVersions "$__os_debian_ver" lt 10; then
+            # we provide binaries for RPI on Raspbian 9/10
+            if isPlatform "rpi" && compareVersions "$__os_debian_ver" gt 8 && compareVersions "$__os_debian_ver" lt 11; then
                 __has_binaries=1
             fi
             ;;
