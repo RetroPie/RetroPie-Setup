092cedd2acf12db1e638442d72ef1dbd5b83b2fd
Neo-Stone <40294190+Neo-Stone@users.noreply.github.com>
2019-11-07 13:28:27 +0100

    Update lr-atari800.sh
    Retrieve Atari 800 colour palettes

--- scriptmodules/libretrocores/lr-atari800.sh
+++ scriptmodules/libretrocores/lr-atari800.sh
@@ -29,6 +29,7 @@ function install_lr-atari800() {
     md_ret_files=(
         'atari800_libretro.so'
         'atari800/COPYING'
+        'atari800/act'
     )
 }
 
@@ -41,6 +42,9 @@ function configure_lr-atari800() {
 
     mkUserDir "$md_conf_root/atari800"
     moveConfigFile "$home/.atari800.cfg" "$md_conf_root/atari800/atari800.cfg"
+
+    cp -rv "$md_inst/act" "$md_conf_root/atari800"
+    chown -R $user:$user "$md_conf_root/atari800/act"
 
     addEmulator 1 "lr-atari800" "atari800" "$md_inst/atari800_libretro.so"
     addEmulator 1 "lr-atari800" "atari5200" "$md_inst/atari800_libretro.so"
