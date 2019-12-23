096786c39d320741f4bd63f9da11ffce4a07fa7f
Jools Wills <buzz@exotica.org.uk>
2019-12-23 05:45:04 +0000

    image - remove old qemu hack to force newer cpu for rpi0/1  * was added due to old qemu bugs with arm1176 - but will cause issues with bins for newer cpus. Suspect it's also no longer needed but will do some testing

--- scriptmodules/admin/image.sh
+++ scriptmodules/admin/image.sh
@@ -129,8 +129,7 @@ modules=(
     'xpad'
 )
 for module in "\${modules[@]}"; do
-    # rpi1 platform would use QEMU_CPU set to arm1176, but it seems buggy currently (lots of segfaults)
-    sudo QEMU_CPU=cortex-a15 __platform=$platform __nodialog=1 ./retropie_packages.sh \$module
+    sudo __platform=$platform __nodialog=1 ./retropie_packages.sh \$module
 done
 
 rm -rf tmp
