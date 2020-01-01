a59aa9d7c1e2018944ec7fa382db68c406346f76
hissingshark <git@hissingshark.co.uk>
2020-01-01 16:20:13 +0000

    Vero4k needs explicit video-mali build since the merger

--- scriptmodules/supplementary/sdl2.sh
+++ scriptmodules/supplementary/sdl2.sh
@@ -79,7 +79,7 @@ function build_sdl2() {
         conf_flags+=("--disable-video-x11")
     fi
     ! isPlatform "x11" && conf_flags+=("--disable-video-vulkan")
-    isPlatform "mali" && conf_flags+=("--enable-video-mali" "--disable-video-opengl")
+    isPlatform "mali" || isPlatform "vero4k" && conf_flags+=("--enable-video-mali" "--disable-video-opengl")
     isPlatform "rpi" && conf_flags+=("--enable-video-rpi")
     isPlatform "kms" || isPlatform "rpi" && conf_flags+=("--enable-video-kmsdrm")
 
