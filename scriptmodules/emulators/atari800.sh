950cd81b7f4ce5e83408942aafd56b9277d4f0b1
Jools Wills <buzz@exotica.org.uk>
2020-01-06 03:40:45 +0000

    atari800 - add some additional emulator entries for forcing model  * useful to force a particular system per game - added 800/800xl and 130xe options - the default atari800 will use the last saved system configuration  * rename atari800 for 5200 to atari800-5200 and force 5200 so it doesn't use the last saved system

--- scriptmodules/emulators/atari800.sh
+++ scriptmodules/emulators/atari800.sh
@@ -66,7 +66,10 @@ function configure_atari800() {
     fi
 
     addEmulator 1 "atari800" "atari800" "$md_inst/atari800.sh %ROM% ${params[*]}"
-    addEmulator 1 "atari800" "atari5200" "$md_inst/atari800.sh %ROM% ${params[*]}"
+    addEmulator 1 "atari800-800" "atari800" "$md_inst/atari800.sh %ROM% ${params[*]} -atari"
+    addEmulator 1 "atari800-800xl" "atari800" "$md_inst/atari800.sh %ROM% ${params[*]} -xl"
+    addEmulator 1 "atari800-130xe" "atari800" "$md_inst/atari800.sh %ROM% ${params[*]} -xe"
+    addEmulator 1 "atari800-5200" "atari5200" "$md_inst/atari800.sh %ROM% ${params[*]} -5200"
     addSystem "atari800"
     addSystem "atari5200"
 }
