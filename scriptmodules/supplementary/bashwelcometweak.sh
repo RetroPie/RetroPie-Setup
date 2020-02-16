621877be437424ed140f1eca92444692c006e6af
Parker Hemphill <aaron.p.hemphill@gmail.com>
2020-02-16 12:31:55 -0500

    Modified existing if statement checking for vcgencmd to only provide GPU temp in output if running on a Pi.  Eliminates the blank GPU temp x86 users currently see

--- scriptmodules/supplementary/bashwelcometweak.sh
+++ scriptmodules/supplementary/bashwelcometweak.sh
@@ -27,10 +27,6 @@ function getIPAddress() {
     [[ -n "$ip_route" ]] && grep -oP "src \K[^\s]+" <<< "$ip_route"
 }
 
-function getClockSpeed() {
-    expr $(echo $(vcgencmd measure_clock $1|awk -F= '{print $2}') / 1000000 )
-}
-
 function retropie_welcome() {
     local upSeconds="$(/usr/bin/cut -d. -f1 /proc/uptime)"
     local secs=$((upSeconds%60))
@@ -48,13 +44,15 @@ function retropie_welcome() {
         cpuTempC=$(($(cat /sys/class/thermal/thermal_zone0/temp)/1000)) && cpuTempF=$((cpuTempC*9/5+32))
     fi
 
+    # calculate CPU max speed
+    local cpuSpeed=$(lscpu|grep "max MHz"|awk '{print $NF}'| awk '{print int($1+0.5)}')
+
     if [[ -f "/opt/vc/bin/vcgencmd" ]]; then
-        if gpuTempC=$(/opt/vc/bin/vcgencmd measure_temp); then
-            gpuTempC=${gpuTempC:5:2}
-            gpuTempF=$((gpuTempC*9/5+32))
-        else
-            gpuTempC=""
-        fi
+        local isPie=yes
+        v3dSpeed=$(expr $(($(vcgencmd measure_clock core|awk -F= '{print $2}') / 1000000)))
+        gpuTempC=$(/opt/vc/bin/vcgencmd measure_temp)
+        gpuTempC=${gpuTempC:5:2}
+        gpuTempF=$((gpuTempC*9/5+32))
     fi
 
     local df_out=()
@@ -128,13 +126,13 @@ function retropie_welcome() {
                 out+="${fgred}IP Address.........: $(getIPAddress)"
                 ;;
             9)
-                out+="Temperature........: CPU: $cpuTempC°C/$cpuTempF°F GPU: $gpuTempC°C/$gpuTempF°F"
+                out+="Temperature........: CPU: $cpuTempC°C/$cpuTempF°F $([[ $isPie == yes ]] && echo "GPU: $gpuTempC°C/$gpuTempF°F")"
                 ;;
             10)
-                [[ -f /usr/bin/vcgencmd ]] && out+="Clock Speed........: CPU: $(getClockSpeed arm)MHz Core: $(getClockSpeed core)MHz V3D: $(getClockSpeed v3d)MHz\n"
+                out+="Max Clock Speed....: CPU: ${cpuSpeed}MHz $([[ $isPie == yes ]] && echo "V3D: ${v3dSpeed}MHz")"
                 ;;
             11)
-                out+="${fgwht}The RetroPie Project, https://retropie.org.uk"
+                out+="\n${fgwht}The RetroPie Project, https://retropie.org.uk"
                 ;;
         esac
         out+="${rst}\n"
@@ -145,7 +143,6 @@ function retropie_welcome() {
 retropie_welcome
 # RETROPIE PROFILE END
 _EOF_
-
 
 }
 
