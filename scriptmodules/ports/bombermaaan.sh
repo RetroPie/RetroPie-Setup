40f25ced9c5ddc93f3b501d1383d9951e14bf059
MrCoolSpan <rikkmar@city.dk>
2020-03-15 14:42:20 +0100

    made it work on ubuntu x64
    made it work on ubuntu x64

--- scriptmodules/ports/bombermaaan.sh
+++ scriptmodules/ports/bombermaaan.sh
@@ -27,7 +27,7 @@ function build_bombermaaan() {
     cd trunk
     cmake . -DCMAKE_INSTALL_PREFIX="$md_inst" -DLOAD_RESOURCES_FROM_FILES:BOOL=ON
     make
-    mv src/Bombermaaan bombermaaan
+    mv $md_build/trunk/bin/Bombermaaan bombermaaan
     md_ret_require="$md_build/trunk/bombermaaan"
 }
 
