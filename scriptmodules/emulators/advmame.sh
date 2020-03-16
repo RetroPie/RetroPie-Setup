da2ac63635bf5a7459078f9a1e00eed9358e2805
cmitu <31816814+cmitu@users.noreply.github.com>
2020-03-16 17:40:09 +0000

    advmame: disable multithreading temporary to resolve a crash

--- scriptmodules/emulators/advmame.sh
+++ scriptmodules/emulators/advmame.sh
@@ -121,6 +121,8 @@ function configure_advmame() {
             iniSet "device_keyboard" "sdl"
             # default for best performance
             iniSet "display_magnify" "1"
+            # disable threading to get rid of the crash-on-exit when using SDL, preventing config save
+            initSet "misc_smp" "no"
         else
             iniSet "device_video_output" "overlay"
             iniSet "display_aspectx" 16
