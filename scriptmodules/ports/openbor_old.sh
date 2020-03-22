643e865db1ae9e57a427b83fb59913d8ac1f7092
MrCoolSpan <rikkmar@city.dk>
2020-03-22 17:42:56 +0100

    Add files via upload

--- scriptmodules/ports/openbor_old.sh
+++ scriptmodules/ports/openbor_old.sh
@@ -9,7 +9,7 @@
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
-rp_module_id="openbor"
+rp_module_id="openbor_old"
 rp_module_desc="OpenBOR - Beat 'em Up Game Engine"
 rp_module_help="OpenBOR games need to be extracted to function properly. Place your pak files in $romdir/ports/openbor and then run $rootdir/ports/openbor/extract.sh. When the script is done, your original pak files will be found in $romdir/ports/openbor/originals and can be deleted."
 rp_module_licence="BSD https://raw.githubusercontent.com/rofl0r/openbor/master/LICENSE"
@@ -20,51 +20,55 @@ function depends_openbor() {
     getDepends libsdl1.2-dev libsdl-gfx1.2-dev libogg-dev libvorbisidec-dev libvorbis-dev libpng-dev zlib1g-dev yasm
 }
 
-
-
 function sources_openbor() {
+    if uname -m |grep "x86_64"; then
+    gitPullOrClone "$md_build" https://github.com/MrCoolSpan/openbor.git
+    else
     gitPullOrClone "$md_build" https://github.com/rofl0r/openbor.git
+    fi
 }
 
 function build_openbor() {
     if uname -m |grep "x86_64"; then
-    sed -i 's/-fsigned-char/-fsigned-char -fasm -g -no-pie /g' Makefile
-    make
+    cd "$md_build/engine"
+    sed -i 's/-Werror//g' Makefile 
+    ./build.sh 4 amd64
+    cd "$md_build/tools/borpak/source/"
+    chmod 755 build.sh
+    ./build.sh lin
     else
     local params=()
     ! isPlatform "x11" && params+=(NO_GL=1)
     make clean
     make "${params[@]}"
-    fi
     cd "$md_build/tools/borpak/"
     ./build-linux.sh
     md_ret_require="$md_build/OpenBOR"
+    fi
 }
 
 function install_openbor() {
-    md_ret_files=(
+    if uname -m |grep "x86_64"; then
+      md_ret_files=(
+       'engine/releases/LINUX_AMD64/OpenBOR/OpenBOR'
+       'tools/borpak/source/borpak'
+       'tools/unpack.sh'
+    )
+       else
+       md_ret_files=(
        'OpenBOR'
        'tools/borpak/borpak'
        'tools/unpack.sh'
     )
+    fi
 }
 
 function configure_openbor() {
-#    addPort "$md_id" "openbor" "OpenBOR - Beats of Rage Engine" "$md_inst/openbor.sh"
-     addPort "$md_id" "openbor" "OpenBOR - Beats of Rage Engine" "pushd $md_inst; $md_inst/OpenBOR %ROM%; popd"
+    addPort "$md_id" "openbor" "OpenBOR - Beats of Rage Engine" "pushd $md_inst; $md_inst/OpenBOR; popd"
 
     mkRomDir "ports/$md_id"
-    setDispmanx "$md_id" 1
 
-    cat >"$md_inst/openbor.sh" << _EOF_
-#!/bin/bash
-pushd "$md_inst"
-./OpenBOR "\$@"
-popd
-_EOF_
-    chmod +x "$md_inst/openbor.sh"
-
-    cat >"$md_inst/extract.sh" <<_EOF_
+cat >"$md_inst/extract.sh" <<_EOF_
 #!/bin/bash
 PORTDIR="$md_inst"
 BORROMDIR="$romdir/ports/$md_id"
@@ -82,10 +86,14 @@ do
   mv data/ "\$CURRENTFILE"/
   mv "\$CURRENTFILE"/ ../
 done
-
 echo "Your games are extracted and ready to be played. Your originals are stored safely in $BORROMDIR/original/ but they won't be needed anymore. Everything within it can be deleted."
 _EOF_
+
     chmod +x "$md_inst/extract.sh"
+    chmod +x "$md_inst/unpack.sh"
+    chown -R "$user":"$user" "$md_inst/extract.sh"
+    chown -R "$user":"$user" "$md_inst/unpack.sh"
+    chown -R "$user":"$user" "$md_inst"
 
     local dir
     for dir in ScreenShots Logs Saves; do
