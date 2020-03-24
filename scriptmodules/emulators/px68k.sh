RetroPie/RetroPie-Setup

commit 5a82a16176667d60ec9a30973d6e4c2a7f7e44aa

setup: add back the question for the install/update dialog

scriptmodules/emulators/px68k.sh
@@ -13,7 +13,7 @@ rp_module_id="px68k"
 rp_module_desc="SHARP X68000 Emulator"
 rp_module_help="You need to copy a X68000 bios file (iplrom30.dat, iplromco.dat, iplrom.dat, or iplromxv.dat), and the font file (cgrom.dat or cgrom.tmp) to $biosdir/keropi. Use F12 to access the in emulator menu."
 rp_module_section="exp"
-rp_module_flags="!mali !kms"
+rp_module_flags="!mali"
 
 function depends_px68k() {
     getDepends libsdl1.2-dev libsdl-gfx1.2-dev

scriptmodules/admin/setup.sh
@@ -235,7 +235,7 @@ function package_setup() {
 
         case "$choice" in
             U|B|S)
-                dialog --defaultno --yesno "Are you sure you want to ${option_msgs[$choice]}" 22 76 2>&1 >/dev/tty || continue
+                dialog --defaultno --yesno "Are you sure you want to ${option_msgs[$choice]}?" 22 76 2>&1 >/dev/tty || continue
                 local mode
                 case "$choice" in
                     U) mode="_auto_" ;;

commit 32f1ae8b7951d47ff826fe9ab3541c1a9422b8f1

cleanup: revert errors introduced in 5a82a1617

scriptmodules/emulators/px68k.sh
@@ -13,7 +13,7 @@ rp_module_id="px68k"
 rp_module_desc="SHARP X68000 Emulator"
 rp_module_help="You need to copy a X68000 bios file (iplrom30.dat, iplromco.dat, iplrom.dat, or iplromxv.dat), and the font file (cgrom.dat or cgrom.tmp) to $biosdir/keropi. Use F12 to access the in emulator menu."
 rp_module_section="exp"
-rp_module_flags="!mali"
+rp_module_flags="!malii !kms"
 
 function depends_px68k() {
     getDepends libsdl1.2-dev libsdl-gfx1.2-dev