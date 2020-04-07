b53d509dfd36c7a9745b387435261e61ede3e802
michael <michael@ubuntu.bionic>
2020-04-07 15:53:13 -0300

    Added lr-bsnes (current)

---
+++ scriptmodules/libretrocores/lr-bsnes.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="lr-bsnes"
+rp_module_desc="Super Nintendo emu - bsnes (current) port for libretro"
+rp_module_help="ROM Extensions: .smc .sfc .zip .7z\n\nCopy your SNES roms to $romdir/snes"
+rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/bsnes/master/LICENSE.txt"
+rp_module_section="opt"
+rp_module_flags="!armv6"
+
+function sources_lr-bsnes() {
+    gitPullOrClone "$md_build" https://github.com/libretro/bsnes.git
+}
+
+function build_lr-bsnes() {
+    cd "bsnes"
+    make clean
+    make target=libretro binary=library -j`nproc`
+    md_ret_require="$md_build/bsnes/out/bsnes_libretro.so"
+}
+
+function install_lr-bsnes() {
+    md_ret_files=(
+        'bsnes/out/bsnes_libretro.so'
+        'GPLv3.txt'
+	'LICENSE.txt'
+    )
+}
+
+function configure_lr-bsnes() {
+    mkRomDir "snes"
+    ensureSystemretroconfig "snes"
+
+    addEmulator 1 "$md_id" "snes" "$md_inst/bsnes_libretro.so"
+    addSystem "snes"
+}
