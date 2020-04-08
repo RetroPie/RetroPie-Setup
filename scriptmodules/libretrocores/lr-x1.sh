8216abb9759820308054cafe92cc232899d6b9e9
michael <michael@ubuntu.bionic>
2020-04-08 18:22:56 -0300

    Updates to lr-x1

--- scriptmodules/libretrocores/lr-x1.sh
+++ scriptmodules/libretrocores/lr-x1.sh
@@ -12,16 +12,17 @@
 rp_module_id="lr-x1"
 rp_module_desc="Sharp X1 emulator - X Millenium port for libretro"
 rp_module_help="ROM Extensions: .dx1 .zip .2d .2hd .tfd .d88 .88d .hdm .xdf .dup .cmd\n\nCopy your X1 roms to $romdir/x1\n\nCopy the required BIOS files IPLROM.X1 and IPLROM.X1T to $biosdir"
+rp_module_licence="BSD 3-Clause https://raw.githubusercontent.com/libretro/xmil-libretro/master/LICENSE"
 rp_module_section="exp"
 
 function sources_lr-x1() {
-    gitPullOrClone "$md_build" https://github.com/r-type/xmil-libretro.git
+    gitPullOrClone "$md_build" https://github.com/libretro/xmil-libretro.git
 }
 
 function build_lr-x1() {
     cd libretro
-    make clean
-    make
+    make -f Makefile.libretro clean
+    make -f Makefile.libretro -j`nproc`
     md_ret_require="$md_build/libretro/x1_libretro.so"
 }
 
