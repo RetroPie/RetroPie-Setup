8c60552558345cecde802a9e674e2b6bcc0513c0
c64-dev <58976930+c64-dev@users.noreply.github.com>
2020-04-15 13:39:27 +0300

    Added theme EpicNoir
    New ES theme

--- scriptmodules/supplementary/esthemes.sh
+++ scriptmodules/supplementary/esthemes.sh
@@ -236,6 +236,7 @@ function gui_esthemes() {
         'arcadeforge 240p_Bubblegum'
         'arcadeforge 240p-honey'
         'dionmunk clean'
+        'c64-dev epicnoir'
     )
     while true; do
         local theme
