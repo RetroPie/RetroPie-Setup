e06519a08ca21207091f0e0f89e499e1e1ffa0dc
valerino <xoanino@gmail.com>
2020-04-19 15:58:12 +0200

    added lr-vice-xvic (commodore vic20), gsplus (Apple II/gs), sdltrs (TRS-80, updated version)

--- scriptmodules/emulators/sdltrs.sh
+++ scriptmodules/emulators/sdltrs.sh
@@ -11,48 +11,48 @@
 
 rp_module_id="sdltrs"
 rp_module_desc="Radio Shack TRS-80 Model I/III/4/4P emulator"
-rp_module_help="ROM Extension: .dsk\n\nCopy your TRS-80 games to $romdir/trs-80\n\nCopy the required BIOS file level2.rom, level3.rom, level4.rom or level4p.rom to $biosdir"
+rp_module_help="ROM Extension: .dsk\n\nCopy your TRS-80 games to $romdir/trs-80\n\nCopy the required BIOS file level2.rom, model3.rom, model4.rom or model4p.rom to $biosdir\n\nOptionally, you may copy DOS disks to $biosdir$ as well: trs80-newdos.dsk for model 1, and trs80-lddos.dsk for model3/4/4p."
 rp_module_section="exp"
-rp_module_flags="dispmanx !mali !kms"
+rp_module_flags="dispmanx !mali"
 
 function depends_sdltrs() {
-    getDepends libsdl1.2-dev libxt-dev
+    getDepends libsdl2-dev libsdl1.2-dev libxt-dev
 }
 
 function sources_sdltrs() {
-    gitPullOrClone "$md_build" https://github.com/RetroPie/sdltrs.git
+    gitPullOrClone "$md_build" https://gitlab.com/jengun/sdltrs
 }
 
 function build_sdltrs() {
-    cd src/linux
+    cd src
     make clean
-    make
-    md_ret_require="$md_build/src/linux/sdltrs"
+    make nox
+    md_ret_require="$md_build/src/sdltrs"
 }
 
 function install_sdltrs() {
-    chmod 644 README
     md_ret_files=(
-        'src/linux/sdltrs'
-        'README'
+        'src/sdltrs'
     )
 }
 
 function configure_sdltrs() {
     mkRomDir "trs-80"
 
-    addEmulator 1 "$md_id-model1" "trs-80" "$md_inst/sdltrs -model 1 -romfile $biosdir/level2.rom -showled -diskdir $romdir/trs-80 -disk0 %ROM%"
-    addEmulator 0 "$md_id-model3" "trs-80" "$md_inst/sdltrs -model 3 -romfile3 $biosdir/level3.rom -showled -diskdir $romdir/trs-80 -disk0 %ROM%"
-    addEmulator 0 "$md_id-model4" "trs-80" "$md_inst/sdltrs -model 4 -romfile3 $biosdir/level4.rom -showled -diskdir $romdir/trs-80 -disk0 %ROM%"
-    addEmulator 0 "$md_id-model4p" "trs-80" "$md_inst/sdltrs -model 4p -romfile4p $biosdir/level4p.rom -showled -diskdir $romdir/trs-80 -disk0 %ROM%"
+    addEmulator 0 "$md_id-model1" "trs-80" "$md_inst/sdltrs -scanlines -model 1 -diskdir $romdir/trs-80 -disk0 %ROM%"
+    addEmulator 1 "$md_id-model3" "trs-80" "$md_inst/sdltrs -scanlines -model 3 -diskdir $romdir/trs-80 -disk0 %ROM%"
+    addEmulator 0 "$md_id-model4" "trs-80" "$md_inst/sdltrs -scanlines -model 4 -diskdir $romdir/trs-80 -disk0 %ROM%"
+    addEmulator 0 "$md_id-model4p" "trs-80" "$md_inst/sdltrs -scanlines -model 4p -diskdir $romdir/trs-80 -disk0 %ROM%"
+    addEmulator 0 "$md_id-model1-dos" "trs-80" "$md_inst/sdltrs -scanlines -model 1 -diskdir $romdir/trs-80 -disk0 /opt/retropie/emulators/sdltrs/trs80-newdos.dsk -disk1 %ROM%"
+    addEmulator 0 "$md_id-model3-dos" "trs-80" "$md_inst/sdltrs -scanlines -model 3 -diskdir $romdir/trs-80 -disk0 /opt/retropie/emulators/sdltrs/trs80-ldos.dsk -disk1 %ROM%"
+    addEmulator 0 "$md_id-model4-dos" "trs-80" "$md_inst/sdltrs -scanlines -model 4 -diskdir $romdir/trs-80 -disk0 /opt/retropie/emulators/sdltrs/trs80-lddos.dsk -disk1 %ROM%"
+    addEmulator 0 "$md_id-model4p-dos" "trs-80" "$md_inst/sdltrs -scanlines -model 4p -diskdir $romdir/trs-80 -disk0 /opt/retropie/emulators/sdltrs/trs80-lddos.dsk -disk1 %ROM%"
     addSystem "trs-80"
 
     [[ "$md_mode" == "remove" ]] && return
 
-    moveConfigFile "$home/sdltrs.t8c" "$md_conf_root/trs-80/sdltrs.t8c"
-
     local rom
-    for rom in level2.rom level3.rom level4.rom level4p.rom; do
+    for rom in level2.rom model3.rom model4.rom model4p.rom trs80-newdos.dsk trs80-lddos.dsk; do
         ln -sf "$biosdir/$rom" "$md_inst/$rom"
     done
 
