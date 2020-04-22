a9eac3dead3e3b7a1d8edc0bf725869e9110c5dd
valerino <xoanino@gmail.com>
2020-04-23 00:11:46 +0100

    added lr-mess-coco.sh

---
+++ scriptmodules/libretrocores/lr-mess-coco.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="lr-mess-coco"
+rp_module_desc="MESS emulator (Tandy Color Computer) - MESS Port for libretro"
+rp_module_help="ROM Extensions: .zip .cas .bin .ccc .rom\\n
+Put games in:\n
+$romdir/coco\n\n
+Put BIOS files in $biosdir:\n
+coco.zip\n\n
+instructions to load .cas:\n\n
+https://www.youtube.com/watch?v=vITUfbk-sDk"
+
+
+
+rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/mame/master/LICENSE.md"
+rp_module_section="exp"
+rp_module_flags=""
+
+function depends_lr-mess-coco() {
+	_mess=$(dirname "$md_inst")/lr-mess/mess_libretro.so
+	if [ ! -f "$_mess" ]; then
+		printMsgs dialog "cannot find '$_mess' !\n\nplease install 'lr-mess' package."
+		exit 1
+	fi
+}
+
+function sources_lr-mess-coco() {
+	true
+}
+
+function build_lr-mess-coco() {
+	true
+}
+
+function install_lr-mess-coco()  {
+	true
+}
+
+function configure_lr-mess-coco() {
+	_mess=$(dirname "$md_inst")/lr-mess/mess_libretro.so
+	_retroarch_bin="$rootdir/emulators/retroarch/bin/retroarch"
+	_system="coco"
+	_config="$configdir/$_system/retroarch.cfg"
+	_add_config="$_config.add"
+	_custom_coreconfig="$configdir/$_system/custom-core-options.cfg"
+	_script="$configdir/$_system/run_mess.sh"
+
+	# create retroarch configuration
+	ensureSystemretroconfig "$_system"
+
+	# ensure it works without softlists, using a custom per-fake-core config
+    iniConfig " = " "\"" "$_custom_coreconfig"
+    iniSet "mame_softlists_enable" "disabled"
+	iniSet "mame_softlists_auto_media" "disabled"
+	iniSet "mame_boot_from_cli" "disabled"
+
+	# this will get loaded too via --append_config
+	iniConfig " = " "\"" "$_add_config"
+	iniSet "core_options_path" "$_custom_coreconfig"
+	#iniSet "save_on_exit" "false"
+
+	# setup rom folder
+	mkRomDir "$_system"
+
+	# copy the juicy script which will do the all the hard work to the fake-core config folder
+	cp "$scriptdir/scriptmodules/run_mess.sh" "$_script"
+	chmod 755 "$_script"
+
+	# add the emulators.cfg as normal, pointing to the above script
+	addEmulator 1 "$md_id-cass" "$_system" "$_script $_retroarch_bin $_mess $_config coco $biosdir -cass %ROM%"
+	addEmulator 0 "$md_id-cart" "$_system" "$_script $_retroarch_bin $_mess $_config coco $biosdir -cart %ROM%"
+
+	# add system to es_systems.cfg as normal
+	addSystem "$_system" "Tandy Color Computer" ".zip .cas .bin .ccc .rom"
+}
