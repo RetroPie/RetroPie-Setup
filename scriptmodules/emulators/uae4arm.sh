3864a8eacab8f96ce4e8238a40d4f25bfdf50f41
Jools Wills <buzz@exotica.org.uk>
2020-04-23 14:18:49 +0100

    uae4arm - added missing libmpeg2-4-dev and removed unneeded parameter support  * the depends_uae4arm is no longer used by amiberry

--- scriptmodules/emulators/uae4arm.sh
+++ scriptmodules/emulators/uae4arm.sh
@@ -17,7 +17,7 @@ rp_module_section="opt"
 rp_module_flags="!all videocore"
 
 function depends_uae4arm() {
-    getDepends libsdl1.2-dev libsdl-gfx1.2-dev libsdl-ttf2.0-dev libguichan-dev libmpg123-dev libxml2-dev libflac-dev "${@}"
+    getDepends libsdl1.2-dev libsdl-gfx1.2-dev libsdl-ttf2.0-dev libguichan-dev libmpg123-dev libxml2-dev libflac-dev libmpeg2-4-dev
 }
 
 function sources_uae4arm() {
