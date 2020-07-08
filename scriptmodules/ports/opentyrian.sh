c841f745cab0d8a78c1bfbd44b609cf4a2752950
Jools Wills <buzz@exotica.org.uk>
2020-07-08 03:15:35 +0100

    opentyrian - upstream has now switched to sdl2 / rework module  * alter dependencies to libsdl2-dev / libsdl2-net-dev  * default to fullscreen  * move Makefile change to retain cflags and fullscreen change into patch file  * as now using sdl2, remove dispmanx flag and enable for mali targets

--- scriptmodules/ports/opentyrian.sh
+++ scriptmodules/ports/opentyrian.sh
@@ -13,16 +13,16 @@ rp_module_id="opentyrian"
 rp_module_desc="Open Tyrian - port of the DOS shoot-em-up Tyrian"
 rp_module_licence="GPL2 https://bitbucket.org/opentyrian/opentyrian/raw/3e3d6b925342a5891d8b937989dc50b563ff83dd/COPYING"
 rp_module_section="opt"
-rp_module_flags="dispmanx !mali"
+rp_module_flags=""
 
 function depends_opentyrian() {
-    getDepends libsdl1.2-dev libsdl-net1.2-dev
+    getDepends libsdl2-dev libsdl2-net-dev
 }
 
 function sources_opentyrian() {
     gitPullOrClone "$md_build" https://github.com/opentyrian/opentyrian.git
-    # don't replace our CFLAGS
-    sed -i "s/CFLAGS := -pedantic/CFLAGS += -pedantic/" Makefile
+    # patch to keep our custom cflags, and default to fullscreen
+    applyPatch "$md_data/01_cflags_and_fullscreen.diff"
 }
 
 function build_opentyrian() {
