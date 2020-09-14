c4100f51c7d87810c2862e4071ba2e7555c3fa2b
cmitu <31816814+cmitu@users.noreply.github.com>
2020-09-14 19:16:16 +0100

    openmsx: enable for `mali`, since it uses SDL2 now

--- scriptmodules/emulators/openmsx.sh
+++ scriptmodules/emulators/openmsx.sh
@@ -14,7 +14,7 @@ rp_module_desc="MSX emulator OpenMSX"
 rp_module_help="ROM Extensions: .rom .mx1 .mx2 .col .dsk .zip\n\nCopy your MSX/MSX2 games to $romdir/msx"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/openMSX/openMSX/master/doc/GPL.txt"
 rp_module_section="opt"
-rp_module_flags="!mali"
+rp_module_flags=""
 
 function depends_openmsx() {
     local depends=(libsdl2-dev libsdl2-ttf-dev libao-dev libogg-dev libtheora-dev libxml2-dev libvorbis-dev tcl-dev libasound2-dev)
