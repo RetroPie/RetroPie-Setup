cdcff18528fb7a343e876a153bdf51b0dc1f7b9b
cmitu <31816814+cmitu@users.noreply.github.com>
2020-10-19 18:50:58 +0100

    lr-mess2016: bgfx removal patch integrated upstream
    Similar to 70e96f1ad02e395e1

--- scriptmodules/libretrocores/lr-mess2016.sh
+++ scriptmodules/libretrocores/lr-mess2016.sh
@@ -18,9 +18,6 @@ rp_module_flags="nobin"
 
 function sources_lr-mess2016() {
     gitPullOrClone "$md_build" https://github.com/libretro/mame2016-libretro.git
-    # disable bgfx (fails on neon with recent GCC due to outdated SIMD instrinsics)
-    # see https://github.com/libretro/mame2016-libretro/pull/25
-    applyPatch "$scriptdir/scriptmodules/$md_type/lr-mame2016/01_disable_bgfx.diff"
 }
 
 function build_lr-mess2016() {
