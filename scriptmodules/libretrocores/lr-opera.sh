2b1890f213f8c2d2f509bd83e5a45656a435ee37
cmitu <31816814+cmitu@users.noreply.github.com>
2021-01-05 20:14:06 +0000

    platform/systems - add new supported extensions
    * Amiga - new extensions supported by `lr-puae`
    * C64 - new extensions supported by `lr-vice`
    * 3DO - added `.chd`, supported by the Opera Libretro core; updated license URL for the core.
    * GC/Wii - added `.rvz`, supported by Dolphin
    * MSX - added `.cas`, supported by both `lr-bluesmsx` and `openmsx`
    * PC88 - added `.m3u`, supported by `lr-quasi88`

--- scriptmodules/libretrocores/lr-opera.sh
+++ scriptmodules/libretrocores/lr-opera.sh
@@ -11,8 +11,8 @@
 
 rp_module_id="lr-opera"
 rp_module_desc="3DO emulator - fork of 4DO/FreeDO for libretro"
-rp_module_help="ROM Extension: .iso\n\nCopy your 3do roms to $romdir/3do\n\nCopy the required BIOS file panazf10.bin to $biosdir"
-rp_module_licence="LGPL https://raw.githubusercontent.com/libretro/opera-libretro/master/libfreedo/freedo_3do.c"
+rp_module_help="ROM Extension: .cue .chd .iso\n\nCopy your 3do roms to $romdir/3do\n\nCopy the required BIOS file panazf10.bin to $biosdir"
+rp_module_licence="LGPL https://raw.githubusercontent.com/libretro/opera-libretro/master/libopera/opera_3do.c"
 rp_module_section="exp"
 
 function sources_lr-opera() {
