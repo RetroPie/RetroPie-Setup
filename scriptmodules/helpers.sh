RetroPie/RetroPie-Setup

commit 6fa33497317defdc0117e7644d6fb1b357081cb8

helpers - fix own_sdl2 ini config logic - fixes #3279  * switched to use -eq so it will be less strict with for example whitespace in value  * do a test for -eq 0 rather than just else, to avoid defaulting to 0 in the case of a bad config

scriptmodules/helpers.sh
@@ -239,7 +239,11 @@ function _mapPackage() {
                 isPlatform "x11" && own_sdl2=0
                 iniConfig " = " '"' "$configdir/all/retropie.cfg"
                 iniGet "own_sdl2"
-                [[ "$ini_value" == "1" ]] && own_sdl2=1
+                if [[ "$ini_value" -eq 1 ]]; then
+                    own_sdl2=1
+                elif [[ "$ini_value" -eq 0 ]]; then
+                    own_sdl2=0
+                fi
                 [[ "$own_sdl2" -eq 1 ]] && pkg="RP sdl2 $pkg"
             fi
             ;;

commit 994bb42e5532c99a52fba68351f232c0b97525d9

helpers - fix custom sdl2 installing when no ini value set  * 6fa33497 broke defaulting to custom sdl2 as [[ "" -eq 0 ]] evaluates as true - use string matching as previously.

scriptmodules/helpers.sh
@@ -239,9 +239,9 @@ function _mapPackage() {
                 isPlatform "x11" && own_sdl2=0
                 iniConfig " = " '"' "$configdir/all/retropie.cfg"
                 iniGet "own_sdl2"
-                if [[ "$ini_value" -eq 1 ]]; then
+                if [[ "$ini_value" == "1" ]]; then
                     own_sdl2=1
-                elif [[ "$ini_value" -eq 0 ]]; then
+                elif [[ "$ini_value" == "0" ]]; then
                     own_sdl2=0
                 fi
                 [[ "$own_sdl2" -eq 1 ]] && pkg="RP sdl2 $pkg"