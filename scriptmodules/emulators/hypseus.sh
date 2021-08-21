0326e12d3534354be0a2d0468f0966d8a705d4a9
DirtBag Xon <github@pani.cx>
2021-08-21 12:10:06 +0100

    Add Hypsues Singe (Daphne) SDL2 Laserdisc emulator.
    This is to automate: https://github.com/DirtBagXon/hypseus-singe/blob/master/src/3rdparty/retropie/RETROPIE.md
    
    This plugin is not restricted in arcitecture like the previous SDL1 version.
    Supports Pi (inc. aarch64), ODroid & PC (i386/AMD64).
    
    Standing Forum discussions:
    https://retropie.org.uk/forum/topic/18505/new-hypseus-and-lr-daphne-to-add-on-retropie-setup
    https://retropie.org.uk/forum/topic/27426/request-add-daphne-singe-emulator
    https://retropie.org.uk/forum/topic/31155/integrate-daphne-emulator-back-into-the-retropie-script-for-ubuntu
    
    This setup follows the existing Daphne plugin installation as closely as possible.
    I have mirrored the included config file to the existing plugin where appropriate.

---
+++ scriptmodules/emulators/hypseus.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="hypseus"
+rp_module_desc="Hypseus Singe - Laserdisc Emulator"
+rp_module_help="ROM Extension: .daphne\n\nCopy your Daphne roms to $romdir/daphne"
+rp_module_licence="GPL3 https://raw.githubusercontent.com/DirtBagXon/hypseus-singe/master/LICENSE"
+rp_module_repo="git https://github.com/DirtBagXon/hypseus-singe.git RetroPie"
+rp_module_section="exp"
+rp_module_flags="sdl2"
+
+function depends_hypseus() {
+    getDepends libvorbis-dev libogg-dev zlib1g-dev libmpeg2-4-dev libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev cmake
+}
+
+function sources_hypseus() {
+    gitPullOrClone
+}
+
+function build_hypseus() {
+    rm -rf build
+    mkdir build
+    cd build
+    rpSwap on 1024
+    cmake ../src
+    make
+    rpSwap off
+    cp hypseus ../hypseus.bin
+    md_ret_require="hypseus"
+}
+
+function install_hypseus() {
+    md_ret_files=(
+        'sound'
+        'pics'
+        'fonts'
+        'hypseus.bin'
+        'LICENSE'
+    )
+}
+
+function configure_hypseus() {
+    mkRomDir "daphne"
+    mkRomDir "daphne/roms"
+
+    addEmulator 0 "$md_id" "daphne" "$md_inst/hypseus.sh %ROM%"
+    addSystem "daphne"
+
+    [[ "$md_mode" == "remove" ]] && return
+
+    mkUserDir "$md_conf_root/daphne"
+
+    local dir
+    for dir in ram logs screenshots; do
+        mkUserDir "$md_conf_root/daphne/$dir"
+        ln -snf "$md_conf_root/daphne/$dir" "$md_inst/$dir"
+    done
+
+    copyDefaultConfig "$md_data/hypinput.ini" "$md_conf_root/daphne/hypinput.ini"
+
+    ln -snf "$romdir/daphne/roms" "$md_inst/roms"
+    ln -snf "$romdir/daphne/roms" "$md_inst/singe"
+
+    ln -sf "$md_conf_root/daphne/hypinput.ini" "$md_inst/hypinput.ini"
+
+    local common_args="-framefile \"\$dir/\$name.txt\" -homedir \"$md_inst\" -fullscreen \$params"
+
+    cat >"$md_inst/hypseus.sh" <<_EOF_
+#!/bin/bash
+dir="\$1"
+name="\${dir##*/}"
+name="\${name%.*}"
+
+if [[ -f "\$dir/\$name.commands" ]]; then
+    params=\$(<"\$dir/\$name.commands")
+fi
+
+if [[ -f "\$dir/\$name.singe" ]]; then
+    "$md_inst/hypseus.bin" singe vldp -retropath -manymouse -script "\$dir/\$name.singe" $common_args
+else
+    "$md_inst/hypseus.bin" "\$name" vldp $common_args
+fi
+_EOF_
+    chmod +x "$md_inst/hypseus.sh"
+    mkdir -p "$md_inst/framefile"
+}
