d9b68d7630c03fca7411a2907e3c0e1802d473a3
hlad <hladma@gmail.com>
2021-08-28 21:52:46 +0200

    lr-opera works with .zip files

--- scriptmodules/libretrocores/lr-opera.sh
+++ scriptmodules/libretrocores/lr-opera.sh
@@ -11,7 +11,7 @@
 
 rp_module_id="lr-opera"
 rp_module_desc="3DO emulator - fork of 4DO/FreeDO for libretro"
-rp_module_help="ROM Extension: .cue .chd .iso\n\nCopy your 3do roms to $romdir/3do\n\nCopy the required BIOS file panazf10.bin to $biosdir"
+rp_module_help="ROM Extension: .cue .chd .iso .zip\n\nCopy your 3do roms to $romdir/3do\n\nCopy the required BIOS file panazf10.bin to $biosdir"
 rp_module_licence="LGPL https://raw.githubusercontent.com/libretro/opera-libretro/master/libopera/opera_3do.c"
 rp_module_repo="git https://github.com/libretro/opera-libretro.git master"
 rp_module_section="exp"
