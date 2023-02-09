778931777488018487ef3fa1ba6ea706eeba07c4
Stefan <gizmo98@users.noreply.github.com>
2023-02-09 12:31:36 +0100

    yabasanshiro: remove package python-pip
    Package python-pip is not necessary to compile and run yabasanshiro. Debian >= 10 has no python-pip package. Remove it.

--- scriptmodules/emulators/yabasanshiro.sh
+++ scriptmodules/emulators/yabasanshiro.sh
@@ -18,7 +18,7 @@ rp_module_section="exp"
 rp_module_flags="!all rpi !videocore"
 
 function depends_yabasanshiro() {
-    local depends=(cmake pkg-config python-pip protobuf-compiler libprotobuf-dev libsecret-1-dev libssl-dev libsdl2-dev libboost-all-dev)
+    local depends=(cmake pkg-config protobuf-compiler libprotobuf-dev libsecret-1-dev libssl-dev libsdl2-dev libboost-all-dev)
     getDepends "${depends[@]}"
 }
 
