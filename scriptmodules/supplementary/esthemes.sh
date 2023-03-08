794f662eca531bbb3c8d3bc0a28ed350070020bc
flpowergamesretro <102295892+flpowergames@users.noreply.github.com>
2023-03-07 22:35:07 -0300

    >>Text translated from Portuguese (Brazil) into English by Google translator<<
    Hello! I'd like to share a theme I developed for the RetroPie 4.8 update!

--- scriptmodules/supplementary/esthemes.sh
+++ scriptmodules/supplementary/esthemes.sh
@@ -315,6 +315,7 @@ function gui_esthemes() {
         'cjonasw raspixel-320-240'
         'crxone 3twenty2fourty'
         'leochely Guilty-Gear'
+        'flpowergamesretro es-theme-flpowergamesretro_v1.0'
     )
     while true; do
         local theme
