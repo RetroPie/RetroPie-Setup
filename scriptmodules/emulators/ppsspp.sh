5b20070cc4639bd14f974b1b87d36eb6e943a22e
cmitu <31816814+cmitu@users.noreply.github.com>
2023-10-19 20:07:08 +0100

    ppsspp: upgrade to version 1.16.6
    Fixes building on Debian 12 Bookworm for the included `ffmpeg` project.
    
    Changes since 13.x:
    
    * ver 1.14.x (full list of changes - https://www.ppsspp.org/news/release-1.14)
    
        * Graphics fixes in Burnout Dominator, Kurohyou, Ratchet & Clank, Outrun, Ridge Racer, Spongebob: Yellow Avenger, Cars: Race-o-Rama, Hunter x Hunter, many others
        * Performance improvements: Killzone now runs at playable speed, Juiced 2 performance fixed, lighting ubershader optimization
        * Many software renderer improvements, both performance and accuracy.
        * Some input handling fixes, IR interpreter fixes, and so much more.
        * MSAA antialiasing added to the Vulkan backend (desktop only)
        * New API for plugins to access aspect ratio, scaling and fast-forward ([#16441]), other new APIs & improvements
        * UI improvements
           - New interactive Display Layout & Effects screen, replacing the old Display Layout Editor
           - Add default shader for LCD persistence simulation
           - Game being played can now be seen as a background in most menus
           - Reorganize speed hack settings
        * Stability fixes
           - Workaround for hangs on older Adreno GPUs
           - Input handling fixes for deadzones and touch controls
           - Avoid game bugs in Twinbee Portable and Shining Ark
        * Performance improvements
           - Vulkan bandwidth and synchronization optimizations
           - Lighting "ubershader" optimization to prevent hitches
           - Assorted minor other improvements
           - Improve texture replacement memory usage
           - Texture upscaling speedup and fixes
    
    * version 1.15 (full list of changes - https://www.ppsspp.org/news/release-1.15)
    
         * Tilt controls: Restore "Low end radius" control
         * Improve the workarounds for the DJ Max games
         * OpenGL: Restore most of the lost performance on low-end hardware from the shader variant reduction
         * Add a simple profiling tool to check CPU usage by the GL backend
         * Loader: Fix bug preventing WWE 2009 from starting
         * Control mapping: Fix bugs in mapping UI causing trouble with some controllers
         * Fix for broken tilt d-pad controls
         * Stutter caused by shader compilation has been reduced drastically
         * Rendering performance and fixes
         * Texture replacement improvements
         * Optimizations for Vulkan texture uploads, Software renderer, Lighting shader optimizations.
         * GLSL shader compatibility fixes
         * Rewind savestates no longer slows things down a lot
         * Depth-related rendering fixes
         * UI fixes: Vertical use of space, scrollbars, touchpad scroll on Windows
    
    * version 1.16 (full list of changes - https://www.ppsspp.org/news/release-1.16)
    
         * RetroAchievements support !
         * Fix performance issue with Vulkan descriptor set allocation
         * Smoother loading of replacement textures
         * Fix problem with waves background
         * Fix for crash when changing render backend in-game
         * Simplify shader cache lookups on Vulkan, hopefully fixing some crashes
         * Other crash and stability fixes
         * Assorted improvements to the IR JITs
         * Fix crash bug and performance issue in Vulkan shader cache
         * Fix for HTTP support on Linux on networks with shaky or incomplete IPv6 support
         * Fix hang when switching UMD with RetroAchievements enabled
         * Input fixes
         * Rendering performance and fixes
         * Texture replacement fixes
         * UI changes
             - Color emoji support on some platforms
             - Use TTF fonts on SDL where available (macOS, Linux/Steam Deck)
             - Allow setting the PSP's MAC address directly
             - Better looking notifications
         * Fix horrible audio glitch in After Burner
         * Fix timing issue causing slowdowns in MLB games
         * Emulate the vrnd instruction accurately

--- scriptmodules/emulators/ppsspp.sh
+++ scriptmodules/emulators/ppsspp.sh
@@ -13,7 +13,7 @@ rp_module_id="ppsspp"
 rp_module_desc="PlayStation Portable emulator PPSSPP"
 rp_module_help="ROM Extensions: .iso .pbp .cso\n\nCopy your PlayStation Portable roms to $romdir/psp"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/hrydgard/ppsspp/master/LICENSE.TXT"
-rp_module_repo="git https://github.com/hrydgard/ppsspp.git v1.13.2"
+rp_module_repo="git https://github.com/hrydgard/ppsspp.git v1.16.6"
 rp_module_section="opt"
 rp_module_flags=""
 
