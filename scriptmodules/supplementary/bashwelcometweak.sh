4f3d246396dabd358cc2212ff492780a14e6ecf4
cmitu <31816814+cmitu@users.noreply.github.com>
2024-01-21 06:26:55 +0000

    bashwelcometweak: fix GPU temp reading
    On RasPI OS 11/12 (bullseye/bookworm) the `vcgencmd` command has moved completely to `/usr/bin`, while on Buster it's just a symlink from `/usr/bin/` to `/opt/vc/bin`. Remove the full path to the command and just check whether is't in our `$PATH` before running.

--- scriptmodules/supplementary/bashwelcometweak.sh
+++ scriptmodules/supplementary/bashwelcometweak.sh
@@ -44,8 +44,8 @@ function retropie_welcome() {
         cpuTempC=$(($(cat /sys/class/thermal/thermal_zone0/temp)/1000)) && cpuTempF=$((cpuTempC*9/5+32))
     fi
 
-    if [[ -f "/opt/vc/bin/vcgencmd" ]]; then
-        if gpuTempC=$(/opt/vc/bin/vcgencmd measure_temp); then
+    if [[ -n $(command -v vcgencmd) ]]; then
+        if gpuTempC=$(vcgencmd measure_temp); then
             gpuTempC=${gpuTempC:5:2}
             gpuTempF=$((gpuTempC*9/5+32))
         else
