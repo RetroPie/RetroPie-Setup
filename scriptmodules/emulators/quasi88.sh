7479eb2d42ec1d56b9121b0aef98edefb384ddd7
cmitu <31816814+cmitu@users.noreply.github.com>
2024-01-27 04:51:54 +0000

    quasi88: switch upstream repository
    Switched the Quasi88 upstream repository to the "Quasi88Kai" fork [1], since it has some new developments and also supports SDL2.
    
    Changes in the fork:
    
        * General updates to the build system and migration to CMake
        * All files converted to UTF-8
        * Many improvements in Windows, X11, GTK and SDL backends
        * Add a RetroAchievements-compatible version "RAQUASI88" (only for Windows at this time)
    
    Had to apply a build patch since:
      * SDL2 2.0.10 libraries do not provide the SDL:SDL aliases in the `sdl2-config.cmake`
      * Buster's GCC (8) is implementing the filesystem C++17 API in `libstdc++fs`
    
    [1] https://github.com/winterheart/quasi88

--- scriptmodules/emulators/quasi88.sh
+++ scriptmodules/emulators/quasi88.sh
@@ -12,27 +12,36 @@
 rp_module_id="quasi88"
 rp_module_desc="NEC PC-8801 emulator"
 rp_module_help="ROM Extensions: .d88 .88d .cmt .t88\n\nCopy your pc88 games to to $romdir/pc88\n\nCopy bios files FONT.ROM, N88.ROM, N88KNJ1.ROM, N88KNJ2.ROM, and N88SUB.ROM to $biosdir/pc88"
-rp_module_repo="file $__archive_url/quasi88-0.6.4.tgz"
+rp_module_repo="git https://github.com/winterheart/quasi88.git master"
+rp_module_licence="BSD https://raw.githubusercontent.com/winterheart/quasi88/master/LICENSE.txt"
 rp_module_section="exp"
-rp_module_flags="sdl1 !mali !kms"
+rp_module_flags="sdl2"
 
 function depends_quasi88() {
-    getDepends libsdl1.2-dev
+    getDepends cmake libsdl2-dev libfmt-dev libspdlog-dev
 }
 
 function sources_quasi88() {
-    downloadAndExtract "$md_repo_url" "$md_build" --strip-components 1
-    applyPatch "$md_data/01_fixes.diff"
+    gitPullOrClone
+    applyPatch "$md_data/01_cmake.diff"
 }
 
 function build_quasi88() {
-    make X11_VERSION= SDL_VERSION=1 clean
-    make X11_VERSION= SDL_VERSION=1 ARCH=linux SOUND_SDL=1 USE_OLD_MAME_SOUND=1 USE_FMGEN=1 ROMDIR="$biosdir/pc88" DISKDIR="$romdir/pc88" TAPEDIR="$romdir/pc88"
-    md_ret_require="$md_build/quasi88.sdl"
+    rm -fr build
+    mkdir -p build && pushd build
+    cmake -DROMDIR="$biosdir/pc88" -DDISKDIR="$romdir/pc88" -DTAPEDIR="$romdir/pc88" ..
+    make
+    popd
 }
 
 function install_quasi88() {
-    make X11_VERSION= SDL_VERSION=1 BINDIR="$md_inst" install
+    md_ret_files=(
+        "build/quasi88.sdl"
+        "document"
+        "LICENSE.txt"
+        "README.md"
+        "ChangeLog.md"
+    )
 }
 
 function configure_quasi88() {
@@ -42,4 +51,23 @@ function configure_quasi88() {
 
     addEmulator 1 "$md_id" "pc88" "$md_inst/quasi88.sdl -f6 IMAGE-NEXT1 -f7 IMAGE-NEXT2 -f8 NOWAIT -f9 ROMAJI -f10 NUMLOCK -fullscreen %ROM%"
     addSystem "pc88"
+
+    [[ "$mode" == "remove" ]] && return
+
+    # add a minimal configuration file
+    local conf="$md_conf_root/pc88/quasi88.ini"
+    if [[ ! -f "$conf" ]]; then
+cat > "$conf" << EOF
+-auto_mouse
+-nostatus
+-double
+-english
+-use_joy
+-verbose 1
+-romdir "$biosdir/pc88"
+-diskdir "$romdir/pc88"
+-tapedir "$romdir/pc88"
+EOF
+    chown $user:$user "$conf"
+    fi
 }
