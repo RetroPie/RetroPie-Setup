28f2c8d367387ec5caa735178879c351930800f9
Jools Wills <buzz@exotica.org.uk>
2024-05-15 20:03:46 +0100

    Move nodistcc flag out of module sections
    Errors introduced in #3902 (oops!)

--- scriptmodules/libretrocores/lr-picodrive.sh
+++ scriptmodules/libretrocores/lr-picodrive.sh
@@ -14,7 +14,8 @@ rp_module_desc="Sega 8/16 bit emu - picodrive arm optimised libretro core"
 rp_module_help="ROM Extensions: .32x .iso .cue .sms .smd .bin .gen .md .sg .zip\n\nCopy your Megadrive / Genesis roms to $romdir/megadrive\nMasterSystem roms to $romdir/mastersystem\nSega 32X roms to $romdir/sega32x and\nSegaCD roms to $romdir/segacd\nThe Sega CD requires the BIOS files us_scd1_9210.bin, eu_mcd1_9210.bin, jp_mcd1_9112.bin copied to $biosdir"
 rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/picodrive/master/COPYING"
 rp_module_repo="git https://github.com/libretro/picodrive.git master"
-rp_module_section="main nodistcc"
+rp_module_section="main"
+rp_module_flags="nodistcc"
 
 function sources_lr-picodrive() {
     gitPullOrClone
