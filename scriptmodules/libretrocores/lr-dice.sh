303770cf4dcb5b93b1b5e9c850c8448d72b22eb2
Ken Mitton <mittonk@alum.mit.edu>
2025-03-12 11:11:40 -0400

    DICE emulator for machines without a CPU.

---
+++ scriptmodules/libretrocores/lr-dice.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="lr-dice"
+rp_module_desc="Discrete Integrated Circuit Emulator for machines without a CPU - DICE core for libretro"
+rp_module_help="ROM Extensions: .zip .dmy .k1 .a1 .6c .c6 .d2 .s1 .f4 .a4 .1da .da1 .C4 .4c .4d .d7 .d4\n\nCopy your DICE TTL roms to $romdir/dice"
+rp_module_licence="GPL3 https://raw.githubusercontent.com/mittonk/dice-libretro/main/LICENSE.txt"
+rp_module_repo="git https://github.com/mittonk/dice-libretro.git main"
+rp_module_section="exp"
+
+function depends_lr-dice() {
+    getDepends zip
+}
+
+function sources_lr-dice() {
+    gitPullOrClone
+}
+
+function build_lr-dice() {
+    make clean
+    make
+    md_ret_require="$md_build/dice_libretro.so"
+}
+
+function install_lr-dice() {
+    md_ret_files=(
+        'README.md'
+        'dice_libretro.so'
+        'LICENSE.txt'
+    )
+}
+
+function configure_lr-dice() {
+    mkRomDir "dice"
+    defaultRAConfig "dice"
+
+    addEmulator 0 "$md_id" "dice" "$md_inst/dice_libretro.so"
+    addSystem "dice"
+}
