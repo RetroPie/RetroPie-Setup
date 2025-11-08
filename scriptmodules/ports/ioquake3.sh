9bfef8227b8e242ccb586f0d913e9c7c0ee6fe6b
Vladimir Kochnev <hashtable@yandex.ru>
2025-11-08 11:49:18 +0300

    ioquake3: Switch to CMake for building (#4094)
    * ioquake3: Switch to CMake for building
    
    ioquake3 - building using a `Makefile` is deprecated starting November 6, 2025.
    
    See:
     - https://github.com/ioquake/ioq3/pull/750
     - https://github.com/ioquake/ioq3/commit/2c893f2fc2a5792c835ae415c090be681b634495

--- scriptmodules/ports/ioquake3.sh
+++ scriptmodules/ports/ioquake3.sh
@@ -12,12 +12,21 @@
 rp_module_id="ioquake3"
 rp_module_desc="Quake 3 source port"
 rp_module_licence="GPL2 https://github.com/ioquake/ioq3/blob/master/COPYING.txt"
-rp_module_repo="git https://github.com/ioquake/ioq3 main"
+rp_module_repo="git https://github.com/ioquake/ioq3 main :_get_commit_ioquake3"
 rp_module_section="opt"
 rp_module_flags="!videocore"
 
+function _get_commit_ioquake3() {
+    # On Buster and Bullseye we have to build using make (an old method) instead of cmake.
+    # This is because ioquake3 requires CMake 3.25 or higher which is satisfied only on Bookworm.
+    if [[ "$__os_debian_ver" -lt 12 ]]; then
+        # This is the latest commit before the Makefile was removed.
+        echo 7ac92951f2da597611ab4525023979df2f92047a
+    fi
+}
+
 function depends_ioquake3() {
-    getDepends libsdl2-dev libgl1-mesa-dev
+    getDepends cmake libsdl2-dev libgl1-mesa-dev
 }
 
 function sources_ioquake3() {
@@ -25,27 +34,37 @@ function sources_ioquake3() {
 }
 
 function build_ioquake3() {
-    make clean
-    make
-    md_ret_require="$md_build/build/release-linux-$(_arch_ioquake3)/ioquake3.$(_arch_ioquake3)"
+    if [[ "$__os_debian_ver" -lt 12 ]]; then
+        make clean
+        I_ACKNOWLEDGE_THE_MAKEFILE_IS_DEPRECATED=1 make
+    else
+        cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+        cmake --build build --clean-first
+    fi
+    md_ret_require="$md_build/$(_release_dir)/ioquake3"
 }
 
-function _arch_ioquake3() {
-    # exact parsing from Makefile
-    echo "$(uname -m | sed -e 's/i.86/x86/' | sed -e 's/^arm.*/arm/' | sed -e 's/aarch64/arm64/')"
+function _release_dir() {
+    if [[ "$__os_debian_ver" -lt 12 ]]; then
+        # exact parsing from Makefile
+        local arch_ioquake3="$(uname -m | sed -e 's/i.86/x86/' | sed -e 's/^arm.*/arm/' | sed -e 's/aarch64/arm64/')"
+        echo "build/release-linux-${arch_ioquake3}"
+    else
+        echo "build/Release"
+    fi
 }
 
 function install_ioquake3() {
     md_ret_files=(
-        "build/release-linux-$(_arch_ioquake3)/ioq3ded.$(_arch_ioquake3)"
-        "build/release-linux-$(_arch_ioquake3)/ioquake3.$(_arch_ioquake3)"
-        "build/release-linux-$(_arch_ioquake3)/renderer_opengl1_$(_arch_ioquake3).so"
-        "build/release-linux-$(_arch_ioquake3)/renderer_opengl2_$(_arch_ioquake3).so"
+        "$(_release_dir)/ioq3ded"
+        "$(_release_dir)/ioquake3"
+        "$(_release_dir)/renderer_opengl1.so"
+        "$(_release_dir)/renderer_opengl2.so"
     )
 }
 
 function configure_ioquake3() {
-    local launcher=("$md_inst/ioquake3.$(_arch_ioquake3)")
+    local launcher=("$md_inst/ioquake3")
     isPlatform "mesa" && launcher+=("+set cl_renderer opengl1")
     isPlatform "kms" && launcher+=("+set r_mode -1" "+set r_customwidth %XRES%" "+set r_customheight %YRES%" "+set r_swapInterval 1")
 
